14c45db85aa954b06594f5219840cc51c1ec3bbb
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-24 15:37:52 -0800

    Time: 3 ms (29.17%) | Memory: 16.9 MB (18.91%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,19 +1,9 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        #self.ans = 0
         memo = {}
         def dp(i, curr):
-            if i >= len(s):
-                return 1
-            if s[i] == "0":
-                return 0
-            if i in memo:
-                return memo[i]
-            if i < (len(s) - 1) and ((s[i] == "2" and s[i+1] <= "6") or s[i] < "2"):
-                temp = dp(i + 1, 0) + dp(i + 2, 0)
-                memo[i] = temp
-                return curr + temp
-            temp = dp(i + 1, 0)
-            memo[i] = temp
-            return curr + temp
-        return dp(0,0)
+            if i >= len(s) or s[i] == "0" or i in memo: return int(i >= len(s)) if i not in memo else memo[i]
+            memo[i] = dp(i + 1, 0)
+            if i < (len(s) - 1) and ((s[i] == "2" and s[i+1] <= "6") or s[i] < "2"): memo[i] += dp(i + 2, 0)
+            return curr + memo[i]
+        return dp(0, 0)
