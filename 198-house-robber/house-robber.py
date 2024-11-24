ceb12b819d0961bd6630203e8f3f39021fa2f1f9
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-24 15:02:59 -0800

    Time: 0 ms (100.00%) | Memory: 16.5 MB (67.02%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,7 +2,5 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         prevTwo = prevOne = 0
         for n in nums:
-            temp = prevTwo
-            prevTwo = prevOne
-            prevOne = max(temp + n, prevOne)
+            temp, prevTwo, prevOne = prevTwo, prevOne, max(prevTwo + n, prevOne)
         return prevOne
