3c5e2664ccc5d3d7e576d52ecc46556948893104
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-24 15:02:23 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (32.66%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,17 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
-        def dp(i, cont, tot):
-            if i == len(nums):
-                return tot
-            if (i, cont) in memo:
-                return tot+memo[i, cont]
-            if cont:
-                temp = max(dp(i + 1, False, nums[i]), dp( i+ 1, True, 0))
-                memo[i, cont] = temp
-                return tot+temp
-            else:
-                temp = dp(i + 1, True, 0)
-                memo[i,cont] = temp
-                return tot+temp
-        return dp(0, True, 0)
+        prevTwo = prevOne = 0
+        for n in nums:
+            temp = prevTwo
+            prevTwo = prevOne
+            prevOne = max(temp + n, prevOne)
+        return prevOne
