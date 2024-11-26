680f45a4d58dbd7fabb79600079d8e854782d131
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 15:22:53 -0800

    Time: 927 ms (12.61%) | Memory: 26 MB (44.32%) - LeetSync

--- 2889-number-of-black-blocks/number-of-black-blocks.py
+++ 2889-number-of-black-blocks/number-of-black-blocks.py
@@ -1,15 +1,12 @@
 class Solution:
     def countBlackBlocks(self, m: int, n: int, coordinates: List[List[int]]) -> List[int]:
-        counts=defaultdict(int)
-        for x,y in coordinates:
-            for i in range(max(0,x-1),min(m-1,x+1)):
-                for j in range(max(0,y-1),min(n-1,y+1)):
-                    counts[(i,j)]+=1
-
-
-        ans=[0]*5 
-        for count in counts.values():
-            ans[count]+=1
-
-        ans[0]=(m-1)*(n-1)-sum(ans)
-        return ans
+        count, blocks = [(m-1)*(n-1),0,0,0,0], {}
+        for x, y in coordinates:
+            for i in range(max(0, x - 1), min(m - 1, x + 1)):
+                for j in range(max(0, y - 1), min(n - 1, y + 1)):
+                    if (i, j) not in blocks:
+                        blocks[(i, j)] = 0
+                    count[blocks[(i, j)]] -= 1
+                    blocks[(i, j)] += 1
+                    count[blocks[(i, j)]] += 1
+        return  count#(m - 1) * (n - 1) - sum(count[1:])
