a47c2f4c8efa5e48e1a020f91eafef827b8abb12
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 01:31:42 -0800

    Time: 311 ms (7.22%) | Memory: 29.8 MB (68.59%) - LeetSync

--- 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.py
+++ 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.py
@@ -4,6 +4,5 @@ class Solution:
         suffix[-1], prefix[0] = nums[-1], nums[0]
         for i in range(1,l): prefix[i], suffix[l - i - 1] = max(prefix[i - 1], nums[i]), min(suffix[l - i], nums[l - i - 1])
         for i in range(1, l - 1):
-            if prefix[i-1] < nums[i] < suffix[i+1]: ans+=2
-            elif nums[i-1] < nums[i] < nums[i+1]: ans+=1
+            ans += int(nums[i-1] < nums[i] < nums[i+1]) + int(prefix[i-1] < nums[i] < suffix[i+1])
         return ans
