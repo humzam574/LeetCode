e9d4be48cfa33dfc0f53625f74baf4568240ad84
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 01:32:15 -0800

    Time: 73 ms (98.97%) | Memory: 29.3 MB (87.43%) - LeetSync

--- 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.py
+++ 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.py
@@ -1,8 +1,20 @@
 class Solution:
     def sumOfBeauties(self, nums: List[int]) -> int:
-        prefix, suffix, ans, l = [None] * len(nums), [None] * len(nums), 0, len(nums)
-        suffix[-1], prefix[0] = nums[-1], nums[0]
-        for i in range(1,l): prefix[i], suffix[l - i - 1] = max(prefix[i - 1], nums[i]), min(suffix[l - i], nums[l - i - 1])
-        for i in range(1, l - 1):
-            ans += int(nums[i-1] < nums[i] < nums[i+1]) + int(prefix[i-1] < nums[i] < suffix[i+1])
-        return ans
+        suffix = [0] * len(nums)
+        suf = nums[len(nums) - 1]
+        for i in range(len(nums) - 1, -1, -1):
+            suffix[i] = suf
+            if nums[i] < suf:
+                suf = nums[i]
+        
+        res = 0
+        pre = nums[0]
+        for i in range(1, len(nums) - 1):
+            if pre < nums[i] < suffix[i]:
+                res += 2
+            elif nums[i - 1] < nums[i] < nums[i + 1]:
+                res += 1
+            if pre < nums[i]:
+                pre = nums[i]
+
+        return res
