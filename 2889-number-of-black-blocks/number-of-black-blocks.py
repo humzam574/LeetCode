8613d78e55adb0615d6fedcb11aa9be9e9ce2eff
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 15:23:46 -0800

    Time: 923 ms (12.61%) | Memory: 26.1 MB (41.48%) - LeetSync

--- 2889-number-of-black-blocks/number-of-black-blocks.py
+++ 2889-number-of-black-blocks/number-of-black-blocks.py
@@ -4,9 +4,8 @@ class Solution:
         for x, y in coordinates:
             for i in range(max(0, x - 1), min(m - 1, x + 1)):
                 for j in range(max(0, y - 1), min(n - 1, y + 1)):
-                    if (i, j) not in blocks:
-                        blocks[(i, j)] = 0
+                    if (i, j) not in blocks: blocks[(i, j)] = 0
                     count[blocks[(i, j)]] -= 1
                     blocks[(i, j)] += 1
                     count[blocks[(i, j)]] += 1
-        return  count#(m - 1) * (n - 1) - sum(count[1:])
+        return  count
