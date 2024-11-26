e6123f630816ff7d5375ed273d3dcf293959828a
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-25 20:01:01 -0800

    Time: 10 ms (54.97%) | Memory: 17.6 MB (94.59%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.py
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -1,20 +1,5 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        losers = set()
-        tot = set([i for i in range(n)])
-        for edge in edges:
-            # if edge[0] in losers:
-            #     tot.remove(edge[1])
-            # if edge[0] not in losers:
-            #     losers.add(edge[1])
-            #if edge[1] in tot:
-            tot.discard(edge[1])
-        # print(losers)
-        # print(tot)
-        # for l in losers:
-        #     tot.remove(l)
-        if len(tot) > 1 or len(tot) == 0:
-            return -1
-        return tot.pop()
-            
-                
+        ans = set([i for i in range(n)])
+        for edge in edges: ans.discard(edge[1])
+        return -1 if len(ans) > 1 else ans.pop()
