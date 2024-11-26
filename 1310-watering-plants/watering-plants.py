3de979f150a18db1cd962fc8c89f9a511bb1bee1
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 01:13:24 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (15.36%) - LeetSync

--- 1310-watering-plants/watering-plants.py
+++ 1310-watering-plants/watering-plants.py
@@ -1,8 +1,7 @@
 class Solution:
     def wateringPlants(self, plants: List[int], capacity: int) -> int:
-        ans, can = 1, capacity
-        for i in range(len(plants) - 1):
-            can -= plants[i]
-            if plants[i+1] <= can: ans += 1
-            else: ans, can = ans+2*i+3, capacity
+        temp, ans = capacity, 0
+        for index, i in enumerate(plants):
+            if i <= capacity: capacity, ans = capacity - i, ans + 1
+            else: capacity, ans = temp - i, ans + 2*index +1
         return ans
