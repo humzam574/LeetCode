6936d4e5a88eefb6e8c12abf8d85eabd7177d637
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 00:20:10 -0800

    Time: 56 ms (96.78%) | Memory: 63.4 MB (48.04%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.py
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.py
@@ -1,7 +1,16 @@
+import collections
+import math
+
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
-        dict,ans=defaultdict(int),0
-        for rec in rectangles:dict[rec[0]/rec[1]]+=1
-        for val in dict.values():
-            if val>1:ans+=(val*(val-1)//2)
-        return ans
+        s=0
+        ratios=[]
+        for r in rectangles:
+            ratios.append(r[0]/r[1])
+
+        c = collections.Counter(ratios)
+
+        for value in c.values():
+            s+=math.comb(value, 2)
+        
+        return s
