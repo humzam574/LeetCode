54c32d63952b472c9bd852c7a45cc96721f0c916
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 01:31:24 -0800

    Time: 222 ms (27.84%) | Memory: 29.7 MB (75.92%) - LeetSync

--- 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.py
+++ 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.py
@@ -1,20 +1,9 @@
 class Solution:
     def sumOfBeauties(self, nums: List[int]) -> int:
-        prefix = [nums[0]]
-        for i in range(1,len(nums)):
-            prefix.append(max(prefix[-1], nums[i]))
-        suffix = [None] * len(nums)
-        suffix[-1] = nums[-1]
-        for i in range(len(nums) - 2, -1, -1):
-            suffix[i] = min(suffix[i+1], nums[i])
-        ans = 0
-        #print(prefix)
-        #print(suffix)
-        for i in range(1, len(nums) - 1):
-            if prefix[i-1] < nums[i] < suffix[i+1]:
-                #print("plus two at i = "+ str(i))
-                ans+=2
-            elif nums[i-1] < nums[i] < nums[i+1]:
-                #print("plus one at i = " + str(i))
-                ans+=1
+        prefix, suffix, ans, l = [None] * len(nums), [None] * len(nums), 0, len(nums)
+        suffix[-1], prefix[0] = nums[-1], nums[0]
+        for i in range(1,l): prefix[i], suffix[l - i - 1] = max(prefix[i - 1], nums[i]), min(suffix[l - i], nums[l - i - 1])
+        for i in range(1, l - 1):
+            if prefix[i-1] < nums[i] < suffix[i+1]: ans+=2
+            elif nums[i-1] < nums[i] < nums[i+1]: ans+=1
         return ans
