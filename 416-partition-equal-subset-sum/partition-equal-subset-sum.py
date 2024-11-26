facd9dbaaaa92b8c281e0627be126b0e2828f520
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-25 22:20:58 -0800

    Time: 1384 ms (29.18%) | Memory: 53.6 MB (24.69%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,19 +1,11 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        tot = sum(nums)
-        if tot % 2 == 1:
-            return False
-        memo = {}
+        tot, n = sum(nums), len(nums)
+        if tot % 2: return False
+        memo = [[-1] * (tot // 2 + 1) for i in range(n + 1)]
         def bt(curr, i):
-            if curr == 0:
-                return True
-            if i >= len(nums):
-                return False
-            if curr < 0:
-                return False
-            if (curr, i) in memo:
-                return memo[curr,i]
-            temp = bt(curr, i+1) or bt(curr -nums[i], i+1)
-            memo[curr, i] = temp
-            return temp
+            if curr == 0 or i >= n or curr < 0: return curr == 0
+            if memo[i][curr] != -1: return memo[i][curr]
+            memo[i][curr] = bt(curr, i + 1) or bt(curr - nums[i], i + 1)
+            return memo[i][curr]
         return bt(tot // 2, 0)
