831a109e97a4f936fb3090a6260c8579db2bd23a
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-26 00:08:14 -0800

    Time: 152 ms (65.93%) | Memory: 18.8 MB (46.15%) - LeetSync

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.py
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.py
@@ -1,12 +1,8 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        sd = collections.Counter(s)
-        td = collections.Counter(t)
+        sd, td = collections.Counter(s), collections.Counter(t)
         for key, value in sd.items():
             if key in td:
                 temp = min(td[key], sd[key])
-                td[key]-=temp
-                sd[key]-=temp
-        print(td)
-        print(sd)
+                td[key], sd[key] = td[key] - temp, sd[key] - temp
         return sum(sd.values()) + sum(td.values())
