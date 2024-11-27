209fb681afb5e289f7a7c3dde8c1e9b2c5ff1c64
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-27 01:23:48 -0800

    Time: 7 ms (28.45%) | Memory: 17.2 MB (6.13%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,6 +1,6 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        self.m, self.n, self.depth, dq, self.orang, sub = len(grid), len(grid[0]), 0, collections.deque(), 0, 0
+        self.m, self.n, self.depth, dq, self.orang = len(grid), len(grid[0]), 0, collections.deque(), 0
         def search(dq):
             while dq:
                 for m in range(len(dq)):
@@ -10,11 +10,12 @@ class Solution:
                             grid[x+dx][y+dy], self.orang = 3, self.orang - 1
                             dq.append((x+dx,y+dy))
                 self.depth+=1
+            self.depth = max(0, self.depth - 1)
         for i in range(self.m):
             for j in range(self.n):
                 if grid[i][j] == 1: self.orang += 1
                 elif grid[i][j] == 2:
-                    grid[i][j], sub = 3, 1
+                    grid[i][j] = 3
                     dq.append((i, j))
         search(dq)
-        return self.depth - sub if self.orang == 0 else -1
+        return self.depth if self.orang == 0 else -1
