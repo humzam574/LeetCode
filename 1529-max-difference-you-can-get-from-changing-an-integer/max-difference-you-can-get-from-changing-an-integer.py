5b8630c3dbba008029189951365bd96c8b9635dd
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-27 12:32:31 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (12.24%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
@@ -1,30 +1,34 @@
 class Solution:
     def maxDiff(self, num: int) -> int:
-        #take the first num and make that x
         strn = str(num)
         x = "9"
-        for char in strn:
-            if char != "9":
-                x = char
-                break
+        d1, d2 = True, True
         ans = 0
+        #ans = ""
         l = len(strn)
-        add = 9 - int(x)
-        for i in range(l):
-            if strn[i] == x:
-                ans+=(add * 10**(l - i - 1))
         y = "9"
-        floor = 1
-        for j,char in enumerate(strn):
-            if char != "1" and char != "0":
+        add1 = add2 = 0
+        for i,char in enumerate(strn):
+            if char != "1" and char != "0" and d1:
                 y = char
-                floor = int(j != 0)
+                add2 = int(y) - 1 + int(i != 0)
+                d1 = False
+            if char != "9" and d2:
+                x = char
+                add1 = 9 - int(x)
+                d2 = False
+            if not (d1 or d2):
                 break
-        add = int(y) - 1 + floor
-        print(add)
-        for j in range(l):
-            if strn[j] == y:
-                ans+=(add * 10 **(l - j - 1))
-        
-
-        return ans
+        print(add1)
+        print(add2)
+        for i in range(l):
+            #ans = ans + str(add1*(strn[i] == x) + add2*(strn[i] == y))
+            if strn[i] == x:
+                #ans+=str(add1)
+                ans+=(add1 * 10** (l - i - 1))
+            if strn[i] == y:
+                #ans+=str(add2)
+                ans+=(add2 * 10 **(l - i - 1))
+            # else:
+            #     ans+="0"
+        return int(ans)
