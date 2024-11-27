c538b1db6ef13ee10b06189e195ebaa20616b337
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-27 13:24:03 -0800

    Time: 47 ms (40.24%) | Memory: 21.2 MB (75.02%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,25 +1,9 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        #pbbcggttciiippooaais
-        #pcggttciiippooaais
-        #pcttciiippooaais
-        #pcciiippooaais
-        #piiippooaais
-        #pippooaais
-        #piooaais
-        #piaais
-        #piis
-        #ps
         stack = []
-        occur = []
-        for i,c in enumerate(s):
-            stack.append(c)
-            occur.append(1)
-            if len(stack) >= 2 and stack[-2] == stack[-1]: occur[-1]+=occur[-2]
-            if len(occur) != 0 and occur[-1] >= k:
-                for i in range(k):
-                    stack.pop()
-                    occur.pop()
-            #print(stack)
-            #print(occur)
-        return ''.join(stack)
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1] = (char, stack[-1][1] + 1)
+                if stack[-1][1] == k: stack.pop()
+            else: stack.append((char, 1))
+        return ''.join([char * count for char, count in stack])
