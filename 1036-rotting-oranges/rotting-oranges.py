a3aa4fb2db78099ac5f2995e1cb2c93613d1c83e
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-27 01:23:08 -0800

    Time: 7 ms (28.45%) | Memory: 17.5 MB (6.13%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,20 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ans, self.m, self.n, dq = 0, len(grid), len(grid[0]), collections.deque()
+        self.m, self.n, self.depth, dq, self.orang, sub = len(grid), len(grid[0]), 0, collections.deque(), 0, 0
         def search(dq):
-            depth = 0
             while dq:
                 for m in range(len(dq)):
                     x, y = dq.popleft()
                     for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
                         if 0 <= x + dx < self.m and 0 <= y + dy < self.n and grid[x+dx][y+dy] == 1:
-                            grid[x+dx][y+dy] = 3
+                            grid[x+dx][y+dy], self.orang = 3, self.orang - 1
                             dq.append((x+dx,y+dy))
-                depth+=1
-            return max(0, depth-1)
+                self.depth+=1
         for i in range(self.m):
             for j in range(self.n):
-                if grid[i][j] == 2:
-                    grid[i][j] = 3
+                if grid[i][j] == 1: self.orang += 1
+                elif grid[i][j] == 2:
+                    grid[i][j], sub = 3, 1
                     dq.append((i, j))
-        ans = search(dq)
-        for i in range(self.m):
-            for j in range(self.n):
-                if grid[i][j] == 1:
-                    return -1
-        return ans
-
+        search(dq)
+        return self.depth - sub if self.orang == 0 else -1
