0a406bf3717c584e43060fc582dab79109937d53
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-27 13:38:26 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.80%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.py
+++ 1252-break-a-palindrome/break-a-palindrome.py
@@ -1,7 +1,5 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        middle = -1 if len(palindrome) % 2 == 0 else len(palindrome) // 2
-        for i,char in enumerate(palindrome):
-            if char != "a" and i != middle:
-                return palindrome[:i] + "a" + palindrome[i+1:]
+        for i in range(len(palindrome) // 2):
+            if palindrome[i] != "a": return palindrome[:i] + "a" + palindrome[i+1:]
         return "" if len(palindrome) == 1 else palindrome[:len(palindrome) - 1] + "b"
