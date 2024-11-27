0292af9819ff8b289a7b1f59c0731a3e1e2d782c
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-27 12:34:55 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (12.24%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
@@ -1,34 +1,9 @@
 class Solution:
     def maxDiff(self, num: int) -> int:
-        strn = str(num)
-        x = "9"
-        d1, d2 = True, True
-        #ans = 0
-        ans = ""
-        l = len(strn)
-        y = "9"
-        add1 = add2 = 0
+        strn, x, y, l, d1, d2, ans, add1, add2 = str(num), "9", "9", len(str(num)), True, True, "", 0, 0
         for i,char in enumerate(strn):
-            if char != "1" and char != "0" and d1:
-                y = char
-                add2 = int(y) - 1 + int(i != 0)
-                d1 = False
-            if char != "9" and d2:
-                x = char
-                add1 = 9 - int(x)
-                d2 = False
-            if not (d1 or d2):
-                break
-        print(add1)
-        print(add2)
-        for i in range(l):
-            ans = ans + str(add1*(strn[i] == x) + add2*(strn[i] == y))
-            # if strn[i] == x:
-            #     #ans+=str(add1)
-            #     ans+=(add1 * 10** (l - i - 1))
-            # if strn[i] == y:
-            #     #ans+=str(add2)
-            #     ans+=(add2 * 10 **(l - i - 1))
-            # # else:
-            # #     ans+="0"
+            if char != "1" and char != "0" and d1: y, add2, d1 = char, int(char) - 1 + int(i != 0), False
+            if char != "9" and d2: x, add1, d2 = char, 9 - int(char), False
+            if not (d1 or d2): break
+        for i in range(l): ans = ans + str(add1*(strn[i] == x) + add2*(strn[i] == y))
         return int(ans)
