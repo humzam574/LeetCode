f49c8427260448d5a6129f972fbdcd63e0684902
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-27 02:23:30 -0800

    Time: 199 ms (76.19%) | Memory: 55.4 MB (23.86%) - LeetSync

---
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getDistances(self, arr: List[int]) -> List[int]:
+        dict, ans = defaultdict(list), []
+        for i,num in enumerate(arr): dict[num].append(i)
+        for key, value in dict.items():
+            srt, length = [sum(value) - value[0]*len(value)], len(value)
+            for i in range(1, length): srt.append(srt[i-1] - (length - 2*i)*(value[i] - value[i-1]))
+            dict[key] = srt[::-1]
+        for num in arr: ans.append(dict[num].pop())
+        return ans
