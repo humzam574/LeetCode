e91bee6b5f21aa01c344237c21ab981bc774f251
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-29 01:25:08 -0800

    Time: 23 ms (98.54%) | Memory: 18.6 MB (12.11%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        count = Counter(hand)
+        sorted_keys = sorted(count.keys())
+        for key in sorted_keys:
+            if count[key] > 0:
+                start_count = count[key]
+                # Check and form a group starting from `key`
+                for i in range(key, key + groupSize):
+                    if count[i] < start_count:
+                        return False
+                    count[i] -= start_count
+        
+        # Step 5: Return True if all groups are formed successfully
+        return True
