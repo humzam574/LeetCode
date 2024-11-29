359f407abb89451e245b1a9b3a10dcee4abfbd50
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-29 01:45:36 -0800

    Time: 75 ms (74.27%) | Memory: 29 MB (80.59%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.py
+++ 2725-mice-and-cheese/mice-and-cheese.py
@@ -1,12 +1,7 @@
 class Solution:
     def miceAndCheese(self, reward1: List[int], reward2: List[int], k: int) -> int:
         heap = []
-        #for i in range(len(reward1)):
-        rw = [reward1[i] - reward2[i] for i in range(len(reward1))]
-        #print(rw)
-        for r in rw:
-            heapq.heappush(heap, r)  # Push negative value
-            if len(heap) > k:
-                heapq.heappop(heap)  # Remove the smallest (most negative)
-        print(heap)
+        for r in [reward1[i] - reward2[i] for i in range(len(reward1))]:
+            heapq.heappush(heap, r)
+            if len(heap) > k: heapq.heappop(heap)
         return sum(reward2) + sum(heap)
