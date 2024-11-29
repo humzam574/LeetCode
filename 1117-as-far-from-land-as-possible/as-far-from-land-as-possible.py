20be0b884473311f1dd9512471d5f9c3c52c3e40
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-29 02:38:44 -0800

    Time: 127 ms (80.27%) | Memory: 18.1 MB (42.53%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
@@ -1,37 +1,17 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        #create a set with all the 0 coords
-        #start traveling through t = 0,1,2 and popping from the set till it's empty
-        #return # of iterations - 1
-        water = set()
-        land = set()
-        m = len(grid)
-        n = len(grid[0])
-        for i in range(m):
+        n, q, ans = len(grid), collections.deque(), 0
+        for i in range(n):
             for j in range(n):
-                if grid[i][j] == 0:
-                    water.add((i,j))
-                else:
-                    land.add((i,j))
-        
-        t = 0
-        if not land or not water:
-            return -1
-        while water:
-            # print(water)
-            # print(land)
-            # print()
-            #for x,y in land:
-            #temp = set()
-            for x,y in land.copy():
-                #temp = {(x,y)}
-                for dx, dy in [(0,1),(1,0),(-1,0),(0,-1)]:
-                    if 0 <= x+dx < m and 0 <= y+dy < n:
-                        if (x+dx, y+dy) not in water: continue
-                        water.remove((x+dx,y+dy))
-                        land.add((x+dx,y+dy))
-
-            #land = land.union(temp)
-                
-            t+=1
-        return t
+                if grid[i][j] == 1: q.append((i, j))
+        if not q or len(q) == n * n: return -1
+        while q:
+            size, ans = len(q), ans + 1
+            while size:
+                size = size - 1
+                x, y = q.popleft()
+                for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    if 0 <= x + dx < n and 0 <= y + dy < n and grid[x+dx][y+dy] == 0:
+                        grid[x + dx][y + dy] = 1
+                        q.append((x + dx, y + dy))
+        return ans - 1
