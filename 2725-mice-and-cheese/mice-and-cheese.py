0ba1fc085a1ab78646a1d0ecffb617e8ab6ebadd
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-29 01:47:40 -0800

    Time: 44 ms (95.63%) | Memory: 28.8 MB (80.59%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.py
+++ 2725-mice-and-cheese/mice-and-cheese.py
@@ -1,7 +1,3 @@
 class Solution:
     def miceAndCheese(self, reward1: List[int], reward2: List[int], k: int) -> int:
-        heap = []
-        for r in [reward1[i] - reward2[i] for i in range(len(reward1))]:
-            heapq.heappush(heap, r)
-            if len(heap) > k: heapq.heappop(heap)
-        return sum(reward2) + sum(heap)
+        return sum(reward2) + sum(sorted([(reward1[i] - reward2[i]) for i in range(len(reward1))], reverse=True)[:k])
