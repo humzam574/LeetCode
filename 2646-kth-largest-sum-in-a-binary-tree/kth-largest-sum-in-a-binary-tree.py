6708d699c54447fe62e4367280c30650bdac81f9
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-01 01:50:26 -0800

    Time: 191 ms (11.60%) | Memory: 59.9 MB (11.75%) - LeetSync

---
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        self.arr = []
+        def dfs(rt, dep):
+            if not rt: return
+            if dep >= len(self.arr):
+                self.arr.append(-rt.val)
+            else:
+                self.arr[dep] -= rt.val
+            dfs(rt.right, dep+1)
+            dfs(rt.left, dep+1)
+        dfs(root,0)
+        if len(self.arr) < k:
+            return -1
+        heapify(self.arr)
+        for i in range(k-1):
+            heappop(self.arr)
+        return -heappop(self.arr)
