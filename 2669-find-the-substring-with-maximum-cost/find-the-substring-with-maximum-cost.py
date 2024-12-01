fcf073548514d1c6c672bc4aa94aa75412f316a5
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-01 02:18:35 -0800

    Time: 49 ms (99.50%) | Memory: 17.6 MB (45.89%) - LeetSync

--- 2669-find-the-substring-with-maximum-cost/find-the-substring-with-maximum-cost.py
+++ 2669-find-the-substring-with-maximum-cost/find-the-substring-with-maximum-cost.py
@@ -1,24 +1,9 @@
 class Solution:
     def maximumCostSubstring(self, s: str, chars: str, vals: List[int]) -> int:
-        # def kadane(nums):
-        #     res = -inf
-        #     curr = 0
-        #     for num in nums:
-        #         curr = max(num, curr+num)
-        #         res = max(res, curr)
-        #     return res
-        dict = {}
-        for i in range(len(chars)):
-            dict[chars[i]] = vals[i]
-        nums = []
+        costs, ans, curr = [i + 1 for i in range(26)], 0, 0
+        for c, v in zip(chars, vals): costs[ord(c) - 97] = v    
         for char in s:
-            if char in dict:
-                nums.append(dict[char])
-            else:
-                nums.append(ord(char) - ord('a') + 1)
-        ans = -inf
-        curr = 0
-        for num in nums:
-            curr = max(num, curr+num)
-            ans = max(ans, curr)
-        return max(0, ans)
+            curr += costs[ord(char) - 97]
+            if curr < 0: curr = 0
+            if curr > ans: ans = curr
+        return ans
