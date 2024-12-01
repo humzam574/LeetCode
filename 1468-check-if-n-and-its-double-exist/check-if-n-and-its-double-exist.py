ab5988a6a8898dd769637af13cad9479d0ba8d6e
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-30 16:03:20 -0800

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,13 +1,7 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        t = set(arr)
-        nzero = 0
-        for j in arr:
-            if j == 0:
-                nzero+=1
-        if nzero > 1:
-            return True
-        for item in t:
-            if 2*item in t and item != 0:
-                return True
+        mp = defaultdict(int)
+        for i in range(len(arr)):
+            if arr[i]*2 in mp or arr[i]/2 in mp: return True
+            else: mp[arr[i]]+=1
         return False
