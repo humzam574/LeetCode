f76fa76204c590207ded18e56edbb86c4d140e5f
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-01 13:55:47 -0800

    Time: 2 ms (98.67%) | Memory: 17.2 MB (50.17%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.py
@@ -3,10 +3,11 @@ class Solution:
         ans, st, words = "", set(words), sorted(words, key = len, reverse = True)
         for word in words:
             if len(word) < len(ans): break
-            valid = True
-            for i in range(len(word) - 1, 0, -1):
-                if word[:i] not in st:
-                    valid = False
-                    break
-            if valid and (len(word) > len(ans) or (len(word) == len(ans) and word < ans)): ans = word
+            valid = (len(word) > len(ans) or (len(word) == len(ans) and word < ans))
+            if valid:
+                for i in range(len(word) - 1, 0, -1):
+                    if word[:i] not in st:
+                        valid = False
+                        break
+            if valid: ans = word
         return ans
