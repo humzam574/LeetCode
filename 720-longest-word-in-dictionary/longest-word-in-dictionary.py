d6d10db29151c200f5f31fa1a6bfc3c3506a9ef6
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-01 13:50:15 -0800

    Time: 37 ms (56.46%) | Memory: 17.6 MB (44.30%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.py
@@ -1,17 +1,12 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        ans = ""
-        st = set(words)
-        words = sorted(words, key = len, reverse = True)
+        ans, st, words = "", set(words), sorted(words, key = len, reverse = True)
         for word in words:
-            if len(word) < len(ans):
-                break
+            if len(word) < len(ans): break
             valid = True
             for i in range(len(word) - 1, 0, -1):
-                #print(word[:i])
                 if word[:i] not in st:
                     valid = False
                     break
-            if valid and (len(word) > len(ans) or (len(word) == len(ans) and word < ans)):
-                ans = word
+            if valid and (len(word) > len(ans) or (len(word) == len(ans) and word < ans)): ans = word
         return ans
