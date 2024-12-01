be4d6238ae34922f74cd840e434cd3180e6b50fc
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-11-30 18:24:36 -0800

    Time: 4 ms (63.24%) | Memory: 17.8 MB (6.94%) - LeetSync

--- 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.py
+++ 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.py
@@ -5,9 +5,17 @@ class Solution:
             if dep == n:
                 ans.append(curr)
                 return
-            if num + k < 10: bt(num+k,curr,dep+1)
-            if k > 0 and num - k >= 0: bt(num-k,curr,dep+1)
+            if num <= 9-k: bt(num+k,curr,dep+1)
+            if k > 0 and num >= k: bt(num-k,curr,dep+1)
         ans = []
-        for i in range(1,10):
-            bt(i,0,1)
+        #for i in range(1,10): 
+        bt(1,0,1)
+        bt(2,0,1)
+        bt(3,0,1)
+        bt(4,0,1)
+        bt(5,0,1)
+        bt(6,0,1)
+        bt(7,0,1)
+        bt(8,0,1)
+        bt(9,0,1)
         return ans
