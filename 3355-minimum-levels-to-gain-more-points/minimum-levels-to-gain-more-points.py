fe6cf414d70a4512bea81a05fe4826df065999ef
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-02 15:03:30 -0800

    Time: 54 ms (95.41%) | Memory: 21.3 MB (43.71%) - LeetSync

--- 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.py
+++ 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.py
@@ -1,10 +1,10 @@
 class Solution:
     def minimumLevels(self, possible: List[int]) -> int:
-        arr = [-1 if i == 0 else i for i in possible]
-        sm = sum(arr)
-        curr = arr[0]
-        for i in range(1, len(arr)):
+        #arr = [-1 if i == 0 else i for i in possible]
+        sm = 2*sum(possible) - len(possible)
+        curr = 2*possible[0]-1
+        for i in range(1, len(possible)):
             if 2*curr > sm:
                 return i
-            curr+=arr[i]
+            curr+=(2*possible[i]-1)
         return -1
