bf87d758039afdc25eed42999af9ff06e0b01d13
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-02 15:51:07 -0800

    Time: 23 ms (88.00%) | Memory: 25.1 MB (84.30%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximumSetSize(self, nums1, nums2):
-        s1, s2 = set(nums1), set(nums2)
-        n, x = len(nums1), len(s1)
+        s1, s2, n = set(nums1), set(nums2), len(nums1)
+        x = len(s1)
         ans = min(n // 2, x)
         rem, c, t = x - ans, ans, n // 2 + ans
         for i in s2:
