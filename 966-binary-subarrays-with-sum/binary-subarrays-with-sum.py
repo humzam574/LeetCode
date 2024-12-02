4f498476d10717238f75fa3e6d14140922398e26
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-01 23:05:32 -0800

    Time: 7 ms (99.87%) | Memory: 18.1 MB (54.58%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,17 +1,25 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        start, curr, ans = 0, 0, 0
-        for end in range(len(nums)):
-            curr += nums[end]
-            while start <= end and curr > goal:
-                curr -= nums[start]
-                start += 1
-            ans += (end - start + 1)
-        start, curr, goal = 0, 0, goal - 1
-        for end in range(len(nums)):
-            curr += nums[end]
-            while start <= end and curr > goal:
-                curr -= nums[start]
-                start += 1
-            ans += (start - end - 1)
-        return ans
+        arr = [] 
+        zeros = 0
+        for c in nums:
+            if c == 0:
+                zeros += 1
+            else:
+                arr.append(zeros)
+                zeros = 0
+        
+        ones = len(arr)
+        if ones < goal:
+            return 0
+        arr.append(zeros)
+        out = 0
+        
+        if goal == 0:
+            for x in arr:
+                out += (x * (x+1)) // 2
+            return out
+
+        for i in range(ones-goal+1):
+            out += (arr[i]+1)*(arr[i+goal]+1)
+        return out
