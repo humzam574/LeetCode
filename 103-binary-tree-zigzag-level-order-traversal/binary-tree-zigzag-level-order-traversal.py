8b23c2c7d3bd42bcf0723252a41c926742cc271c
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-03 02:10:43 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.07%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -6,13 +6,12 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        self.ans = []
-        def levelOrder(rt, dep):
-            if dep >= len(self.ans): self.ans.append([])
+        self.ans=[]
+        def levelOrder(rt,dep):
+            if dep>=len(self.ans):self.ans.append([])
             self.ans[dep].append(rt.val)
-            if rt.left: levelOrder(rt.left, dep+1)
-            if rt.right: levelOrder(rt.right, dep+1)
-        if root: levelOrder(root, 0)
-        for i in range(1, len(self.ans), 2):
-            self.ans[i] = self.ans[i][::-1]
+            if rt.left:levelOrder(rt.left, dep+1)
+            if rt.right:levelOrder(rt.right, dep+1)
+        if root:levelOrder(root, 0)
+        for i in range(1,len(self.ans),2):self.ans[i]=self.ans[i][::-1]
         return self.ans
