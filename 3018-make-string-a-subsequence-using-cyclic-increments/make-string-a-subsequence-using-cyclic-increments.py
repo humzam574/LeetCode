316092dc2945f4ede18c0213e04f071d98588f57
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-03 16:08:01 -0800

    Time: 63 ms (88.89%) | Memory: 17.6 MB (50.24%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,19 +1,7 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
         p2 = 0
-        def doable(c1, c2):
-            if c1 == c2:
-                return True
-            elif c2 == "a" and c1 == "z":
-                #print()
-                return True
-            elif ord(c2) - 1 == ord(c1):
-                #print("increment at c1 = " + c1)
-                return True
-            return False
         for char in str1:
-            if doable(char, str2[p2]):
-                p2+=1
-            if p2 == len(str2):
-                return True
+            if char==str2[p2] or (char == "z" and str2[p2] == "a") or ord(str2[p2]) -1 == ord(char): p2+=1
+            if p2 == len(str2): return True
         return p2 == len(str2)
