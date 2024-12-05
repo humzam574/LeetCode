b7e0cfdc183050c0875980af26436875a05b08f0
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-04 16:59:20 -0800

    Time: 204 ms (20.16%) | Memory: 33.6 MB (22.16%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -1,22 +1,21 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
         Ltarg, Rtarg, l, Lstart, Rstart = [], [], 0, [], []
+        if start.count("R") != target.count("R") or start.count("L") != target.count("L"): return False
         for i, char in enumerate(target):
             if char == "L": Ltarg.append(i)
             elif char == "R": Rtarg.append(i)
         for r, char in enumerate(start):
             if char == "L": Lstart.append((l, r))
             if char == "R": l = r + 1
-        if len(Lstart) != len(Ltarg): return False
+        #if len(Lstart) != len(Ltarg): return False
         for i,pair in enumerate(Lstart):
             if not (pair[0] <= Ltarg[i] <= pair[1]): return False
         for l in range(len(start) - 1, -1 , -1):
             if start[l] == "R": Rstart.append((l, r))
             elif target[l] == "L": r = l - 1
         Rstart = Rstart[::-1]
-        if len(Rstart) != len(Rtarg): return False
+        #if len(Rstart) != len(Rtarg): return False
         for i, pair in enumerate(Rstart):
             if not (pair[0] <= Rtarg[i] <= pair[1]): return False
         return True
-
-        
