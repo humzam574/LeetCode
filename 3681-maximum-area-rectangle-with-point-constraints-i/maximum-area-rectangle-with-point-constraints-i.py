256cfa23edd07435324e1c314c83505810c30ce9
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-07 23:17:55 -0800

    Time: 100 ms (100.00%) | Memory: 17.6 MB (100.00%) - LeetSync

--- 3681-maximum-area-rectangle-with-point-constraints-i/maximum-area-rectangle-with-point-constraints-i.py
+++ 3681-maximum-area-rectangle-with-point-constraints-i/maximum-area-rectangle-with-point-constraints-i.py
@@ -2,8 +2,10 @@ class Solution:
     def maxRectangleArea(self, points: List[List[int]]) -> int:
         def possible(xl, xh, yl, yh):
             d = -4
-            for pt in points: d += (xl <= pt[0] <= xh and yl <= pt[1] <= yh)
-            return not bool(d)
+            for pt in points:
+                d += (xl <= pt[0] <= xh and yl <= pt[1] <= yh)
+                if d>0: return False
+            return d==0
         ans, memo, ln = -1, {}, len(points)
         for i in range(ln):
             for j in range(i + 1, ln):
@@ -11,6 +13,5 @@ class Solution:
                     for l in range(k + 1, ln):
                         if (i, j, k, l) not in memo:
                             xl, xh, xst, yst, yl, yh = min(points[i][0], points[j][0], points[k][0], points[l][0]), max(points[i][0], points[j][0], points[k][0], points[l][0]), set([points[i][0], points[j][0], points[k][0], points[l][0]]), set([points[i][1], points[j][1], points[k][1], points[l][1]]), min(points[i][1], points[j][1], points[k][1], points[l][1]), max(points[i][1], points[j][1], points[k][1], points[l][1])
-                            if len(xst) == 2 and len(yst) == 2 and possible(xl, xh, yl, yh): ans = max(ans, (xh - xl)*(yh - yl))
-                            memo[(i,j,k,l)] = True
+                            ans, memo[(i,j,k,l)] = max(ans, (xh - xl)*(yh - yl)) if len(xst) == len(yst) == 2 and possible(xl, xh, yl, yh) else ans, True
         return -1 if ans == 0 else ans
