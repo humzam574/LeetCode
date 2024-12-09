af6c31c223abb15c24839b9fe2d02a4bf1b3f93a
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-08 17:31:39 -0800

    Time: 47 ms (57.87%) | Memory: 55.5 MB (16.60%) - LeetSync

--- 3427-special-array-ii/special-array-ii.py
+++ 3427-special-array-ii/special-array-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
         dict, v = defaultdict(int), 0
-        for i in range(1, len(nums)): v, dict[i] = v + int(nums[i]%2 == nums[i-1]%2), v + int(nums[i]%2 == nums[i-1]%2)
+        for i in range(1, len(nums)):
+            if nums[i] % 2 == nums[i-1] % 2: v+=1
+            dict[i] = v
         return [dict[query[0]] == dict[query[1]] for query in queries]
