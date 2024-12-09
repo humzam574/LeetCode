5cf4c352485dd6ac24ca4aa48852d36148f69f7f
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-08 17:29:28 -0800

    Time: 55 ms (50.93%) | Memory: 55.4 MB (16.60%) - LeetSync

--- 3427-special-array-ii/special-array-ii.py
+++ 3427-special-array-ii/special-array-ii.py
@@ -1,27 +1,7 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        #find a way to process the array and then redo it
-        #[3, 4, 1, 2, 6]
-        #[1, 0, 1, 0, 0]
-        #[2, 2, 2, 1, 1]
-    
-        #[4, 3, 1, 6, 7, 6]
-        #[0, 1, 1, 0, 1, 0]
-        #[2, 1, 1, 2, 2, 2]
-
-        #the check will be that all the insides are 2 and the edges have a parity difference
-        ans = []
-        nums = [i % 2 for i in nums]
-        dict = {}
-        dict[0] = 0
-        v = 0
-        #print(nums)
+        dict, v = defaultdict(int), 0
         for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                v+=1
+            if nums[i]%2 == nums[i-1]%2: v+=1
             dict[i] = v
-        #print(dict)
-        for query in queries:
-            #print(query)
-            ans.append(dict[query[0]] == dict[query[1]])
-        return ans
+        return [dict[query[0]] == dict[query[1]] for query in queries]
