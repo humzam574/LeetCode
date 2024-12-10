5788221d32825d6e110fb251793518b25b374f64
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 02:35:12 -0800

    Time: 11 ms (88.59%) | Memory: 17.4 MB (27.76%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
@@ -1,15 +1,14 @@
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
         def check(w1, w2, thresh):
-            if len(w1) != len(w2): return False
             for i in range(len(w1)):
-                if w1[i] != w2[i]: thresh -= 1
-                if thresh == 0: return False
+                thresh -= (w1[i] != w2[i])
+                if not thresh: return False
             return True
         ans = []
         for query in queries:
             for word in dictionary:
-                if check(query,word, 3):
+                if len(query) == len(word) and check(query,word, 3):
                     ans.append(query)
                     break
         return ans
