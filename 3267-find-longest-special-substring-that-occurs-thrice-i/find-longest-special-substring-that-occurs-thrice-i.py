c1016e2cb25102971eaecf5ccdb9bc00e06336b7
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-09 19:01:52 -0800

    Time: 16 ms (57.57%) | Memory: 17.1 MB (17.44%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,36 +1,19 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
-        def search():
-            l, r = 0, 1
-            val = 0
-            while r < len(s):
-                if s[r] != s[l]:
-                    l = r
-                else:
-                    r+=1
-                    val = max(val, r-l)
-            return val
-        ans = -1
         #solution doesnt have to be super efficient, maybe even O(n^3)
         #maybe make a hashmap counter
         #use two pointer to get the largest length substr, and get num occurrences
         #then find the num occurrences for substr of length - 1
         #and repeat until you hit 3
-        
-        high = search()
+        l, r, high = 0, 1, 0
+        while r < len(s):
+            if s[r] != s[l]: l = r
+            else: r, high = r + 1, max(high, r - l + 1)
         for i in range(high, 0, -1):
-            dict = defaultdict(int)
-            l = 0
-            r = i
-            #print(i)
-            possible = (len(set(s[l:r])) == 1)
+            dict, l, r = defaultdict(int), 0, i
             while r <= len(s):
-                #print(dict)
                 if (len(set(s[l:r])) == 1):
-                    dict[s[l]]+=1
-                    if dict[s[l]] == 3:
-                        return i
-                l+=1
-                r+=1
-            
+                    if dict[s[l]] == 2: return i
+                    dict[s[l]]+=1   
+                l, r = l + 1, r + 1
         return -1
