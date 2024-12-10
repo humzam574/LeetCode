612bbca8ec1aead9dc83e738f8a04505b1a2ff6c
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 15:07:27 -0800

    Time: 45 ms (82.86%) | Memory: 17.4 MB (55.64%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,18 +1,11 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        #penalty is number if y's after and during
-        #plus number of n's before
-        #use prefix and suffix
-        prefix = [0] * (len(customers)+1)
-        for i in range(len(customers)):
-            prefix[i+1] = prefix[i] + int(customers[i] == "N")
-        suffix = [0] * (len(customers)+1)
-        suffix[-1] = 0
-        for i in range(len(customers) - 1, -1, -1):
-            suffix[i] = suffix[i + 1] + int(customers[i] == "Y")
-        low, idx = inf, -1
-        for i in range(len(suffix) - 1, -1, -1):
-            if prefix[i] + suffix[i] <= low:
-                low = prefix[i] + suffix[i]
-                idx = i
-        return idx
+        max_score = score = 0
+        best_hour = -1
+
+        for i, c in enumerate(customers):
+            score += 1 if c == 'Y' else -1
+            if score > max_score:
+                max_score, best_hour = score, i
+                
+        return best_hour + 1
