13325df13f177db9b452f104bea074e17d191c63
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 01:41:38 -0800

    Time: 31 ms (91.04%) | Memory: 20 MB (22.51%) - LeetSync

--- 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
@@ -1,10 +1,8 @@
 class Solution:
-    def maxSum(self, grid: List[List[int]]) -> int:
-        ans = -inf
-        for r in range(2, len(grid)):
-            curr = sum(grid[r-2][:3]) + grid[r-1][1] + sum(grid[r][:3])
-            ans = max(curr, ans)
-            for c in range(3, len(grid[0])):
-                curr = curr - grid[r-2][c-3] - grid[r-1][c-2] - grid[r][c-3] + grid[r-2][c] + grid[r-1][c-1] + grid[r][c]
-                ans = max(curr, ans)
-        return ans
+    def maxSum(self, g: List[List[int]]) -> int:
+        a=-inf
+        for i in range(1,len(g)-1):
+            for j in range(1,len(g[0])-1):
+                s=g[i][j]+g[i-1][j-1]+g[i-1][j]+g[i-1][j+1]+g[i+1][j-1]+g[i+1][j]+g[i+1][j+1]
+                if s>a:a=s
+        return a
