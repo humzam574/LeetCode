d89c4d813f50f8accd8dcc80198c2aff97a5d61b
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 14:11:12 -0800

    Time: 39 ms (78.46%) | Memory: 30.7 MB (41.26%) - LeetSync

--- 2878-apply-operations-to-make-all-array-elements-equal-to-zero/apply-operations-to-make-all-array-elements-equal-to-zero.py
+++ 2878-apply-operations-to-make-all-array-elements-equal-to-zero/apply-operations-to-make-all-array-elements-equal-to-zero.py
@@ -3,7 +3,5 @@ class Solution:
         curr = 0
         for i, num in enumerate(nums):
             if curr > num: return False
-            nums[i] = num - curr
-            curr = num
-            if i >= k - 1: curr -= nums[i - k + 1]
+            nums[i], curr = num - curr, num if i < k - 1 else num - nums[i - k + 1]
         return curr == 0
