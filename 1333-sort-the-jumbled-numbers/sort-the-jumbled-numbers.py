f3f6baa50381ae90cda01ec3dbf358427ce64e17
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 23:11:03 -0800

    Time: 698 ms (28.16%) | Memory: 26 MB (39.90%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,12 +1,5 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        def conv(num):
-            ans = ""
-            for char in str(num):
-                ans+=str(mapping[int(char)])
-            return int(ans)
         ans = []
-        for num in nums:
-            ans.append((num, conv(num)))
-        ans.sort(key = lambda x : x[1])
-        return [a[0] for a in ans]
+        for num in nums: ans.append((num, int("".join(str(mapping[int(char)]) for char in str(num)))))
+        return [a[0] for a in sorted(ans, key = lambda x : x[1])]
