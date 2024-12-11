1450416ba15a26a9f12df9f698d9e7cfa473569b
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 02:00:42 -0800

    Time: 13 ms (37.14%) | Memory: 19.9 MB (14.71%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -1,12 +1,5 @@
 class Solution:
-    def findMinDifference(self, timePoints: List[str]) -> int:
-        tp = [int(s[:2])*60 + int(s[3:]) for s in timePoints]
-        tp.sort()
-        print(tp)
-        low = 1440
-        high = 0
-        for i in range(1, len(tp)):
-            low = min(low, abs(tp[i] - tp[i-1]))
-            high = max(high, abs(tp[i] - tp[i-1]))
-        high = max(high, abs(tp[-1] - tp[0]))
-        return min(low, 1440 - high)
+    def findMinDifference(self, tp: List[str]) -> int:
+        tp, low, high = sorted([int(s[:2])*60 + int(s[3:]) for s in tp]), 1440, 0
+        for i in range(1, len(tp)): low, high = min(low, abs(tp[i] - tp[i-1])), max(high, abs(tp[i] - tp[i-1]))
+        return min(low, 1440 - max(high, abs(tp[-1] - tp[0])))
