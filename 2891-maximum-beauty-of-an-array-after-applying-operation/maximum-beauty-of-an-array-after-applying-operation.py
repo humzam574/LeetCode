385dd085dd2472db6724222cb5c3aa2a4d585147
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 22:45:47 -0800

    Time: 240 ms (41.88%) | Memory: 28.1 MB (39.52%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,21 +1,8 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-        #you can do the operation an infinite number of times
-        #find the most numbers that are within k of each other
-        #ex 1: [2,6] + [4,8] + [-1,3] + [0, 4]
-        #becomes 3
-        events = []
-        # if k == 0:
-        #     for num in nums:
-        #         events.append((num, 1))
-        # else:
-        for num in nums:
-            events.append((num - k, 1))
-            events.append((num + k + 1, -1))
-        events.sort()
-        ans = 0
-        curr = 0
-        for rand, rang in events:
-            curr += rang
-            ans = max(curr, ans)
+        nums.sort()
+        l, r, ans = 0, 0, 1
+        while r < len(nums):
+            if nums[r] <= 2*k+nums[l]: r, ans = r + 1, max(ans, r - l + 1)
+            else: l+=1
         return ans
