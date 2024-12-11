85d4ce5d89979f52668d3088fa57f579ed629198
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 23:13:31 -0800

    Time: 316 ms (96.76%) | Memory: 23.1 MB (79.81%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,5 +1,4 @@
 class Solution:
-    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        mapping = {str(i): str(j) for i, j in enumerate(mapping)}
-        def sort_map(a: int) -> int: return int("".join(map(mapping.get, str(a))))
-        return sorted(nums, key=sort_map)
+    def sortJumbled(self,m,n):
+        m={str(i):str(j) for i,j in enumerate(m)}
+        return sorted(n,key=lambda a:int("".join(map(m.get,str(a)))))
