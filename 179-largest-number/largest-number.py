f1ca14504d3187b73b16b016936a8ccda16caa2e
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 23:45:02 -0800

    Time: 2 ms (56.30%) | Memory: 17.3 MB (6.70%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,7 +1,2 @@
 class Solution:
-    def largestNumber(self, nums: List[int]) -> str:
-        nums = sorted(nums,key=lambda x: x / (10 ** len(str(x)) - 1 ), reverse=True)
-        str_nums = [str(num) for num in nums]
-        res = ''.join(str_nums)
-        res = str(int(res))
-        return res
+    def largestNumber(self,s):return str(int(''.join([str(n)for n in sorted(s,key=lambda x:x/(10**len(str(x))-1),reverse=True)])))
