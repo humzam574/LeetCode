8e06587fc7836d391ee1971779a3ae7628b1743f
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 01:48:33 -0800

    Time: 75 ms (52.50%) | Memory: 26.8 MB (9.17%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.py
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -1,18 +1,12 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
-        #use monotonic stacc
-        #create a stack and as you start iterating add numbers which are strictly decreasing
-        stacc = [(nums[0], 0)]
+        stacc, ans = [(nums[0], 0)], 0
         for i,num in enumerate(nums[1:]):
             if stacc[-1][0] > num:
                 stacc.append((num,i+1))
-        #print(stacc)
-        ans = 0
         for i in range(len(nums) - 1, 0, -1):
             while stacc and nums[i] >= stacc[-1][0]:
                 ans = max(ans, i - stacc[-1][-1])
                 stacc.pop()
-            if not stacc:
-                return ans
-            
+            if not stacc: break
         return ans
