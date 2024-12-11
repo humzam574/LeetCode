fd19fb83d5c99dc6874d15ad236be6b5037cd928
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-10 23:31:34 -0800

    Time: 125 ms (93.78%) | Memory: 17.5 MB (62.64%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -2,6 +2,6 @@ class Solution:
     def minimumDeletions(self, s: str) -> int:
         ans, n = 0, 0
         for c in s:
-            if c == 'b': n+=1
+            if c == 'b': n += 1
             elif n != 0: ans, n = ans + 1, n - 1
         return ans
