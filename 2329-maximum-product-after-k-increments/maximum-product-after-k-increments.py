3d62613445720762fd13988917f56a4b66238b34
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 21:19:56 -0800

    Time: 402 ms (65.98%) | Memory: 27 MB (52.87%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.py
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.py
@@ -2,5 +2,5 @@ class Solution:
     def maximumProduct(self, heap: List[int], k: int) -> int:
         heapify(heap); ans = 1
         for i in range(k): heappush(heap, heappop(heap)+1)
-        for item in heap: ans = (ans * item) % (10**9 + 7)
-        return ans# % (10**9 + 7)
+        for item in heap: ans = ans * item % (10**9 + 7)
+        return ans
