8da6c20f4535a999322cf2c60dbb54e0242bc3fe
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 21:42:55 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.86%) - LeetSync

--- 2033-the-number-of-full-rounds-you-have-played/the-number-of-full-rounds-you-have-played.py
+++ 2033-the-number-of-full-rounds-you-have-played/the-number-of-full-rounds-you-have-played.py
@@ -1,7 +1,5 @@
 class Solution:
     def numberOfRounds(self, pit: str, pot: str) -> int:
         it, ot = int(pit[:2])*60 + int(pit[3:]), int(pot[:2])*60 + int(pot[3:])
-        if pot < pit: ot+=1440
-        if it % 15 != 0: it = it - (it % 15) + 15
-        if ot % 15 != 0: ot -= (ot % 15)
+        ot, it = ot + (pot<pit)*1440 - (ot%15), it + int(bool(it % 15))*(15 - it%15)
         return 0 if (pit < pot and it > ot) or it == ot else (ot - it)//15
