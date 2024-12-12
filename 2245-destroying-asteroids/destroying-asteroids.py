780b1f27bd922fa7cf1ef29a765af28741b6cc0c
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 20:51:52 -0800

    Time: 68 ms (66.85%) | Memory: 31.1 MB (6.12%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -0,0 +1,7 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for a in asteroids:
+            if mass < a: return False
+            mass+=a
+        return True
