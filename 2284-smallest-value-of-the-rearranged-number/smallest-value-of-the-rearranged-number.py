f274296eb96f5a6b8cef958ee49bc8b127b8fac5
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 20:50:20 -0800

    Time: 3 ms (19.57%) | Memory: 17.4 MB (8.73%) - LeetSync

--- 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
@@ -1,18 +1,6 @@
 class Solution:
     def smallestNumber(self, num: int) -> int:
-        #if negative, chop off the negative and sort by decreasing order and ur done
-        #if positive, sort by increasing order and put all the zeros after the first digit
-        if num == 0: return 0
-        if num < 0:
-            s = list(str(num)[1:])
-            s.sort(reverse = True)
-            return int("-" + ''.join(s))
-        s = list(str(num))
-        z = -1
-        s.sort()
-        for i,d in enumerate(s):
-            if d != "0":
-                z = i
-                break
-        val = s[z] + ("0"*z) + ''.join(s[z+1:])
-        return int(val)
+        if num < 0: return -int(''.join(sorted(list(str(num)[1:]), reverse = True)))
+        s = sorted(list(str(num)))
+        z = s.count('0')
+        return 0 if num == 0 else int(s[z] + ("0"*z) + ''.join(s[z+1:]))
