e3b616e7d359a8eed693310cafd5926fc61a3d51
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 18:49:54 -0800

    Time: 275 ms (76.82%) | Memory: 20.1 MB (32.95%) - LeetSync

--- 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.py
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.py
@@ -1,9 +1,7 @@
 class Solution:
     def lastNonEmptyString(self, s: str) -> str:
-        cnt = Counter(s)
-        idx, ans, high = {}, [], max(cnt.values())
-        for i,c in enumerate(s): idx[c] = i
-        for k, v in idx.items():
-            if cnt[k] == high:
-                ans.append((v, k))
-        return ''.join(a[1] for a in sorted(ans, key = lambda x: x[0]))
+        cnt, ans = Counter(s), [] 
+        mx = max(cnt.values())
+        for c in s[::-1]:
+            if mx == cnt[c]: ans.append(c); cnt[c] -= 1
+        return ''.join(ans[::-1])
