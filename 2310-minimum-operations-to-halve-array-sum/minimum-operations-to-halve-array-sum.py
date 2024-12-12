ac72506a73f34167bd59629ca59ff8ed83eef79c
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 21:08:08 -0800

    Time: 246 ms (71.01%) | Memory: 30.6 MB (57.62%) - LeetSync

--- 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.py
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.py
@@ -3,7 +3,7 @@ class Solution:
         target, curr, heap, ans = sum(heap) / 2, 0, [-n for n in heap], 0
         heapify(heap)
         while curr < target:
-            v = -1 * heappop(heap) / 2
-            curr, ans = curr + v, ans + 1
-            heappush(heap, -v)
+            v = heappop(heap) / 2
+            curr, ans = curr - v, ans + 1
+            heappush(heap, v)
         return ans
