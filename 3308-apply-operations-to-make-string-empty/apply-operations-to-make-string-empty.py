9ca25fd0382a1eff220b91f2090c37c4c9070769
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 18:45:05 -0800

    Time: 435 ms (36.42%) | Memory: 41.1 MB (5.20%) - LeetSync

--- 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.py
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.py
@@ -1,6 +1,6 @@
 class Solution:
     def lastNonEmptyString(self, s: str) -> str:
-        idx, high, ans = defaultdict(list), sorted(list(Counter(s).values()))[-1], []
+        idx, ans, high = defaultdict(list), [], max(Counter(s).values())
         for i,c in enumerate(s): idx[c].append(i)
         for key, value in idx.items(): ans.append((value[-1], key)) if len(value) == high else None
         return ''.join(a[1] for a in sorted(ans, key = lambda x: x[0]))
