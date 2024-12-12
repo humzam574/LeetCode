a441e2116b8581f00d9935c7fe57293e695394f4
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 21:12:19 -0800

    Time: 240 ms (83.58%) | Memory: 30.6 MB (51.20%) - LeetSync

--- 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.py
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.py
@@ -1,9 +1,6 @@
 class Solution:
     def halveArray(self, heap: List[int]) -> int:
-        target, curr, heap, ans = sum(heap) / 2, 0, [-n for n in heap], 0
-        heapify(heap)
+        target, curr, heap, ans = sum(heap) / 2, 0, [-n for n in heap], 0; heapify(heap)
         while True:
-            v = heappop(heap) / 2
-            curr, ans = curr - v, ans + 1
+            v = heappop(heap) / 2; curr, ans = curr - v, ans + 1; heappush(heap, v)
             if curr >= target: return ans
-            heappush(heap, v)
