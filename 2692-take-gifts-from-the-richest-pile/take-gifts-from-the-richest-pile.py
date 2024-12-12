2e624ee00743e617c14226df46bb6758ff04603c
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-11 16:04:52 -0800

    Time: 7 ms (49.64%) | Memory: 17.5 MB (8.46%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -1,16 +1,6 @@
-import heapq
-import math
-
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
         heap = [-v for v in gifts]
-        heapq.heapify(heap)
-
-        while k > 0:
-            v = heapq.heappop(heap)
-            v = -v
-            v = math.floor(v ** 0.5)
-            heapq.heappush(heap, -v)
-            k -= 1
-
-        return -1 * sum(heap)
+        heapify(heap)
+        for i in range(k): heapq.heappush(heap, -math.floor((-heapq.heappop(heap)) ** 0.5))
+        return -sum(heap)
