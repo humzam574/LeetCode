40cae15b6573b4c71ecb69cca854f7f4861e6e9b
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-15 16:23:58 -0800

    Time: 7 ms (6.11%) | Memory: 17.2 MB (21.02%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,23 +1,9 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        ans = 0
-        start = 0
-        mult = 1
-        fl = 2**31-1
-        while start < len(s) and s[start] == " ":
-            start+=1
-        #print(start)
-        if len(s) > start:
-            if s[start] == "-":
-                mult = -1
-                start+=1
-            elif s[start] == "+":
-                start+=1
-            #print(start)
-            while start < len(s) and s[start].isdigit():
-                if ans > (fl - int(s[start]))//10:
-                    return (fl+1)*mult if mult < 0 else fl
-                ans = ans*10
-                ans+=int(s[start])
-                start+=1
+        ans, start, fl = 0, s.index(next(c for c in s if c != " ")) if s.strip() else len(s), 2**31-1
+        if len(s) <= start: return 0
+        mult, start = 1 - 2*int(s[start] == "-"), start + int(s[start] == "+" or s[start] == "-")
+        while start < len(s) and s[start].isdigit():
+            if ans > (fl - int(s[start]))//10: return (fl+1)*mult if mult < 0 else fl
+            ans, start = ans*10 + int(s[start]), start + 1
         return ans*mult
