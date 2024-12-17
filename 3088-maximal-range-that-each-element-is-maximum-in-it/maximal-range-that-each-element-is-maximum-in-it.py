81a86f8b7b09cf9dee92c47088cc2719904e24ea
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-17 15:34:35 -0800

    Time: 345 ms (9.71%) | Memory: 45.7 MB (11.01%) - LeetSync

--- 3088-maximal-range-that-each-element-is-maximum-in-it/maximal-range-that-each-element-is-maximum-in-it.py
+++ 3088-maximal-range-that-each-element-is-maximum-in-it/maximal-range-that-each-element-is-maximum-in-it.py
@@ -1,27 +1,27 @@
 class Solution:
     def maximumLengthOfRanges(self, nums: List[int]) -> List[int]:
-        ans = [0] * len(nums)
+        l = len(nums)
+        a1 = [0] * l
         stack = []
         for i, n in enumerate(nums):
             while stack and n > stack[-1][0]:
                 sn, si = stack.pop()
-                ans[si] = i - si
+                a1[si] = i - si
             stack.append((n, i))
         for i,n in stack:
-            ans[n] = len(ans) - n
+            a1[n] = l - n
         stack = []
-        a2 = [0] * len(nums)
-        nums = nums[::-1]
-        for i, n in enumerate(nums):
+        a2 = [0] * l
+        l -= 1
+        for i in range(l, -1, -1):
+            n, i = nums[i], l - i
             while stack and n > stack[-1][0]:
                 sn, si = stack.pop()
-                if i - si > 1:
-                    a2[si] = i - si - 1
+                a2[si] = max(0, i - si - 1)
             stack.append((n,i))
         for i,n in stack:
-            a2[n] += (len(ans) - n - 1)
-        a2 = a2[::-1]
-        res = []
-        for i in range(len(a2)):
-            res.append(ans[i]+a2[i])
-        return res
+            a2[n] += (l - n)
+        ans = []
+        for i in range(l + 1):
+            ans.append(a1[i]+a2[l-i])
+        return ans
