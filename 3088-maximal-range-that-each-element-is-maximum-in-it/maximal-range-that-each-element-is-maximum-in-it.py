3388b523d26b30ab5dd6f07bb39e83d91606a70e
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-17 15:45:06 -0800

    Time: 107 ms (99.28%) | Memory: 36 MB (85.94%) - LeetSync

--- 3088-maximal-range-that-each-element-is-maximum-in-it/maximal-range-that-each-element-is-maximum-in-it.py
+++ 3088-maximal-range-that-each-element-is-maximum-in-it/maximal-range-that-each-element-is-maximum-in-it.py
@@ -1,10 +1,7 @@
 class Solution:
     def maximumLengthOfRanges(self, nums: List[int]) -> List[int]:
-        ans = [0]*len(nums)
-        nums.append(inf)
-        stack = [-1]
+        ans, stack, nums = [0]*len(nums), [-1], nums + [10**9]
         for i,n in enumerate(nums):
-            while nums[stack[-1]] < n:
-                ans[stack.pop()] = i-stack[-2]-1
+            while nums[stack[-1]] < n: ans[stack.pop()] = i-stack[-2]-1
             stack.append(i)
         return ans
