c3f0e43856b380357123ac66cf70ac7cc12ef10b
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-17 15:42:11 -0800

    Time: 159 ms (77.25%) | Memory: 41.7 MB (28.41%) - LeetSync

--- 3088-maximal-range-that-each-element-is-maximum-in-it/maximal-range-that-each-element-is-maximum-in-it.py
+++ 3088-maximal-range-that-each-element-is-maximum-in-it/maximal-range-that-each-element-is-maximum-in-it.py
@@ -1,22 +1,13 @@
 class Solution:
     def maximumLengthOfRanges(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        a1, a2 = [0] * l, [0] * l
-        s1, s2 = [], []
-        for i in range(l):
-            n = nums[i]
-            while s1 and n > s1[-1][0]:
-                sn, si = s1.pop()
-                a1[si] = i - si
-            s1.append((n, i))
-            n = nums[l - i - 1]
-            while s2 and n > s2[-1][0]:
-                sn, si = s2.pop()
-                a2[si] = i - si - 1
-            s2.append((n,i))
-        for i,n in s1:
-            a1[n] += l - n
-        l -= 1
-        for i,n in s2:
-            a2[n] += (l - n)
-        return [a1[i]+a2[l-i] for i in range(l+1)]
+        stack = [] #(idx, val), decresing order
+        n = len(nums)
+        ans = [0] * n 
+        nums.append(10**9)
+        for i, num in enumerate(nums):
+            while stack and stack[-1][1] < num:
+                idx, val = stack.pop()
+                left = -1 if not stack else stack[-1][0]
+                ans[idx] = i - left - 1
+            stack.append((i, num))
+        return ans
