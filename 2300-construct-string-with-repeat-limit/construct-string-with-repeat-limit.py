077c4ba0057f8bf17275674634f4eb985b5413f0
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-16 17:06:54 -0800

    Time: 159 ms (94.20%) | Memory: 18.5 MB (64.34%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -1,17 +1,15 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        heap, ans = [(-ord(k), v) for k, v in Counter(s).items()], ""
+        heap, ans = [[-ord(k), v] for k, v in Counter(s).items()], ""
         heapify(heap)
         while heap:
             k, v = heappop(heap)
-            if v <= repeatLimit:
-                ans+=(chr(-k) * v)
+            if v <= repeatLimit: ans+=(chr(-k) * v)
             else:
                 ans+=(chr(-k) * repeatLimit)
                 if not heap: break
-                ok, ov = heappop(heap)
-                ans+=chr(-ok)
-                if ov > 1:
-                    heappush(heap, (ok, ov - 1))
-                heappush(heap, (k, v - repeatLimit))
+                ans+=chr(-heap[0][0])
+                if heap[0][1] == 1: heappop(heap)
+                else: heap[0][1] -= 1
+                heappush(heap, [k, v - repeatLimit])
         return ans
