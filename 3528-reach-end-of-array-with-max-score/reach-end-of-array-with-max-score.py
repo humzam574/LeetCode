043e2aa6c12e2ecdeaf6b69dab233748022868d8
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-17 18:38:13 -0800

    Time: 59 ms (60.00%) | Memory: 32.3 MB (8.73%) - LeetSync

--- 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.py
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMaximumScore(self, nums: List[int]) -> int:
-        curr, ans = 0, 0
-        for i in range(1, len(nums)):
-            if nums[i] > nums[curr]: ans, curr = ans + (i-curr)*nums[curr], i
-        return ans + (len(nums)-1-curr)*nums[curr]
+        ans, curr, idx, n = 0, nums[0], 0, len(nums)
+        for i in range(1, n):
+            if nums[i] > curr: ans, curr, idx = ans + (i - idx) * curr, nums[i], i
+        return ans + (n - 1 - idx) * curr
