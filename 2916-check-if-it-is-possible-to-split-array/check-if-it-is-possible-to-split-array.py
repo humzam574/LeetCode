3183c65a8f3532d23a43dc572e0836e132e43458
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-17 19:27:07 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.34%) - LeetSync

--- 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.py
+++ 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.py
@@ -1,3 +1,6 @@
 class Solution:
     def canSplitArray(self,nums,m):
-        return True if len(nums) < 3 else any(nums[i]+nums[i+1]>=m for i in range(len(nums)-1))
+        if len(nums) < 3: return True
+        for i in range(1, len(nums)): 
+            if nums[i-1] + nums[i] >= m: return True
+        return False
