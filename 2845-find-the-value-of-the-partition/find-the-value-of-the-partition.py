25f702290cbe9da7c5f71108833f6e2a7f1ff897
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-17 20:08:59 -0800

    Time: 68 ms (71.24%) | Memory: 29.4 MB (10.78%) - LeetSync

--- 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.py
+++ 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.py
@@ -1,4 +1,4 @@
 class Solution:
-    def findValueOfPartition(self, nums: List[int]) -> int:
+    def findValueOfPartition(self, nums):
         nums.sort()
         return min(nums[i+1] - nums[i] for i in range(len(nums) - 1))
