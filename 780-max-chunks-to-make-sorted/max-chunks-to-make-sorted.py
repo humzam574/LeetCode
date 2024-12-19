142c71d7719476a754a88d34c0a4ede163a3033c
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-18 22:12:28 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (14.45%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
@@ -1,13 +1,5 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        #backtracking
-        #take every split and see if you can sort and recombine
-        #if so throw it into a max variable
-
         prfx, exp, curr = 0, 0, 0
-        for i, num in enumerate(arr):
-            prfx += num
-            exp += i
-            if prfx == exp:
-                curr += 1
+        for i, num in enumerate(arr): prfx, exp, curr = prfx + num, exp + i, curr + (prfx + num == exp + i)
         return curr
