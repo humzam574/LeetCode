4dbe3e0ec9d7b50e6faa455e353f79cbe882c476
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-20 00:21:18 -0800

    Time: 18 ms (73.44%) | Memory: 22.8 MB (5.99%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
@@ -6,12 +6,12 @@
 #         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def traverse(leftNode, rightNode, level):
+        def traverse(leftNode, rightNode, odd):
             if leftNode is None or rightNode is None:
                 return
-            if (level % 2): leftNode.val, rightNode.val = rightNode.val, leftNode.val
-            traverse(leftNode.left, rightNode.right,  level+1)
-            traverse(leftNode.right, rightNode.left, level+1)
+            if (odd): leftNode.val, rightNode.val = rightNode.val, leftNode.val
+            traverse(leftNode.left, rightNode.right,  not odd)
+            traverse(leftNode.right, rightNode.left, not odd)
         
-        traverse(root.left, root.right, 1)
+        traverse(root.left, root.right, True)
         return root
