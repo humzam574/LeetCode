4396031690bf6db39abab090646f5e9123c8ca68
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-23 00:51:42 -0800

    Time: 155 ms (60.13%) | Memory: 50.7 MB (10.46%) - LeetSync

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
@@ -6,30 +6,37 @@
 #         self.right = right
 class Solution:
     def minimumOperations(self, root: Optional[TreeNode]) -> int:
-        self.levs = []
-        def los(lev, curr):
-            if curr:
-                if lev >= len(self.levs):
-                    self.levs.append([])
-                self.levs[lev].append(curr.val)
-                los(lev+1, curr.left)
-                los(lev+1, curr.right)
-        los(0, root)
-        #print(self.levs)
-        ans = 0
-        for lev in self.levs:
+        def minswap(lev):
             dict = {num : idx for idx, num in enumerate(lev)}
-            #print(dict)
             srt = sorted(lev)
-            #print(ans)
-            #seen = set()
+            swap = 0
             for i, num in enumerate(srt):
-                if i != dict[num]:# and num not in seen:
+                if i != dict[num]:
                     swp = dict[num]
                     dict[num], dict[lev[i]] = dict[lev[i]], dict[num]
                     lev[i], lev[swp] = lev[swp], lev[i]
-                    ans+=1
-                #print(dict)
-            #print()
+                    swap += 1
+            return swap
+        # self.levs = []
+        # def los(lev, curr):
+        #     if curr:
+        #         if lev >= len(self.levs):
+        #             self.levs.append([])
+        #         self.levs[lev].append(curr.val)
+        #         los(lev+1, curr.left)
+        #         los(lev+1, curr.right)
+        # los(0, root)
+        # ans = 0
+        # for lev in self.levs:
+        dq = deque()
+        dq.append(root)
+        ans = 0
+        while dq:
+            lev = []
+            for i in range(len(dq)):
+                if dq[0].left: dq.append(dq[0].left)
+                if dq[0].right: dq.append(dq[0].right)
+                lev.append(dq.popleft().val)
+            ans += minswap(lev)
         return ans
                     
