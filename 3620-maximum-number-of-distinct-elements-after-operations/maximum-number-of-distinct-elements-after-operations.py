1348e7179d261a34795f5fea168402388119bd57
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-24 11:51:29 -0800

    Time: 708 ms (80.16%) | Memory: 31.3 MB (99.22%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
@@ -1,16 +1,10 @@
 class Solution:
     def maxDistinctElements(self, nums, k):
-        #k = 2
-        #2, 2, 2, 2, 2, 3, 3, 3
-        #0, 1, 2, 3, 4, 5, x, x
         nums.sort()
-        low = nums[0] - k - 1
-        ans = 0
+        low, ans = nums[0] - k - 1, 0
         for num in nums:
-            if num - k > low:
-                low = num - k
-                ans += 1
-            elif num + k > low:
-                low += 1
-                ans += 1
+            if num + k > low:
+                low, ans = max(low + 1, num - k), ans + 1
+            # elif num + k > low:
+            #     low, ans = low + 1, ans + 1
         return ans
