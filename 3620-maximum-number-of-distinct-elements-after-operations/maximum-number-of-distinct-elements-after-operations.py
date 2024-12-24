587dffc5dd39aac1ad45d712b3d688aeb405f013
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-24 11:51:42 -0800

    Time: 707 ms (80.61%) | Memory: 31.6 MB (86.39%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
@@ -3,8 +3,5 @@ class Solution:
         nums.sort()
         low, ans = nums[0] - k - 1, 0
         for num in nums:
-            if num + k > low:
-                low, ans = max(low + 1, num - k), ans + 1
-            # elif num + k > low:
-            #     low, ans = low + 1, ans + 1
+            if num + k > low: low, ans = max(low + 1, num - k), ans + 1
         return ans
