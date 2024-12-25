d9106f5f43966f49e743ba512dfc934a0f937a96
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-24 23:48:04 -0800

    Time: 651 ms (92.83%) | Memory: 18 MB (35.55%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,27 +1,7 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-        cs = set(coins)
-        if amount == 0:
-            return 0
-        def dp(target):
-            if target in cs:
-                return 1
-            low = inf
-            neg = True
-            for coin in coins:
-                if target - coin >= 0:
-                    if target - coin in memo:
-                        temp = memo[target-coin]
-                    else:
-                        temp = min(low, dp(target - coin))
-                    if temp != -1:
-                        neg = False
-                        low = min(low, 1 + temp)
-            if neg:
-                low = -1
-            memo[target] = low
-            return low
-        return dp(amount)
+    def coinChange(self, coins, amount):
+        dp = [0] + [10001] * (amount)
+        for coin in coins:
+            for x in range(coin, amount + 1): dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[-1] if dp[-1] != 10001 else -1 
                 
-
