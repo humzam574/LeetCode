f5cf37caa98347224606ee80e11d7582b573f3c4
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-25 17:38:34 -0800

    Time: 19 ms (97.88%) | Memory: 17.8 MB (64.98%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,15 +1,9 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         sm = sum(nums)
-        if (sm + target) % 2 != 0 or sm < abs(target):
-            return 0
-        
+        if (sm + target) % 2 != 0 or sm < abs(target): return 0
         st = (sm + target) // 2
-
         dp = [1] + [0] * st
-
         for n in nums:
-            for i in range(st, n - 1, -1):
-                dp[i] = dp[i - n] + dp[i]
-        
+            for i in range(st, n - 1, -1): dp[i] = dp[i - n] + dp[i]
         return dp[st]
