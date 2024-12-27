cff1bb90d63ce9451a5d57f3d553b69e76a5b731
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-27 15:04:55 -0800

    Time: 65 ms (71.12%) | Memory: 23.3 MB (21.48%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,11 +1,5 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        #keep a curr and ans variable
-        curr = values[0]
-        ans = 0
-        for i in range(1, len(values)):
-            curr -= 1
-            ans = max(ans, values[i] + curr)
-            if values[i] > curr:
-                curr = values[i]
+        curr, ans = values[0], 0
+        for i in range(1, len(values)): ans, curr = max(ans, values[i] + curr - 1), max(curr - 1, values[i])
         return ans
