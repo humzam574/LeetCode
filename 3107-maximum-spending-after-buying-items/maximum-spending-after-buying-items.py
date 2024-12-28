7c53943aea4d7a9d23487969ba5b0bf5f04f4eb9
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-28 02:44:23 -0800

    Time: 173 ms (47.32%) | Memory: 31.7 MB (18.75%) - LeetSync

--- 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.py
+++ 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.py
@@ -1,2 +1,13 @@
 class Solution:
-    def maxSpending(self, v): return sum(starmap(mul, zip(sorted(chain(*v)), count(1))))
+    def maxSpending(self, values):
+        n = len(values[0])
+        heap = [(row[-1], i, n - 1) for i, row in enumerate(values)]
+        heapify(heap)
+        ans = d = 0
+        while heap:
+            d += 1
+            v, i, j = heappop(heap)
+            ans += v * d
+            if j:
+                heappush(heap, (values[i][j - 1], i, j - 1))
+        return ans
