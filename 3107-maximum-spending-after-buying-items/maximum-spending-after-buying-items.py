48a0397fac5bfd85beb7e632bb6cbd67d284e12a
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-28 02:43:34 -0800

    Time: 51 ms (94.64%) | Memory: 32.1 MB (7.14%) - LeetSync

--- 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.py
+++ 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.py
@@ -1,18 +1,2 @@
 class Solution:
-    def maxSpending(self, values: List[List[int]]) -> int:
-        #heapify it
-        heap = [shop[-1] for shop in values]
-        dict = defaultdict(list)
-        for i, shop in enumerate(values): dict[shop[-1]].append(i)
-        heapify(heap)
-        ans = 0
-        for i in range(1, 1 + len(values) * len(values[0])):
-            val = heappop(heap)
-            ans += (i * val)
-            idx = dict[val].pop()
-            #print(idx)
-            values[idx].pop()
-            if values[idx]:
-                dict[values[idx][-1]].append(idx)
-                heappush(heap, values[idx][-1])
-        return ans
+    def maxSpending(self, v): return sum(starmap(mul, zip(sorted(chain(*v)), count(1))))
