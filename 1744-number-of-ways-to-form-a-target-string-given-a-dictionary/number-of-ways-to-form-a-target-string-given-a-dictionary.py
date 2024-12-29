6301ca8b5dceb486ff27950718ba5c079f594a7d
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-28 22:52:41 -0800

    Time: 563 ms (96.04%) | Memory: 49.6 MB (42.89%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -3,10 +3,13 @@ class Solution:
         tl, wl, freq = len(target), len(words[0]), [defaultdict(int) for i in range(len(words[0]))]
         for i in range(wl):
             for j in range(len(words)): freq[i][words[j][i]]+=1
-        @cache
+        #@cache
+        memo = {}
         def bt(curr, k):
             if curr == target: return 1
             if tl - len(curr) > wl - k: return 0
+            if (curr, k) in memo: return memo[(curr, k)]
             char = target[len(curr)]
-            return (freq[k][char] * bt(curr + char, k + 1) + bt(curr, k + 1)) % (10 ** 9 + 7)
+            memo[(curr, k)] = (freq[k][char] * bt(curr + char, k + 1) + bt(curr, k + 1)) % (10 ** 9 + 7)
+            return memo[(curr, k)]
         return bt("", 0) % (10 ** 9 + 7)
