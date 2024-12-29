8a35b5f1148ad3494679254e0ac45f08c5b720b6
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-28 22:46:06 -0800

    Time: 1054 ms (68.07%) | Memory: 115.4 MB (32.63%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,13 +1,7 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
         tl, wl, freq = len(target), len(words[0]), [defaultdict(int) for i in range(len(words[0]))]
-        for i in range(len(words[0])):
-            for j in range(len(words)):
-                freq[i][words[j][i]]+=1
+        for idx in range(len(words) * len(words[0])): freq[idx % len(words[0])][words[idx // len(words[0])][idx % len(words[0])]] += 1
         @cache
-        def bt(curr, k):
-            if curr == target: return 1
-            if tl - len(curr) > len(words[0]) - k: return 0
-            char = target[len(curr)]
-            return freq[k][char] * bt(curr + char, k + 1) + bt(curr, k + 1)
+        def bt(curr, k): return curr == target if curr == target or tl - len(curr) > len(words[0]) - k else freq[k][target[len(curr)]] * bt(curr + target[len(curr)], k + 1) + bt(curr, k + 1)
         return bt("", 0) % (10**9 + 7)
