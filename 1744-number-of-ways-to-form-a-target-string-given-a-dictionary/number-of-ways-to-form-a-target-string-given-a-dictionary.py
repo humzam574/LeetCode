fb28064afb811ca89783f9e1df0a7f478fa5fec7
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-28 22:37:44 -0800

    Time: 548 ms (96.27%) | Memory: 115.5 MB (32.63%) - LeetSync

---
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        #this has to be some sort of tabulation
+        tl = len(target)
+        freq = [defaultdict(int) for i in range(len(words[0]))]
+        for i in range(len(words[0])):
+            for j in range(len(words)):
+                freq[i][words[j][i]]+=1
+        print(freq)
+        @cache
+        def bt(curr, k):
+            if curr == target: return 1
+            if tl - len(curr) > len(words[0]) - k: return 0
+            char = target[len(curr)]
+            return freq[k][char] * bt(curr + char, k + 1) + bt(curr, k + 1)
+        return bt("", 0) % (10**9 + 7)
