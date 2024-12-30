e8e33cd1504bc4b11a31aa501e00e128cf47f8c1
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-29 22:38:26 -0800

    Time: 52 ms (99.53%) | Memory: 22.2 MB (66.77%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -1,15 +1,6 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        #groups of zero
-        #some sort of backtracking where you find the number of good strings at n times
-        #dp tabulation?
-        dp = [0] * (high + 1)
-        dp[0] = 1
-        #number of ways you can make it to that point at each index
-        if one < zero:
-            zero, one = one, zero
-        for i in range(zero, one):
-            dp[i] += (dp[i - zero]) % 1000000007
-        for i in range(one, high + 1):
-            dp[i] = (dp[i] + dp[i - zero]+ dp[i - one]) % 1000000007
+        dp, zero, one = [1] + [0] * high, min(zero, one), max(zero, one)
+        for i in range(zero, one): dp[i] += (dp[i - zero]) % 1000000007
+        for i in range(one, high + 1): dp[i] = (dp[i] + dp[i - zero]+ dp[i - one]) % 1000000007
         return sum(dp[low : high + 1]) % 1000000007
