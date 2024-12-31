e6adb8258a86ecbedf330fd0c6b3ec84db636452
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-31 14:12:48 -0800

    Time: 27 ms (5.72%) | Memory: 18.1 MB (13.43%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,11 +1,16 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        n, left7, left30, dp = len(days), 0, 0, [0] * len(days)
-        for right in range(n):
-            while days[right] - days[left7] >= 7: left7 += 1
-            while days[right] - days[left30] >= 30: left30 += 1
-            cost1 = (dp[right - 1] if right > 0 else 0) + costs[0]
-            cost7 = (dp[left7 - 1] if left7 > 0 else 0) + costs[1]
-            cost30 = (dp[left30 - 1] if left30 > 0 else 0) + costs[2]
-            dp[right] = min(cost1, cost7, cost30)
+        l7, l30 = 0, 0
+        dp = [0] * (len(days))
+        for i, day in enumerate(days):
+            while days[i] - days[l7] >= 7: l7 += 1
+            while days[i] - days[l30] >= 30: l30 += 1
+            p1 = costs[0] + (0 if i == 0 else dp[i - 1])
+            p30 = costs[2] + (0 if l30 == 0 else dp[l30 - 1])
+            p7 = costs[1] + (0 if l7 == 0 else dp[l7 - 1])
+            dp[i] = min(p1, p30, p7)
+            print(dp)
+            print(l7)
+            print(l30)
+        print(dp)
         return dp[-1]
