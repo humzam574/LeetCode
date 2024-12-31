a8e2e3dfbf24034a612bc5795561f1de5964c4ce
humzam574 <65996060+humzam574@users.noreply.github.com>
2024-12-31 14:35:02 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (29.23%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,12 +1,8 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        l7, l30 = 0, 0
-        dp = [0] * (len(days))
-        for i, day in enumerate(days):
+        l7, l30, dp = 0, 0, [0] * len(days)
+        for i in range(len(days)):
             while days[i] - days[l7] >= 7: l7 += 1
             while days[i] - days[l30] >= 30: l30 += 1
-            p1 = costs[0] + (0 if i == 0 else dp[i - 1])
-            p30 = costs[2] + (0 if l30 == 0 else dp[l30 - 1])
-            p7 = costs[1] + (0 if l7 == 0 else dp[l7 - 1])
-            dp[i] = min(p1, p30, p7)
+            dp[i] = min(costs[0] + (i != 0) * dp[i - 1], costs[1] + (l7 != 0) * dp[l7 - 1], costs[2] + (l30 != 0) * dp[l30 - 1])
         return dp[-1]
