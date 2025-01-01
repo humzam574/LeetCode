f84ccb6887c6b6a41b610a03fb49db8cc367818a
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-01 15:02:27 -0800

    Time: 250 ms (11.26%) | Memory: 103.8 MB (9.93%) - LeetSync

---
+++ 651-4-keys-keyboard/4-keys-keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxA(self, n: int) -> int:
+        #some sort of bottom up dp
+        @cache
+        def bt(curr, cb, cnt):
+            if curr >= n - 3:
+                return cnt + (n - curr) * cb
+            return max(bt(curr + 3, cnt, cnt*2), bt(curr + 1, cb, cnt + cb))
+        
+        return bt(0, 1, 0)
