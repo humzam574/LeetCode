1c266662371cb6f4848ba8fc4d1a5eb27162bf95
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-02 23:07:42 -0800

    Time: 33 ms (99.78%) | Memory: 32.6 MB (37.53%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,5 +1,2 @@
 class Solution:
-    def waysToSplitArray(self, nums: List[int]) -> int:
-        l, r, ans = 0, (sum(nums)+1)//2, 0
-        for i in range(len(nums)-1): l, ans = l + nums[i], ans + (l + nums[i] >= r)
-        return ans
+    def waysToSplitArray(self, n):l,r,ans=0,(sum(n)+1)//2,0;return sum((l:=l+n[i])>=r for i in range(len(n)-1))
