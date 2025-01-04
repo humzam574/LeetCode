2c075f2a1ff00b0dee646af39a67e019bca4980a
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-04 14:41:12 -0800

    Time: 147 ms (79.65%) | Memory: 18.4 MB (25.38%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,19 +1,9 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        #use a two pointer
-        locations = []
-        for i in range(26):
-            locations.append([-1, -1])
+        locations, ans = [[-1, -1] for _ in range(26)], 0
         for i, char in enumerate(s):
             idx = ord(char) - ord('a')
-            if locations[idx][0] == -1:
-                locations[idx][0] = i
-                locations[idx][1] = i
-            else:
-                locations[idx][1] = i
-        ans = 0
-        #print(locations)
-        for l, r in locations:
-            if r - l > 1:
-                ans += len(set(s[l+1:r]))
+            if locations[idx][0] == -1: locations[idx] = [i, i]
+            else: locations[idx][1] = i
+        for l, r in locations: ans = ans + len(set(s[l+1:r])) if r - l > 1 else ans
         return ans
