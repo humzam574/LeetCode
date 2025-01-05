51edaed0a92dd2e996e3b601325e78800752ebff
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-04 20:35:34 -0800

    Time: 51 ms (92.10%) | Memory: 41.4 MB (20.36%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,17 +1,6 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        #figure out how to model the shifts in an O(n) way and not O(n^2)
-        sh = [0] * (len(s))
-        for l, r, d in shifts:
-            sh[l] += (1 if d == 1 else -1)
-            if r + 1 < len(s):
-                sh[r + 1] -= (1 if d == 1 else -1)
-            #print(sh)
-        #print(sh)
-        curr = 0
-        ans = list(s)
-        for i in range(len(s)):
-            curr += sh[i]
-            #print(curr)
-            ans[i] = chr(ord('a') + ((ord(ans[i]) + curr - ord('a')) % 26))
+        sh, curr, ans = [0] * (len(s) + 1), 0, list(s)
+        for l, r, d in shifts: sh[l] += (1 if d == 1 else -1); sh[r + 1] -= (1 if d == 1 else -1)
+        for i in range(len(s)): curr, ans[i] = curr + sh[i], chr(97 + ((ord(ans[i]) + curr + sh[i] - 97) % 26))
         return ''.join(ans)
