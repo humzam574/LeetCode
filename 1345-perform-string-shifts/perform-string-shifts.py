9974aa55a61d6310c50300beff0f6c935997732d
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-05 15:49:21 -0800

    Time: 3 ms (6.94%) | Memory: 17.7 MB (66.14%) - LeetSync

--- 1345-perform-string-shifts/perform-string-shifts.py
+++ 1345-perform-string-shifts/perform-string-shifts.py
@@ -1,3 +1,2 @@
 class Solution:
-    def stringShift(self, s: str, shift: List[List[int]]) -> str:
-        return (lambda i: s[i:] + s[:i])(sum(x * (1-2*d) for d, x in shift) % len(s))
+    def stringShift(self,s,h):return(lambda i:s[i:]+s[:i])(sum(x*(1-2*d)for d,x in h)%len(s))
