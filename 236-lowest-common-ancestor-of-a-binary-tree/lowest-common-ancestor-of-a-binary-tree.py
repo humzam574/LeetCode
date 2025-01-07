d1b8959b3611d390d120eb16a06053a9db7a9e4f
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-06 16:50:08 -0800

    Time: 46 ms (84.99%) | Memory: 21.7 MB (36.69%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,18 +7,23 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.ancs = []
-        def dfs(curr, anc):
-            if curr:
-                if curr == p or curr == q:
-                    self.ancs.append(anc)
-                dfs(curr.left, anc + ['l'])
-                dfs(curr.right, anc + ['r'])
-        dfs(root, [])
-        p1 = self.ancs[0]
-        p2 = self.ancs[1]
-        for i in range(min(len(p1), len(p2))):
-            if p1[i] != p2[i]:
-                return root
-            root = root.left if p1[i] == 'l' else root.right
-        return root
+        def dfs(node):
+            # Base case: null node
+            if not node:
+                return None
+            
+            # If the current node is either p or q, return it
+            if node == p or node == q:
+                return node
+            
+            # Recur for left and right children
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            # If both left and right return a non-null value, current node is LCA
+            if left and right:
+                return node
+            
+            # Otherwise, return the non-null child (or null if both are null)
+            return left if left else right
+        return dfs(root)
