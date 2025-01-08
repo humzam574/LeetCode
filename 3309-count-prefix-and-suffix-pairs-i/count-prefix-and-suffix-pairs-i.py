74d9c33cae0db7e77c9434cd8aa057c340c430de
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-07 16:06:44 -0800

    Time: 12 ms (32.20%) | Memory: 17.8 MB (14.64%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
@@ -2,6 +2,5 @@ class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         ans = 0
         for i in range(len(words) - 1):
-            for j in range(i + 1, len(words)):
-                ans+= (1 if len(words[j]) >= len(words[i]) and words[i] == words[j][:len(words[i])] == words[j][-len(words[i]):] else 0)
+            ans += sum(words[i] == words[j][:len(words[i])] == words[j][-len(words[i]):] for j in range(i + 1, len(words)))
         return ans
