a97ebb5bcc8d07fd03be5474ddc79b4c72ad47f5
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-07 16:05:50 -0800

    Time: 11 ms (56.86%) | Memory: 17.9 MB (14.64%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
@@ -1,16 +1,7 @@
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        def ipas(w1, w2):
-            l1 = len(w1)
-            if len(w2) < len(w1): return 0
-            if w1 == w2[:l1]:
-                #print("prfx " + w1 + " " + w2)
-                if w1 == w2[-l1:]:
-                    #print("sfx " + w1 + " " + w2)
-                    return 1
-            return 0
         ans = 0
         for i in range(len(words) - 1):
             for j in range(i + 1, len(words)):
-                ans+=ipas(words[i], words[j])
+                ans+= (1 if len(words[j]) >= len(words[i]) and words[i] == words[j][:len(words[i])] == words[j][-len(words[i]):] else 0)
         return ans
