7400dc4f5c6c485b6bf19ca160ee2912d9e754e9
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-10 18:22:43 -0800

    Time: 36 ms (37.45%) | Memory: 18.1 MB (16.30%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,12 +1,8 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        #all of em have to be sets of 2 except for k of them which can be 1
-        dict = collections.Counter(s)
-        thresh = 0
-        if k >= len(s):
-            return k == len(s)
-        for value in dict.values():
-            thresh += int(value % 2 == 1)
-            if thresh > k:
-                return False
-        return True
+        dict = Counter(s)
+        if k > len(s): return False
+        odds = 0
+        for g, v in dict.items():
+            odds += (v % 2)
+        return odds <= k
