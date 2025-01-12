cc48016984eaa0e45982c0f9d70d4097bde08439
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-11 21:40:20 -0800

    Time: 1 ms (71.30%) | Memory: 17.8 MB (42.42%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        ans, d = 0, 2
+        while n > 1:
+            while n % d == 0: ans, n = ans + d, n // d
+            d += 1
+        return ans
