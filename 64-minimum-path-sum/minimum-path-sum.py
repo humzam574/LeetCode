31fb15e6bb5e6ace9960826533ca2770fc4fb304
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-11 21:31:03 -0800

    Time: 7 ms (99.77%) | Memory: 18.8 MB (90.95%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = [0] + [2**24] * (n - 1)
+        dp = [2**31] * n
+        dp[0] = 0
         for i in range(m):
             for j in range(n):
                 if j == 0:
                     dp[j] += grid[i][j]
                 else:
                     dp[j] = dp[j] + grid[i][j] if dp[j] < dp[j - 1] else dp[j - 1] + grid[i][j]
-        return dp[-1]
+        return dp[n - 1]
