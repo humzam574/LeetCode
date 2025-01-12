8c8835c22db172e7a4c138e482491b6c27dd5b74
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-11 21:40:47 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (50.82%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,3 +1,2 @@
 class Solution:
-    def minSteps(self, n: int) -> int:
-        return next((self.minSteps(n//i)+i for i in range(2,isqrt(n)+1) if n%i<1),(0,n)[n>1])
+    def minSteps(self,n):return next((self.minSteps(n//i)+i for i in range(2,isqrt(n)+1)if n%i<1),(0,n)[n>1])
