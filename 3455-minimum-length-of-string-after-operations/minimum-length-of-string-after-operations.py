6e5e3bc7d388c68b8a3c9911edff2375c6f4caa7
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-13 09:20:21 -0800

    Time: 65 ms (88.25%) | Memory: 19 MB (5.71%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,3 +1,5 @@
 class Solution:
-    def minimumLength(self, s: str) -> int:
-        return sum(2 - int(n % 2) for n in Counter(s).values())
+    def minimumLength(self, s):
+        ans = 0
+        for ch in {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}: count = s.count(ch); ans += (1 if count & 1 else 2 if count != 0 else 0)
+        return ans
