2aca2fa0de003d98e140d25e1c6dc9b0a4b2a501
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-16 00:32:56 +0500

    Time: 11 ms (72.86%) | Memory: 28.7 MB (35.34%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -1,5 +1,5 @@
 class Solution:
     def canReach(self, arr, i):
         if i < 0 or i >= len(arr) or arr[i] < 0: return False
-        arr[i] *= -1
-        return arr[i] == 0 or self.canReach(arr, i - arr[i]) or self.canReach(arr, i + arr[i])
+        temp = arr[i]; arr[i] = -1
+        return temp == 0 or self.canReach(arr, i - temp) or self.canReach(arr, i + temp)
