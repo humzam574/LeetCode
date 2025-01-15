481bca761677c85fc0d90bf3e85993dda5ae1700
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-14 17:51:53 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.20%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -1,19 +1,12 @@
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
-        b1, b2 = bin(num1)[2:][::-1], bin(num2)[2:][::-1]
-        s1, s2 = b1.count("1"), b2.count("1")
-        curr = 0
+        b1, b2, curr = bin(num1)[2:][::-1], bin(num2)[2:][::-1], 0; s1, s2 = b1.count("1"), b2.count("1")
         if s1 == s2: return num1
         elif s1 > s2:
             for i in range(len(b1) - 1, -1, -1):
-                if b1[i] == "1":
-                    curr += (2 ** i)
-                    s2 -= 1
-                    if s2 == 0:
-                        return curr
+                if b1[i] == "1": curr, s2 = curr + (2 ** i), s2 - 1
+                if s2 == 0: return curr
         for i in range(max(len(b1), len(b2))):
-            if i >= len(b1) or b1[i] == "0":
-                curr += (2 ** i)
-                s2 -= 1
-                if s2 == s1: return curr + num1
+            if i >= len(b1) or b1[i] == "0": curr, s2 = curr + (2 ** i), s2 - 1
+            if s2 == s1: return curr + num1
                 
