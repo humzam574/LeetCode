be6c2531a433c3d0e101b0c56c8a7a7c934db605
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-16 10:59:22 +0500

    Time: 881 ms (20.40%) | Memory: 39.3 MB (20.85%) - LeetSync

--- 1893-maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.py
+++ 1893-maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.py
@@ -3,12 +3,12 @@ class Solution:
         dp = [[-inf, -inf] for i in range(len(nums))]
         curr = max(0, nums[0])
         dp[0][0] = nums[0]
-        for i, num in enumerate(nums[1:]):
-            curr = max(num, curr + num)
-            dp[i+1][0] = curr
+        for i in range(1, len(nums)):
+            curr = max(nums[i], curr + nums[i])
+            dp[i][0] = curr
         dp[0][1] = nums[0]**2
-        for i, num in enumerate(nums[1:]):
-            dp[i+1][1] = max(dp[i][1] + num, dp[i][0] + num**2, num**2)#, dp[i][0] + (num ** 2 - num))
+        for i in range(1, len(nums)):
+            dp[i][1] = max(dp[i - 1][1] + nums[i], dp[i - 1][0] + nums[i]**2, nums[i]**2)#, dp[i][0] + (num ** 2 - num))
         high = -inf
         for i in range(len(dp)):
             high = max(high, dp[i][1])
