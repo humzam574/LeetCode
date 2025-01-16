1ff1ca298ddbf8f706fd8534aca927dacb21c89f
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-16 11:52:57 +0500

    Time: 6 ms (84.66%) | Memory: 18.6 MB (38.64%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,6 +1,6 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        r, mx, l, mn = -1, -1e6, len(nums), 1e6
+        r, mx, l, mn = -1, -10001, len(nums), 1e6
         for i in range(l):
             if nums[i] >= mx: mx = nums[i]
             else: r = i
