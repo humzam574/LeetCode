c74b50a92a8953027c2fb4ed9ee9dd55bc53dbde
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-16 06:54:19 +0500

    Time: 7 ms (39.88%) | Memory: 36.5 MB (48.56%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -7,24 +7,11 @@ class Solution:
         n = len(nums1)
         m = len(nums2)
 
-        #n1 = [2, 1, 3]
-        #n2 = [3, 4]
-        #(2^3) ^ (2^4) ^ (1^3) ^ (1^4) ^ (3^3) ^ (3 ^ 4)
-        #1 ^ 6 ^ 2 ^ 5 ^ 0 ^ 7
-        # 2 ^ 3 ^ 2 ^ 4 ^ 1 ^ 3 ^ 1 ^ 4 ^ 3 ^ 3 ^ 3 ^ 4
-        #2 ^ 2 ^ 1 ^ 1 ^ 3 ^ 3 ^ 3 ^ 3 ^ 3 ^ 4 ^ 4 ^ 4
-        memo = defaultdict(int)
-        for num in nums1:
-            memo[num] += m
-        for num in nums2:
-            memo[num] += n
         ans = 0
-        for k, v in memo.items():
-            if v % 2:
-                ans = ans ^ k
+        if m % 2:
+            for num in nums1:
+                ans = ans ^ num
+        if n % 2:
+            for num in nums2:
+                ans = ans ^ num
         return ans
-
-
-        #0 ^ 1 ^ 1 ^ 0 * 2**0 = 0
-        #1 ^ 0 ^ 1 * 2**1 = 0
-        #1 ^1 * 2**2 = 0
