584046b1769ce6cb6028feeff822cf4caf419c73
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-16 11:11:35 +0500

    Time: 19 ms (99.81%) | Memory: 26.2 MB (47.71%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
@@ -1,5 +1,21 @@
 class Solution:
     def maximumSum(self, arr: List[int]):
-        mx = n0 = n1 = -inf
-        for i, num in enumerate(arr): n1 = max(n0,  n1 + num); n0 = max(num, n0 + num); mx = max(mx,  n1,   n0)
+        s = arr[0]
+        non = arr[0]
+        mx = arr[0]
+
+        for i in arr[1:]:
+            if s < 0:
+                s = 0
+            if i >= 0:
+                s = s + i
+            else:
+                if s + i > non:
+                    s = s + i
+                else:
+                    s = non
+            if non < 0:
+                non = 0
+            non += i
+            mx = max(mx, non, s)
         return mx
