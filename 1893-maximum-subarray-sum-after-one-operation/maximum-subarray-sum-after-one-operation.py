631bb5dd9795d939602223a1378ff404b846fd25
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-16 11:00:42 +0500

    Time: 900 ms (16.82%) | Memory: 39.5 MB (14.72%) - LeetSync

--- 1893-maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.py
+++ 1893-maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.py
@@ -2,13 +2,9 @@ class Solution:
     def maxSumAfterOperation(self, nums: List[int]) -> int:
         dp = [[-inf, -inf] for i in range(len(nums))]
         curr = max(0, nums[0])
-        dp[0][0] = nums[0]
+        dp[0] = [nums[0], nums[0] ** 2]
         for i in range(1, len(nums)):
-            curr = max(nums[i], curr + nums[i])
-            dp[i][0] = curr
-        dp[0][1] = nums[0]**2
-        for i in range(1, len(nums)):
-            dp[i][1] = max(dp[i - 1][1] + nums[i], dp[i - 1][0] + nums[i]**2, nums[i]**2)#, dp[i][0] + (num ** 2 - num))
+            curr = max(nums[i], curr + nums[i]); dp[i][0] = curr; dp[i][1] = max(dp[i - 1][1] + nums[i], dp[i - 1][0] + nums[i]**2, nums[i]**2)
         high = -inf
         for i in range(len(dp)):
             high = max(high, dp[i][1])
