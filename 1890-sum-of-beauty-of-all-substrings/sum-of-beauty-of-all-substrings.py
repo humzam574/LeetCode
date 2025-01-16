ec1fba0cd02bc70cc02bf5d6e62536ff740b4b5b
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-16 07:58:14 +0500

    Time: 914 ms (96.06%) | Memory: 17.6 MB (46.62%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -1,18 +1,7 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        n = len(s)
-        ans = 0
+        n, ans = len(s), 0
         for i in range(n):
-            dic = defaultdict(int)
-            low = 0  
-            high = 0
-            for j in range(i,n):
-                dic[s[j]] += 1
-                if dic[s[j]] == high + 1:
-                    high = dic[s[j]]
-                if low == 0 or dic[s[j]]-1 == 0:
-                    low = 1
-                elif dic[s[j]]-1 == low:
-                    low = min(dic.values())
-                ans += high - low
+            dic, low, high = defaultdict(int), 0, 0
+            for j in range(i,n): dic[s[j]] += 1; high, low = max(dic[s[j]], high), 1 if low == 0 or dic[s[j]] == 1 else min(dic.values()); ans += high - low
         return ans
