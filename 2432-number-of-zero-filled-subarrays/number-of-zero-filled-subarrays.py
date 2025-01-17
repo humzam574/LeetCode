27bb3fee2d39c9522a4b0ea5187ae6cffc3bf379
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-17 10:39:18 +0500

    Time: 25 ms (89.13%) | Memory: 28.4 MB (28.49%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -1,15 +1,7 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        #n -> (n^2+n)/2
-        l = 0
-        ans = 0
-        while l < len(nums):
-            if nums[l] == 0:
-                temp = l
-                while temp < len(nums) and nums[temp] == 0:
-                    temp += 1
-                val = temp - l
-                ans += (val*val + val)//2
-                l = temp
-            l += 1
-        return ans
+        ans = k = 0
+        for n in nums:
+            if (n == 0): k += 1
+            else: ans, k = ans + (k * (k + 1)) // 2, 0
+        return ans + (k * (k + 1)) // 2
