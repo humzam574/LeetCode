9d3142a49358fd1bce56ceeaf17d23bb8df2bbc9
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-18 04:38:49 +0500

    Time: 159 ms (40.63%) | Memory: 39.2 MB (25.00%) - LeetSync

--- 519-widest-pair-of-indices-with-equal-range-sum/widest-pair-of-indices-with-equal-range-sum.py
+++ 519-widest-pair-of-indices-with-equal-range-sum/widest-pair-of-indices-with-equal-range-sum.py
@@ -1,17 +1,14 @@
 class Solution:
     def widestPairOfIndices(self, nums1: List[int], nums2: List[int]) -> int:
-        dict = {}
         if len(nums1) == 1: return int(nums1[0] == nums2[0])
-        nums, ans = [0] * len(nums1), 0
-        nums[0] = nums1[0] - nums2[0]
+        nums, ans, dict, sum1, sum2 = [0] * len(nums1), 0, {}, nums1[0], nums2[0]
+        nums = [sum1 - sum2] + [0] * (len(nums1)-1)
         for i in range(1, len(nums1)):
-            nums1[i] += nums1[i - 1]
-            nums2[i] += nums2[i - 1]
-            nums[i] = nums1[i] - nums2[i]
+            sum1 += nums1[i]
+            sum2 += nums2[i]
+            nums[i] = sum1 - sum2
         for i, n in enumerate(nums):
             if n == 0: ans = max(ans, i + 1)
             elif n in dict: ans = max(ans, i - dict[n])
             else: dict[n] = i
         return ans
-
-            
