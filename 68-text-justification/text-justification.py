873a0648ca93181191ab726550b3410a9582622f
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-17 23:38:26 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (10.35%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -1,22 +1,17 @@
 class Solution:
     def fullJustify(self, words: List[str], mw: int) -> List[str]:
-        def process(l, r):
-            if r == l + 1: return words[l] + (" " * (mw - len(words[l])))
-            sp = mw - sum(len(w) for w in words[l:r]); each, rm, stri = sp // (r - l - 1), sp % (r - l - 1), words[l]
-            for i in range(l + 1, r):
-                stri += (each * " ")
-                if rm and (mw - len(stri)): stri, rm = stri + " ", rm - 1
-                stri += words[i]
-            return stri
-        ans, r, l, curr = [], 0, 0, len(words[0])
+        ans, r, l, curr, c2, temp = [], 0, 0, len(words[0]), 0, ""
         for r in range(1, len(words)):
             if curr <= mw and curr + 1 + len(words[r]) > mw:
-                if l != r: ans.append(process(l, r))
+                if r == l + 1: ans.append( words[l] + (" " * (mw - len(words[l]))))
+                elif r > l + 1:
+                    sp = mw - sum(len(w) for w in words[l:r]); each, rm, stri = sp // (r - l - 1), sp % (r - l - 1), words[l]
+                    for i in range(l + 1, r):
+                        stri += (each * " ")
+                        if rm and (mw - len(stri)): stri, rm = stri + " ", rm - 1
+                        stri += words[i]
+                    ans.append(stri)
                 curr, l = len(words[r]), r
             else: curr += 1 + len(words[r])
-        curr, temp = 0, ""
-        for i in range(l, len(words)):
-            temp += words[i]
-            if len(temp) - mw: temp+=" "
-            curr += 1 + len(words[i])
-        temp += (" " * (mw - curr)); ans.append(temp); return ans
+        for i in range(l, len(words)): temp += words[i] + (" " if len(temp) - mw + len(words[i]) else ""); c2 += 1 + len(words[i])
+        return ans + [temp + (" " * (mw - c2))]
