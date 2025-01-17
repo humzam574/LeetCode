a5f44e1335bb36238c51b7bc31e105512426df99
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-17 09:31:49 +0500

    Time: 75 ms (34.00%) | Memory: 18.3 MB (23.09%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -2,29 +2,25 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        lst = []
-        for word in strs:
-            ans = ["" for _ in range(len(strs))]
-            for c in word:
-                ans.append(chr((ord(c) + 1) % 256))
-            lst.append(''.join(ans))
-        return lst
-        
+        res = ""
+        for i in strs:
+            res+=str(len(i))
+            res+="#"
+            res+=i
+        return res
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        lst = []
-        
-        for word in s:
-            ans = ["" for _ in range(len(s))]
-            for c in word:
-                ans.append(chr((ord(c) - 1) % 256))
-            lst.append(''.join(ans))
-        return lst
-        
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+        res = []
+        t = 0
+        l = len(s)
+        while t < l:
+            end = t
+            while s[end] != "#":
+                end+=1
+            num = int(s[t:end])
+            t = end+1
+            res.append(s[t:t+num])
+            t+=num
+        return res
