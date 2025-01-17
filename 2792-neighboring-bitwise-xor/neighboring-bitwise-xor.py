c3e455c71b8f39cd8d85c4026cf7db78d051f6ac
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-17 05:24:54 +0500

    Time: 27 ms (90.51%) | Memory: 22.4 MB (24.82%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -1,9 +1,2 @@
 class Solution:
-    def doesValidArrayExist(self, derived: List[int]) -> bool:
-        #1 = 0 + 1 or 1 + 0
-        #0 = 0 + 0 or 1 + 1
-
-        #d[i]
-
-        #1 0 1 = False
-        return sum(derived) % 2 == 0
+    def doesValidArrayExist(self, derived: List[int]) -> bool: return sum(derived) % 2 == 0
