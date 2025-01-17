790316d9183085ebd08cf3edbf7af3c832126adb
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-17 11:26:20 +0500

    Time: 7 ms (81.69%) | Memory: 55.2 MB (18.63%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -1,13 +1,9 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        #grab all the triplets that have a wanted number
         f = s = t = False
         for trip in triplets:
-            if trip[0] == target[0] and trip[1] <= target[1] and trip[2] <= target[2]:
-                f = True
-            if trip[1] == target[1] and trip[0] <= target[0] and trip[2] <= target[2]:
-                s = True
-            if trip[2] == target[2] and trip[0] <= target[0] and trip[1] <= target[1]:
-                t = True
+            if trip[0] == target[0] and trip[1] <= target[1] and trip[2] <= target[2]: f = True
+            if trip[1] == target[1] and trip[0] <= target[0] and trip[2] <= target[2]: s = True
+            if trip[2] == target[2] and trip[0] <= target[0] and trip[1] <= target[1]: t = True
             if f and s and t: return True
         return False
