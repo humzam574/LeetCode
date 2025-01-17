ee9b5ec7647869df62efa060b27df6936420d5f3
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-18 04:35:23 +0500

    Time: 206 ms (20.31%) | Memory: 41.6 MB (17.19%) - LeetSync

--- 519-widest-pair-of-indices-with-equal-range-sum/widest-pair-of-indices-with-equal-range-sum.py
+++ 519-widest-pair-of-indices-with-equal-range-sum/widest-pair-of-indices-with-equal-range-sum.py
@@ -1,22 +1,17 @@
 class Solution:
     def widestPairOfIndices(self, nums1: List[int], nums2: List[int]) -> int:
-        dict = defaultdict(list)
+        dict = {}
         if len(nums1) == 1: return int(nums1[0] == nums2[0])
-        nums = [0] * len(nums1)
+        nums, ans = [0] * len(nums1), 0
+        nums[0] = nums1[0] - nums2[0]
         for i in range(1, len(nums1)):
             nums1[i] += nums1[i - 1]
             nums2[i] += nums2[i - 1]
-        nums = [nums1[i] - nums2[i] for i in range(len(nums))]
-        print(nums1)
-        print(nums2)
-        print(nums)
-        ans = 0
+            nums[i] = nums1[i] - nums2[i]
         for i, n in enumerate(nums):
-            if n in dict:
-                ans = max(ans, i - dict[n][0])
-            if n == 0:
-                ans = max(ans, i + 1)
-            dict[n].append(i)
+            if n == 0: ans = max(ans, i + 1)
+            elif n in dict: ans = max(ans, i - dict[n])
+            else: dict[n] = i
         return ans
 
             
