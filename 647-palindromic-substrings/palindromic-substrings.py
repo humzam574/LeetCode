e7c9d1d302f90d99cf63d7e8ef1ab85f091a4d40
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-17 09:21:31 +0500

    Time: 127 ms (73.05%) | Memory: 17.9 MB (37.76%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -5,5 +5,5 @@ class Solution:
             a, b, lm, rm, le, re = 0, 0, i, i, i, i + 1
             while lm >= 0 and rm < len(s) and s[lm] == s[rm]: lm, rm, a = lm - 1, rm + 1, a + 1
             while le >= 0 and re < len(s) and s[le] == s[re]: le, re, b = le - 1, re + 1, b + 1
-            ans += (a+b)
+            ans += (a + b)
         return ans
