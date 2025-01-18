a65427fcabe401bff45a4dd1df4d3aca206a5a96
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-19 00:30:27 +0500

    Time: 95 ms (89.29%) | Memory: 19.1 MB (66.88%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,19 +1,12 @@
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        mc = [[inf for i in range(n)] for j in range(m)]
-        mc[0][0] = 0
-        dq = deque([(0,0)])
+        m,n,dq= len(grid), len(grid[0]), deque([(0,0)]); mc = [[inf for i in range(n)] for j in range(m)]; mc[0][0] = 0
         while dq:
             x, y = dq.popleft()
-            d = ((0,1),(0,-1),(1,0),(-1,0))
-            for i, (dx, dy) in enumerate(d):
-                cost = int(grid[x][y] != i +1)
-                nx, ny = x+dx, y+dy
+            for i, (dx, dy) in enumerate(((0,1),(0,-1),(1,0),(-1,0))):
+                cost, nx, ny = grid[x][y] != i +1, x+dx, y+dy
                 if 0 <= nx < m and 0 <= ny < n and mc[x][y] + cost < mc[nx][ny]:
                     mc[nx][ny] = mc[x][y] + cost
-                    if cost:
-                        dq.append((nx, ny))
-                    else:
-                        dq.appendleft((nx, ny))
-        return mc[m-1][n-1]
+                    if cost: dq.append((nx, ny))
+                    else: dq.appendleft((nx, ny))
+        return mc[-1][-1]
