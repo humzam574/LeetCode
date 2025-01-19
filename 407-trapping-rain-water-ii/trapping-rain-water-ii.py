db6f9524d4133700e394a79950b695d72dbd8d1d
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-20 01:38:49 +0500

    Time: 135 ms (57.01%) | Memory: 20 MB (70.36%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.py
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -1,23 +1,12 @@
 class Solution:
     def trapRainWater(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        heap = []
+        m, n, heap, ans, high = len(grid), len(grid[0]), [], 0, -1
         for x in range(m):
             for y in range(n):
-                if x in [0, m - 1] or y in [0, n - 1]:
-                    heappush(heap, (grid[x][y], x, y))
-                    grid[x][y] = -1
-        
-        ans = 0
-        high = -1
-
+                if x in [0, m - 1] or y in [0, n - 1]: heappush(heap, (grid[x][y], x, y)); grid[x][y] = -1
         while heap:
-            h, x, y = heappop(heap)
-            high = max(high, h)
-            ans += high - h
+            h, x, y = heappop(heap); high = max(high, h); ans += high - h
             for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
                 nx, ny = x+dx, y+dy
-                if (0 <= nx < m and 0 <= ny < n and grid[nx][ny] != -1):
-                    heappush(heap, (grid[nx][ny], nx, ny))
-                    grid[nx][ny] = -1
+                if (0 <= nx < m and 0 <= ny < n and grid[nx][ny] != -1): heappush(heap, (grid[nx][ny], nx, ny)); grid[nx][ny] = -1
         return ans
