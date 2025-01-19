349f51d8840bc1ea7e4c7ae3fe6fd22503379211
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-20 03:02:37 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (9.67%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -7,19 +7,12 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root: return []
-        dq = deque()
-        bfs = deque([(root, 0)])
-        offset = 0
+        dq, bfs, offset = deque(), deque([(root, 0)]), 0
         while bfs:
             node, delta = bfs.popleft()
-            if delta < offset:
-                dq.appendleft([])
-                offset -= 1
-            elif len(dq) <= delta - offset:
-                dq.append([])
+            if delta < offset: dq.appendleft([]); offset -= 1
+            elif len(dq) <= delta - offset: dq.append([])
             dq[delta - offset].append(node.val)
-            if node.left:
-                bfs.append((node.left, delta - 1))
-            if node.right:
-                bfs.append((node.right, delta + 1))
+            if node.left: bfs.append((node.left, delta - 1))
+            if node.right: bfs.append((node.right, delta + 1))
         return list(dq)
