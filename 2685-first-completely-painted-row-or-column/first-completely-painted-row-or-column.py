8e1ea9dd14703ed22135e7a718a20f617e8d3eb9
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-20 10:02:06 +0500

    Time: 89 ms (98.11%) | Memory: 43.8 MB (96.21%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -1,16 +1,8 @@
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        m, n = len(mat), len(mat[0])
-        hashset = [None] * (len(arr) + 1)
+        m, n, hashset = len(mat), len(mat[0]), [None] * (len(arr) + 1); rows, cols = [0] * m, [0] * n
         for x in range(m):
-            for y in range(n):
-                hashset[mat[x][y]] = (x, y)
-        rows = [0] * m
-        cols = [0] * n
+            for y in range(n): hashset[mat[x][y]] = (x, y)
         for i in range(len(arr)):
-            x, y = hashset[arr[i]]
-            rows[x] += 1
-            cols[y] += 1
-            if rows[x] == n or cols[y] == m:
-                return i
-        return 0
+            x, y = hashset[arr[i]]; rows[x], cols[y] = rows[x] + 1, cols[y] + 1
+            if rows[x] == n or cols[y] == m: return i
