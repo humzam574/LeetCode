c0dba0c74ed854296bcf0c48b8444fe9373caa31
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-22 11:10:31 +0500

    Time: 3 ms (90.36%) | Memory: 17.8 MB (61.45%) - LeetSync

--- 2141-smallest-greater-multiple-made-of-two-digits/smallest-greater-multiple-made-of-two-digits.py
+++ 2141-smallest-greater-multiple-made-of-two-digits/smallest-greater-multiple-made-of-two-digits.py
@@ -1,23 +1,21 @@
 class Solution:
     def findInteger(self, k: int, digit1: int, digit2: int) -> int:
-        #generate all possible numbers and check the %'s
-        high = 2**31 - 1
-        #if digit1 == 0 and digit2 == 0: return -1
-        dq = deque()
-        if digit1 != 0:
-            dq.append(str(digit1))
-        if digit2 != 0:
-            dq.append(str(digit2))
-        ans = inf
+        d1, d2 = min(digit1, digit2), max(digit1, digit2)
+        dq = collections.deque([])
+        if d1 != 0:
+            dq.append(d1)
+        if d2 != 0:
+            dq.append(d2)
+        if d1 == d2 and dq:
+            dq.popleft()
+
         while dq:
-            #print(dq)
-            st = dq.popleft()
-            num = int(st)
-            if num % k == 0 and num > k:
-                ans = min(ans, num)
-            if num < high:
-                dq.append(st + str(digit1))
-                dq.append(st + str(digit2))
-        return -1 if ans >= high else ans
-            
-            
+            num = dq.popleft()
+            if num > k and num % k == 0:
+                return num
+            if num*10+d1 < 2**31:
+                dq.append(num*10+d1)
+            if num*10+d2 < 2**31:
+                dq.append(num*10+d2)
+
+        return -1
