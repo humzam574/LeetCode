8b7ce39932194a7ebb0fdcb4ccaecaeaf3468c8a
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-22 08:25:34 +0500

    Time: 717 ms (78.35%) | Memory: 77.1 MB (83.29%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.py
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -1,11 +1,8 @@
 class Solution:
     def highestPeak(self, ans: List[List[int]]) -> List[List[int]]:
-        #multisourced BFS
         dq = deque()
         m = len(ans)
         n = len(ans[0])
-        visited = set()
-        #ans = [[1024 for x in range(n)] for y in range(m)]
         for x in range(m):
             for y in range(n):
                 if ans[x][y] == 1:
@@ -13,23 +10,12 @@ class Solution:
                     ans[x][y] = 0
                 else:
                     ans[x][y] = inf
-        
         while dq:
-            #print(dq)
             x, y = dq.popleft()
             val = ans[x][y] + 1
             for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
                 nx, ny = x+dx, y+dy
                 if 0 <= nx < m and 0 <= ny < n and ans[nx][ny] == inf:
-                    ans[nx][ny] = min(ans[nx][ny], val)
+                    ans[nx][ny] = val
                     dq.append((nx, ny))
-            # val = 1025
-            # for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-            #     nx, ny = x+dx, y+dy
-            #     if 0 <= nx < m and 0 <= ny < n:
-            #         if ans[nx][ny] == 1024:
-            #             dq.append((nx, ny))
-            #         else:
-            #             val = min(val, ans[nx][ny] + 1)
-            #     ans[x][y] = val
         return ans
