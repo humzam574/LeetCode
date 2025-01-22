e28b5c22a87ff8648f59b47595c8b70bd597fa70
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-22 12:28:08 +0500

    Time: 345 ms (100.00%) | Memory: 26.4 MB (93.55%) - LeetSync

--- 3148-sum-of-remoteness-of-all-cells/sum-of-remoteness-of-all-cells.py
+++ 3148-sum-of-remoteness-of-all-cells/sum-of-remoteness-of-all-cells.py
@@ -1,45 +1,19 @@
 class Solution:
-    def sumRemoteness(self, grid: List[List[int]]) -> int:
-        #sum everything
-        #find an island and find the value of each number in that island
-        #add size of island * (tot sum - sum of island) to ans
-        self.m = len(grid)
-        self.n = len(grid[0])
-        self.first = 0
-        ans = 0
-        self.tot = 0
-        
+    def sumRemoteness(self, grid):
+        m, n, whole, ans, tot = len(grid), len(grid[0]), False, 0, 0
         for row in grid:
             for num in row:
                 if num != -1:
-                    self.tot += num
-        
-        def bfs(sx, sy):
-            self.first += 1
-            dq = deque([(sx, sy)])
-            visited = {(sx, sy)}
-            sm = 0
-            sz = 0
-            while dq:
-                #print(dq)
-                sz += 1
-                x, y = dq.popleft()
-                sm += grid[x][y]
-                grid[x][y] = -1
-                
-                for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-                    nx, ny = x+dx, y+dy
-                    if 0 <= nx < self.m and 0 <= ny < self.n and grid[nx][ny] != -1 and (nx, ny) not in visited:
-                        dq.append((nx, ny))
-                        visited.add((nx, ny))
-            #print(str(sm) + " " + str(sz) + " " + str(sz * (self.tot - sm)))
-            return sz * (self.tot - sm)
-        
-        for i in range(self.m):
-            for j in range(self.n):
+                    whole, tot = True, tot + num
+        if whole == False and ans != 0: return -1
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] != -1:
-                    ans += bfs(i, j)
-                    #print(ans)
-        return 0 if self.first == 1 else ans
-        
-
+                    dq, sm, sz, grid[i][j] = deque([(i, j)]), grid[i][j], 0, -1
+                    while dq:
+                        sz += 1; x, y = dq.popleft()
+                        for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                            nx, ny = x+dx, y+dy
+                            if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != -1: dq.append((nx, ny)); sm, grid[nx][ny] = sm + grid[nx][ny], -1
+                    ans += (sz * (tot - sm))
+        return ans
