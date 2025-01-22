dcbed5e2481b80d5d615223e1021ecc47a958952
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-22 13:00:57 +0500

    Time: 271 ms (99.51%) | Memory: 68.9 MB (91.34%) - LeetSync

--- 3715-maximum-coins-from-k-consecutive-bags/maximum-coins-from-k-consecutive-bags.py
+++ 3715-maximum-coins-from-k-consecutive-bags/maximum-coins-from-k-consecutive-bags.py
@@ -1,14 +1,41 @@
 class Solution:
-    def maximumCoins(self, A: List[List[int]], k: int) -> int:
-        def slide(A):
-            A.sort()
-            res = cur = j = 0
-            for i in range(len(A)):
-                cur += (A[i][1] - A[i][0] + 1) * A[i][2]
-                while A[j][1] < A[i][1] - k + 1:
-                    cur -= (A[j][1] - A[j][0] + 1) * A[j][2]
-                    j += 1
-                part = max(0, A[i][1] - k - A[j][0] + 1) * A[j][2]
-                res = max(res, cur - part)
-            return res
-        return max(slide(A), slide([[-r,-l,w] for l,r,w in A]))
+    def maximumCoins(self, coins: List[List[int]], k: int) -> int:
+        n = len(coins)
+        if n == 1:
+            return min(coins[0][1] - coins[0][0] + 1, k) * coins[0][2]
+        
+        coins.sort(key=lambda x: x[0])
+        
+        max_coins = 0
+        cur = 0
+        start = coins[0][0]
+        end = start + k - 1
+        
+        left = 0
+        right = 0
+        
+        while right < n:
+            rs, re, r_val = coins[right]
+            
+            if end >= re:
+                cur += (re - rs + 1) * r_val
+                right += 1
+                continue
+            
+            max_coins = max(max_coins, (end - rs + 1) * r_val + cur if rs <= end else cur)
+            
+            le, l_val = coins[left][1], coins[left][2]
+            new_start = re - k + 1
+            
+            if new_start <= le:
+                cur += (re - rs + 1) * r_val - (new_start - start) * l_val
+                end = re
+                start = new_start
+                right += 1
+            else:
+                cur -= (le - start + 1) * l_val
+                left += 1
+                start = coins[left][0]
+                end = start + k - 1
+        
+        return max(max_coins, cur)
