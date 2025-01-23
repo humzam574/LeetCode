881f1073f1d61f34a7eee835ba39705fb3717e56
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-23 06:53:51 +0500

    Time: 17 ms (60.30%) | Memory: 19.5 MB (74.16%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -4,12 +4,15 @@ class Solution:
         rows = [0] * m
         cols = [0] * n
         tot = 0
+        #false -> true -> false -> false -> false -> false
         for x in range(m):
             for y in range(n):
                 if grid[x][y] == 1:
                     tot += 1
-                    rows[x]+=1
-                    cols[y]+=1
+                    if rows[x] != 2:
+                        rows[x] += 1
+                    if cols[y] != 2:
+                        cols[y] += 1
         for x in range(m):
             for y in range(n):
                 if rows[x] == 1 and cols[y] == 1 and grid[x][y] == 1:
