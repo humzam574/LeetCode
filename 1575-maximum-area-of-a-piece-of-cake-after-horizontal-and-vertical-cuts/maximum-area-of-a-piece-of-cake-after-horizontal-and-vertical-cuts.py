9da6908db9624323304b283b50eb54a942e69084
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-23 08:12:47 +0500

    Time: 39 ms (94.19%) | Memory: 28.8 MB (48.26%) - LeetSync

--- 1575-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1575-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,6 +1,16 @@
 class Solution:
-    def maxArea(self, h: int, w: int, hor: List[int], ver: List[int]) -> int:
-        hor.sort(); ver.sort(); hor.append(h); ver.append(w); hor.append(0); ver.append(0); d1, d2, n = 0, 0, 1000000007
-        for i in range(len(hor)): d1 = max(d1, hor[i] - hor[i - 1])
-        for i in range(len(ver)): d2 = max(d2, ver[i] - ver[i - 1])
-        return (d1%n) * (d2%n) % n
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        mod = pow(10,9) + 7
+        def max_interval(length:int, cuts: List[int]) -> int:
+            max_interval = 0
+            cuts.sort()
+            prev = 0
+            for cut in cuts:
+                if cut - prev > max_interval:
+                    max_interval = max(cut - prev, max_interval)
+                prev = cut
+            max_interval = max(length - prev, max_interval)
+            return max_interval
+        horizontal_max_interval = max_interval(h, horizontalCuts) % mod
+        vertical_max_interval = max_interval(w, verticalCuts) % mod
+        return (horizontal_max_interval * vertical_max_interval) % mod
