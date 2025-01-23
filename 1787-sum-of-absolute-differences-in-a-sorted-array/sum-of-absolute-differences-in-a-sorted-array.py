276d35892cc43e75974f3b4211ccc11b8e21a4ff
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-23 07:59:06 +0500

    Time: 632 ms (50.36%) | Memory: 33.8 MB (14.52%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,25 +1,11 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        #for all nums after
-        #future sum - (n-i - 1)*nums[i] + (i)*nums[i]-pre(sum)
-
-        #28 - 4*1 = 24
-        #24 - 3*4 + 1*4-1
-        #18 - 2*6 + 2*6 - 5
-        #10 - 8*1 + 8*3 - 11
-        #0 - 0 + 4*10 - 19
-
-
         n = len(nums)
-        prefix = [0] * n
-        for i in range(1, n):
-            prefix[i] = prefix[i - 1] + nums[i - 1]
+        curr = -nums[-1]
         suffix = [0] * n
         for i in range(n - 2, -1, -1):
             suffix[i] = suffix[i + 1] + nums[i + 1]
-        ans = [0] * n
-        #print(prefix)
-        #print(suffix)
         for i in range(n):
-            ans[i] = suffix[i] - prefix[i] + (1 + 2*i - n)*nums[i]# + i*nums[i]
-        return ans
+            curr += nums[i - 1]
+            suffix[i] += (1 + 2*i - n)*nums[i] - curr
+        return suffix
