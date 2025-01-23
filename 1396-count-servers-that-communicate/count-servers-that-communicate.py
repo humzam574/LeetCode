51acf3428aae76d46618142e65c0b807a688980d
humzam574 <65996060+humzam574@users.noreply.github.com>
2025-01-23 08:17:57 +0500

    Time: 3 ms (98.88%) | Memory: 19.7 MB (35.58%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -1,18 +1,18 @@
 class Solution:
-    def countServers(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        rows = [0] * m
-        cols = [0] * n
-        tot = 0
-        #false -> true -> false -> false -> false -> false
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y] == 1:
-                    tot += 1
-                    rows[x] += 1
-                    cols[y] += 1
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y] == 1 and rows[x] == 1 and cols[y] == 1:
-                    tot -= 1
-        return tot
+  def countServers(self, G: List[List[int]]) -> int:
+    count = 0
+    for r in range(len(G)):
+      s = sum(G[r])
+      if s > 1:
+        count += s
+      elif s == 1:
+        column = G[r].index(1)
+        if sum(G[r][column] for r in range(len(G))) > 1:
+          count += 1
+       
+    return count
+
+      
+
+      
+        
