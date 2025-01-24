27a9ae131ea77906de2464bb5b278c33ff0b4433
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-01-23 19:28:25 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.68%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -2,9 +2,7 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack, arr, ans = path.split("/"), [], "/"
         for item in stack:
-            if item == '' or item == '.': continue
-            elif item == '..': 
-                if len(arr) > 0: arr.pop()
-            else: arr.append(item)
+            if item == '..': len(arr) > 0 and arr.pop()
+            elif item != '' and item != '.': arr.append(item)
         for item in arr: ans+=(item + "/")
         return ans[:-1] if len(ans) > 1 else "/"
