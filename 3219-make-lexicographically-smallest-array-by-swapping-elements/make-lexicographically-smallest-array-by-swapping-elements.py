4c1926b3d3c2ff34b1bd5c818d961b09576bc3f2
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-01-24 22:41:23 -0800

    Time: 147 ms (97.92%) | Memory: 46.3 MB (86.11%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,29 +1,24 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         n = len(nums)
-        # Pair each number with its index and sort by the number
-        sorted_enum = sorted((num, i) for i, num in enumerate(nums))
-        
-        new_positions = []
-        curr_positions = []
-        prev = float('-inf')
-        
-        for num, idx in sorted_enum:
-            # If the current number exceeds the previous number by more than the limit,
-            # sort and append the current positions to the result
-            if num > prev + limit:
-                new_positions.extend(sorted(curr_positions))
-                curr_positions = [idx]
-            else:
-                curr_positions.append(idx)
-            prev = num
-        
-        # Append any remaining positions
-        new_positions.extend(sorted(curr_positions))
-        
-        # Construct the result array using the new positions
-        res = [0] * n
-        for i, idx in enumerate(new_positions):
-            res[idx] = sorted_enum[i][0]
-        
-        return res
+        ordered_nums = sorted(nums)
+        prev = ordered_nums[0]
+        num_to_group = {}
+        current_group = 0
+        group_start = [0]
+
+        for i, x in enumerate(ordered_nums):
+            if x - prev > limit:
+                current_group += 1
+                group_start.append(i)
+
+            num_to_group[x] = current_group
+            prev = x
+            
+        result = []
+        for x in nums:
+            group = num_to_group[x]
+            result.append(ordered_nums[group_start[group]])
+            group_start[group] += 1
+
+        return result
