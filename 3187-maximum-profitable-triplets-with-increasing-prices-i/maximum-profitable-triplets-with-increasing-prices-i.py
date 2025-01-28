502b55476c01193d3ab20057b661a237e496a2e5
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-01-27 21:14:13 -0800

    Time: 4502 ms (51.35%) | Memory: 18.5 MB (29.73%) - LeetSync

--- 3187-maximum-profitable-triplets-with-increasing-prices-i/maximum-profitable-triplets-with-increasing-prices-i.py
+++ 3187-maximum-profitable-triplets-with-increasing-prices-i/maximum-profitable-triplets-with-increasing-prices-i.py
@@ -2,12 +2,14 @@ class Solution:
     def maxProfit(self, prices: List[int], profits: List[int]) -> int:
         n = len(prices)
         ans = -1
+        minl = inf
         for m in range(1, n - 1):
             hl = -1
+            minl = min(minl, prices[m - 1])
+            if minl >= prices[m]: continue
             for l in range(m):
                 if prices[l] < prices[m]:
                     hl = max(hl, profits[l])
-            if hl == -1: continue
             hr = -1
             for r in range(m + 1, n):
                 if prices[r] > prices[m]:
