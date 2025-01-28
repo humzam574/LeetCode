c6b8575a657ed1111e53b0e527272a5328a2e56e
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-01-27 20:10:29 -0800

    Time: 1400 ms (28.15%) | Memory: 17.7 MB (66.99%) - LeetSync

---
+++ 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarrayLCM(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            curr = nums[i]
+            for j in range(i, n):
+                curr = lcm(curr, nums[j])
+                if curr == k:
+                    ans += 1
+        return ans
