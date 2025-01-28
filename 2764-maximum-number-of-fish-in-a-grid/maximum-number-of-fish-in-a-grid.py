e2508bee1ac7cbd3ee1b646aa62d3343dc6312b8
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-01-27 16:25:26 -0800

    Time: 51 ms (34.15%) | Memory: 18.2 MB (18.78%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -1,21 +1,24 @@
 class Solution:
     def findMaxFish(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        ans = 0
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y]:
-                    curr = grid[x][y]
-                    grid[x][y] = 0
-                    dq = deque([(x,y)])
-                    while dq:
-                        r, c = dq.popleft()
-                        curr += grid[r][c]
-                        grid[r][c] = 0
-                        for dr, dc in ((1,0), (0,1), (-1,0), (0,-1)):
-                            nr, nc = r+dr, c+dc
-                            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc]:
-                                dq.append((nr,nc))
-                    ans = max(ans, curr)
-        return ans
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+
+        def dfs(row, col):
+            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == 0 or (row, col) in visited:
+                return 0
+            
+            visited.add((row, col))
+            
+            res = 0
+            for direction in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                res += dfs(row + direction[0], col + direction[1])
+            
+            return res + grid[row][col]
+        
+        res = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] > 0 and (i, j) not in visited:
+                    res = max(res, dfs(i, j))
+
+        return res
