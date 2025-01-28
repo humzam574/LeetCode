f2d4e2f2c22b545b727a347080ec16a3f59579e9
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-01-27 16:26:05 -0800

    Time: 28 ms (90.00%) | Memory: 17.8 MB (74.88%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -1,24 +1,21 @@
 class Solution:
+    def dfs(self, grid, i, j):
+        nrow, ncol = len(grid), len(grid[0])
+        if i < 0 or j < 0 or i >= nrow or j >= ncol:
+            return 0
+        if grid[i][j] == 0:
+            return 0
+        temp = grid[i][j]
+        grid[i][j] = 0
+        return temp + self.dfs(grid, i+1, j) + self.dfs(grid, i-1, j) + self.dfs(grid, i, j+1) + self.dfs(grid, i, j-1)
+
     def findMaxFish(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
-
-        def dfs(row, col):
-            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == 0 or (row, col) in visited:
-                return 0
-            
-            visited.add((row, col))
-            
-            res = 0
-            for direction in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                res += dfs(row + direction[0], col + direction[1])
-            
-            return res + grid[row][col]
-        
         res = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] > 0 and (i, j) not in visited:
-                    res = max(res, dfs(i, j))
-
+        nrow, ncol = len(grid), len(grid[0])
+        for i in range(nrow):
+            for j in range(ncol):
+                if grid[i][j] != 0:
+                    res = max(res, self.dfs(grid, i, j))
         return res
+
+        
