13bd0f440654bc50b6ae030949b0c56c55286775
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-01-30 18:17:45 -0800

    Time: 444 ms (98.77%) | Memory: 27.2 MB (90.95%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,32 +1,16 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        #answer is dependent on a few cases
-        #case 1: the entire grind is island. ans = m * n
-        #case 2: there are no islands. return 1
-        #case 3: there is only one island or no islands can be connected. ans = max(islands) + 1
-        #case 4: islands can be connected. ans = 1 + i1 + i2 + i3 + i4
-        #in case three, you want to search through every single index x,y and if
-        #1. grid[x][y] == 0
-        #2. grid[dx][dy] == island for 2-4 dx,dy combos
-        #then ans = sum(islands) + 1
-
-        #use a bfs to go through and label each island with a numbering system, from 2 onwards
-        #if there is only one island of 
-        #then do a left-right, top-down search and see if you can merge islands
-
         sizes = [-1, -1]
         label = 2
         n = len(grid)
         for i in range(n):
             for j in range(n):
                 if grid[i][j] == 1:
-                    #bfs
                     dq = deque([(i, j)])
                     grid[i][j] = label
                     size = 0
                     while dq:
                         x, y = dq.popleft()
-                        #grid[x][y] = label
                         size += 1
                         for dx, dy in ((1,0), (0,1), (-1,0), (0,-1)):
                             nx, ny = x+dx, y+dy
@@ -39,15 +23,7 @@ class Solution:
             return 1
         if len(sizes) == 3:
             return min(n*n, sizes[2]+1)
-        #in case three, you want to search through every single index x,y and if
-        #1. grid[x][y] == 0
-        #2. grid[dx][dy] == island for 2-4 dx,dy combos
-        #then ans = sum(islands) + 1
         ans = 0
-        # print(sizes)
-        # print()
-        # for row in grid:
-        #     print(row)
         for x in range(n):
             for y in range(n):
                 if grid[x][y] == 0:
