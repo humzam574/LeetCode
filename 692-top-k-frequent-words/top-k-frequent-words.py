281f74ffc8d5c772cb597350a91b1ca447a723fb
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-01 15:59:04 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (15.15%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,32 +1,14 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        dict = Counter(words)
-        freq = defaultdict(list)
-        for key, v in dict.items():
-            freq[v].append(key)
-        for key, v in freq.items():
-            freq[key].sort()#reverse = True)
-        f = sorted(freq.keys(), reverse = True)
-        ans = []
-        #print(f)
-        #print(freq)
-        for i in f:
+        dict, freq, ans = Counter(words), defaultdict(list), []
+        for key, v in dict.items(): freq[v].append(key)
+        for key, v in freq.items(): freq[key].sort()
+        for i in sorted(freq.keys(), reverse = True):
             arr = freq[i]
             if len(arr) > k:
-                #print(len(arr))
-                #print(k)
-                for item in arr[:k]:
-                    ans.append(item)
-                #ans = ans + arr[:k]
-                k = 0
+                for item in arr[:k]: ans.append(item)
             else:
-                #print("whole thing")
-                for item in arr:
-                    ans.append(item)
-                k -= len(arr)
-            
-            #print(arr)
-            #print(k)
-            if k <= 0:
-                break
+                for item in arr: ans.append(item)
+            k -= len(arr)
+            if k <= 0: break
         return ans
