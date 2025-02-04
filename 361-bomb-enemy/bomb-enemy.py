04daf55d1e5ddca89fc7a12bff859a0dc5e4c7e0
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 14:50:16 -0800

    Time: 84 ms (97.80%) | Memory: 45.3 MB (87.91%) - LeetSync

--- 361-bomb-enemy/bomb-enemy.py
+++ 361-bomb-enemy/bomb-enemy.py
@@ -1,16 +1,33 @@
+from collections import defaultdict
 class Solution:
     def maxKilledEnemies(self, grid: List[List[str]]) -> int:
-        m, n, ans = len(grid), len(grid[0]), 0
-        for x in range(m):
-            curr = prev = 0
-            for i in range(n):
-                for j in range(prev,i if grid[x][i] == 'W' else -1): grid[x][j] = curr if grid[x][j] == '0' else grid[x][j]
-                curr, prev = curr + int(grid[x][i] == 'E') if grid[x][i] != 'W' else 0, i + 1 if grid[x][i] == 'W' else prev
-            for j in range(prev, n): grid[x][j] = curr if grid[x][j] == '0' else grid[x][j]
-        for y in range(n):
-            curr = prev = 0
-            for i in range(m):
-                for j in range(prev, i if grid[i][y] == 'W' else -1): grid[j][y] = grid[j][y] + curr if isinstance(grid[j][y], int) else curr if grid[j][y] == '0' else grid[j][y]
-                curr, prev = curr + (grid[i][y] == 'E') if grid[i][y] != 'W' else 0, prev if grid[i][y] != 'W' else i + 1
-            for j in range(prev, m): grid[j][y] = grid[j][y] + curr if isinstance(grid[j][y], int) else curr if grid[j][y] == '0' else grid[j][y]
-        return max(max([item if isinstance(item, int) else 0 for item in row]) for row in grid)
+        m = len(grid)
+        n = len(grid[0])
+        dprows = [[0] for _ in range(m)]
+        dpcols = [[0] for _ in range(n)]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                
+                if grid[i][j] == 'E':
+                    
+                    dprows[i][-1] += 1
+                    dpcols[j][-1] += 1
+                elif grid[i][j] == 'W':
+                    dprows[i].append(0)
+                    dpcols[j].append(0)
+                    
+       
+        rowidx = [0] * m
+        colidx = [0] * n
+       
+        result = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '0':
+                    result = max(result, dprows[i][rowidx[i]] + dpcols[j][colidx[j]])
+ 
+                elif grid[i][j] == 'W':
+                    rowidx[i] += 1
+                    colidx[j] += 1
+
+        return result
