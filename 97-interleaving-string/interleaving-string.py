7c585a792262d7fe8c2f12451f310d28f71eee0c
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 01:21:37 -0800

    Time: 42 ms (72.07%) | Memory: 18.4 MB (34.04%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -3,4 +3,4 @@ class Solution:
         self.x, self.y, self.z = len(s1), len(s2), len(s3)
         @cache
         def dp(i, j, k): return (i == self.x and j == self.y) if k == self.z else s2[j:] == s3[k:] if i == self.x else s1[i:] == s3[k:] if j == self.y else dp(i + 1, j, k + 1) or dp(i, j + 1, k + 1) if s1[i] == s3[k] and s2[j] == s3[k] else dp(i + 1, j, k + 1) if s1[i] == s3[k] else dp(i, j + 1, k + 1) if s2[j] == s3[k] else False
-        return self.x + self.y == self.z and dp(0, 0, 0)
+        return dp(0, 0, 0)
