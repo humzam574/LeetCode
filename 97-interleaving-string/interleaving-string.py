ed76e7ac5cfc137fec43ab844e501e9db47d1a6b
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 01:21:29 -0800

    Time: 44 ms (61.72%) | Memory: 18.5 MB (27.72%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,24 +1,6 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        #memoized dp
         self.x, self.y, self.z = len(s1), len(s2), len(s3)
-        if self.x + self.y != self.z:
-            return False
         @cache
-        def dp(i, j, k):
-            if k == self.z:
-                return (i == self.x and j == self.y)
-            if i == self.x:
-                #return self.y - j == self.z - k and s2[j:] == s3[k:]
-                return s2[j:] == s3[k:]
-            if j == self.y:
-                #return self.x - i == self.z - k and s1[i:] == s3[k:]
-                return s1[i:] == s3[k:]
-            if s1[i] == s3[k] and s2[j] == s3[k]:
-                return dp(i + 1, j, k + 1) or dp(i, j + 1, k + 1)
-            elif s1[i] == s3[k]:
-                return dp(i + 1, j, k + 1)
-            elif s2[j] == s3[k]:
-                return dp(i, j + 1, k + 1)
-            return False
-        return dp(0, 0, 0)
+        def dp(i, j, k): return (i == self.x and j == self.y) if k == self.z else s2[j:] == s3[k:] if i == self.x else s1[i:] == s3[k:] if j == self.y else dp(i + 1, j, k + 1) or dp(i, j + 1, k + 1) if s1[i] == s3[k] and s2[j] == s3[k] else dp(i + 1, j, k + 1) if s1[i] == s3[k] else dp(i, j + 1, k + 1) if s2[j] == s3[k] else False
+        return self.x + self.y == self.z and dp(0, 0, 0)
