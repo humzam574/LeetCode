85a08eec487e54ec23b6b60f753c9ad6ad61ba27
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 00:41:17 -0800

    Time: 41 ms (100.00%) | Memory: 27.7 MB (54.89%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.py
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.py
@@ -1,7 +1,11 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        curr = ans = 0
+        prev_sum = 0
+        min_maximum = 0
         for i in range(len(nums)):
-            curr += nums[i]
-            ans = max(ans, curr // (i+1) + (curr % (i+1) != 0))
-        return int(ans)
+            prev_sum += nums[i]
+            if nums[i] > min_maximum:
+                new_maximum = (prev_sum + i) // (i + 1)
+                if new_maximum > min_maximum:
+                    min_maximum = new_maximum
+        return min_maximum
