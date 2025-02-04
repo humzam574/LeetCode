537f42753439645db2e6a73c709d5d1415188a1a
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 01:09:27 -0800

    Time: 7 ms (82.53%) | Memory: 19.9 MB (36.80%) - LeetSync

--- 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
@@ -1,7 +1,3 @@
 class Solution:
-    def arrangeWords(self, text: str) -> str:
-        text = chr(ord(text[0]) + ord('a') - ord('A')) + text[1:]
-        arr = sorted(text.split(), key = lambda x : len(x))
-        arr[0] = chr(ord(arr[0][0]) - ord('a') + ord('A')) + arr[0][1:]
-        return ' '.join(arr)
+    def arrangeWords(self, text: str) -> str: arr = sorted((chr(ord(text[0]) + ord('a') - ord('A')) + text[1:]).split(), key = lambda x : len(x)); arr[0] = chr(ord(arr[0][0]) - ord('a') + ord('A')) + arr[0][1:]; return ' '.join(arr)
         
