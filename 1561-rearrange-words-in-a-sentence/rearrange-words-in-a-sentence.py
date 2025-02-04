a314f19f41c5d40fc2422a8df10fa2cd87c1a61e
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 01:09:39 -0800

    Time: 5 ms (86.99%) | Memory: 19.5 MB (68.77%) - LeetSync

--- 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
@@ -1,3 +1,13 @@
 class Solution:
-    def arrangeWords(self, text: str) -> str: arr = sorted((chr(ord(text[0]) + ord('a') - ord('A')) + text[1:]).split(), key = lambda x : len(x)); arr[0] = chr(ord(arr[0][0]) - ord('a') + ord('A')) + arr[0][1:]; return ' '.join(arr)
-        
+    def arrangeWords(self, text: str) -> str:
+        words = text.split(" ")
+        words[0] = words[0].lower()
+        max_length = max([len(word) for word in words])
+        buckets = [[] for _ in range(max_length + 1)]
+        for word in words:
+            buckets[len(word)].append(word)
+        res = []
+        for bucket in buckets:
+            res.extend(bucket)
+        res[0] = res[0].capitalize()
+        return " ".join(res)
