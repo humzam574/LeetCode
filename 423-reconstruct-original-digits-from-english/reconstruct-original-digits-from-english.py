a477f7cccfa16880f858ea1a29d42fa60857a9f9
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 20:22:04 -0800

    Time: 13 ms (62.68%) | Memory: 18.2 MB (44.44%) - LeetSync

---
+++ 423-reconstruct-original-digits-from-english/reconstruct-original-digits-from-english.py
@@ -0,0 +1,13 @@
+class Solution:
+    def originalDigits(self, s: str) -> str:
+        dict = Counter(s)
+        print(dict)
+        arr = [['z', 'ero', '0'], ['w', 'to', '2'], ['u', 'for', '4'], ['x', 'si', '6'], ['g', 'eiht', '8'], ['f', 'ive', '5'], ['v', 'seen', '7'], ['i', 'nne', '9'], ['r', 'thee', '3'], ['o', 'ne', '1']]
+        ans = []
+        for i in range(10):
+            if arr[i][0] in dict and dict[arr[i][0]] != 0:
+                val = dict[arr[i][0]]
+                ans = ans + [arr[i][2]] * val
+                for char in arr[i][1]:
+                    dict[char] -= val
+        return ''.join(sorted(ans))
