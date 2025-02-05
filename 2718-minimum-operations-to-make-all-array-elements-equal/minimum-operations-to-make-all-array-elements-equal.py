f5644ed1b2a2f91f532f954e60f1e6549dd3ad39
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 19:38:47 -0800

    Time: 777 ms (46.05%) | Memory: 45.8 MB (34.87%) - LeetSync

--- 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
@@ -3,34 +3,14 @@ class Solution:
         nums.sort()
         ans = [0] * len(queries)
         n = len(nums)
-        
+        low, high = nums[0], nums[-1]
         prefix = list(nums)
         for i in range(1, len(nums)):
             prefix[i] += prefix[i - 1]
-        # print(nums)
-        # print(prefix)
         for i,q in enumerate(queries):
-            #prev*q - post*q
-            #q(i - 1) - q(len - i)
-            #postsum - presum
-
-
-
-            #1, 3, 6, 8
-            #i*q - prefix[i - 1] + prefix[-1] - prefix[i - 1] - (n - i)*q
-            idx = bisect_left(nums, q)
-            
-            if idx == 0:
-                ans[i] = prefix[-1] - q*n
-            elif idx == n:
-                ans[i] = q*n - prefix[-1]
+            if q <= low or q >= high:
+                ans[i] = abs(prefix[-1] - q*n)
             else:
-                # print()
-                # print(idx)
-                # print(q)
-                # print(max(nums))
-                # print(i*q - prefix[idx - 1])
-                # print(prefix[-1] - prefix[i - 1] - (n - i)*q)
-                # print()
-                ans[i] = idx*q - prefix[idx - 1] + prefix[-1] - prefix[idx - 1] - (n - idx)*q#(q*(i) - q*(n - i)) + prefix[-1] - prefix[i + 1] - prefix[i - 1]
+                idx = bisect_left(nums, q)
+                ans[i] = idx*q - prefix[idx - 1] + prefix[-1] - prefix[idx - 1] - (n - idx)*q
         return ans
