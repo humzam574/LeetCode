6c78caa8de28f93166a9c49209defb4d9a094409
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-05 14:56:37 -0800

    Time: 7 ms (95.72%) | Memory: 18 MB (28.38%) - LeetSync

--- 1084-find-k-length-substrings-with-no-repeated-characters/find-k-length-substrings-with-no-repeated-characters.py
+++ 1084-find-k-length-substrings-with-no-repeated-characters/find-k-length-substrings-with-no-repeated-characters.py
@@ -1,22 +1,16 @@
 class Solution:
     def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
-        l, r = 0, k
-        curr = Counter(s[:r])
-        n = len(curr.keys())
-        ans = int(n == k)
-        #print(curr)
-        for r in range(k, len(s)):
-            if curr[s[l]] == 1:
-                del curr[s[l]]
-                n -= 1
-            else:
-                curr[s[l]] -= 1
-            curr[s[r]] = curr.get(s[r], 0) + 1
-            if curr[s[r]] == 1:
-                n += 1
-            ans += (n == k)
-            l += 1
-            # print(curr)
-            # print(n)
-            # print()
-        return ans
+        if k > len(s): return 0
+        chars = set()
+        left = 0
+        count = 0
+        for right in range(len(s)):
+            while s[right] in chars:
+                chars.remove(s[left])
+                left += 1
+            chars.add(s[right])
+            if right - left + 1 == k:
+                count += 1
+                chars.remove(s[left])
+                left += 1
+        return count
