c26446c247a9f1ba603a52b74ea46e3aa0683918
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 21:09:19 -0800

    Time: 3 ms (92.40%) | Memory: 23.2 MB (78.15%) - LeetSync

--- 370-range-addition/range-addition.py
+++ 370-range-addition/range-addition.py
@@ -1,9 +1,6 @@
 class Solution:
     def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
         arr = [0] * (length + 1)
-        for l, r, a in updates:
-            arr[l] += a
-            arr[r + 1] -= a
-        for i in range(1, len(arr) - 1):
-            arr[i] = arr[i - 1] + arr[i]
+        for l, r, a in updates: arr[l] += a; arr[r + 1] -= a
+        for i in range(1, len(arr) - 1): arr[i] = arr[i - 1] + arr[i]
         return arr[:-1]
