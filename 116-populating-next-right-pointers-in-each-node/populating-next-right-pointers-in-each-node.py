52251f90182308be709770a77a4f0a397be89866
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 18:25:28 -0800

    Time: 49 ms (67.70%) | Memory: 19.4 MB (17.17%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -10,19 +10,25 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root: return
-        if not root.right: return root
-        dq = deque([root.left, root.right])
-        while dq:
-            print(dq)
-            for i in range(len(dq) - 1):
-                dq[0].next = dq[1]
-                if dq[0].right:
-                    dq.append(dq[0].left)
-                    dq.append(dq[0].right)
-                dq.popleft()
-            if dq[0].right:
-                    dq.append(dq[0].left)
-                    dq.append(dq[0].right)
-            dq.popleft()
+        if not root:
+            return None
+        
+        lev = root
+        
+        while lev:
+            dummy = Node(0)
+            child = dummy
+            
+            while lev:
+                if lev.left:
+                    child.next = lev.left
+                    child = child.next
+                    
+                if lev.right:
+                    child.next = lev.right
+                    child = child.next
+                    
+                lev = lev.next
+            lev = dummy.next
+            
         return root
