7c940764a70a5fcbce9171f8ac4592e8a043326c
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 22:03:12 -0800

    Time: 961 ms (26.23%) | Memory: 22 MB (40.22%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,6 +1,6 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        l, r = 0, min(ranks) * cars * cars
+        l, r = 1, min(ranks) * cars * cars
         while l < r:
             m = (l + r) // 2
             if sum(math.floor((m/v)**(0.5)) for v in ranks) < cars: l = m + 1
