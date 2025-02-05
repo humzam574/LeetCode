6d02030777a04912ba8c6f4e0d981090f657ac43
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 21:15:21 -0800

    Time: 54 ms (53.85%) | Memory: 17.6 MB (60.26%) - LeetSync

---
+++ 3370-guess-the-number-using-bitwise-questions-i/guess-the-number-using-bitwise-questions-i.py
@@ -0,0 +1,9 @@
+# Definition of commonSetBits API.
+# def commonSetBits(num: int) -> int:
+
+class Solution:
+    def findNumber(self) -> int:
+        ans = 0
+        for i in range(31):
+            if commonSetBits(1 << i): ans |= 1 << i
+        return ans
