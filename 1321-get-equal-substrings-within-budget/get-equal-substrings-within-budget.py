680ce683d678358b2349aa5cb008de7606e800f2
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 21:02:24 -0800

    Time: 27 ms (72.25%) | Memory: 18.7 MB (21.18%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -1,15 +1,8 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        arr = [0] * len(s)
-        for i in range(len(s)):
-            arr[i] = abs(ord(s[i]) - ord(t[i]))
-        ans = 0
-        l = 0
-        curr = 0
+        arr, ans, l, curr = [abs(ord(s[i]) - ord(t[i])) for i in range(len(s))], 0, 0, 0
         for r in range(len(s)):
             curr += arr[r]
-            while l <= r and curr > maxCost:
-                curr -= arr[l]
-                l += 1
+            while l <= r and curr > maxCost: curr, l = curr - arr[l], l + 1
             ans = max(ans, r - l + 1)
         return ans
