789225abc82c1146789964380e76e0f6e0402190
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 22:02:25 -0800

    Time: 919 ms (29.56%) | Memory: 21.7 MB (68.44%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,41 +1,8 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        #pick a minutes m
-        #floor 
-        #take the square root
-        #temp = cars
-        #temp -= root(m / r)
-
-        l = 0
-        r = min(ranks) * cars * cars
-        #r = 1024
-        potential = [inf]
+        l, r = 0, min(ranks) * cars * cars
         while l < r:
             m = (l + r) // 2
-            
-            temp = cars
-            for v in ranks:
-                #r * n^2 = m
-                temp = temp - int(math.sqrt(m/v))
-            #print(str(l) + " " + str(r) + " " + str(m) + " " + str(temp))
-            if temp <= 0:
-                potential.append(m)
-            if temp > 0:
-                l = m + 1
-                #r = m - 1
-            else:
-                r = m - 1
-                #l = m + 1
-        # print(potential)
-        # print(l)
-        # print(r)
-        t1 = cars
-        t2 = cars
-        for v in ranks:
-            t1 -= int(math.sqrt(l/v))
-            t2 -= int(math.sqrt(r/v))
-        if t1 <= 0:
-            potential.append(l)
-        if t2 <= 0:
-            potential.append(r)
-        return min(potential)
+            if sum(int(math.sqrt(m/v)) for v in ranks) < cars: l = m + 1
+            else: r = m
+        return l
