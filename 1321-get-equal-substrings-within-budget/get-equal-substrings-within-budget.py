021320726c16bcb8d08901330897496589c4198c
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 21:02:45 -0800

    Time: 17 ms (97.86%) | Memory: 18.1 MB (57.72%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -1,8 +1,9 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        arr, ans, l, curr = [abs(ord(s[i]) - ord(t[i])) for i in range(len(s))], 0, 0, 0
+        l = 0
         for r in range(len(s)):
-            curr += arr[r]
-            while l <= r and curr > maxCost: curr, l = curr - arr[l], l + 1
-            ans = max(ans, r - l + 1)
-        return ans
+            maxCost -= abs(ord(s[r]) - ord(t[r]))
+            if maxCost < 0:
+                maxCost += abs(ord(s[l]) - ord(t[l]))
+                l += 1
+        return len(s) - l
