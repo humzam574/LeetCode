376c32234da176f94354bdee10f9a9d26a9d61fc
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 16:28:09 -0800

    Time: 12 ms (31.47%) | Memory: 18.4 MB (47.98%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.py
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.py
@@ -1,19 +1,6 @@
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
-        #handle edge cases and find the longest gap
-        #if odd number of zeros, return num // 2
-        #if even number of zeros, return num // 2
-        first = -1
-        last = -1
-        prev = -1
-        high = -1
+        first, last, prev, high = 20000, -1, -1, -1
         for i in range(len(seats)):
-            if seats[i]:
-                last = i
-                if first == -1:
-                    first = i
-                high = max(high, i - prev)
-                prev = i
-        #high = max(high, i - prev)
-        #print(high)
+            if seats[i]: last, first, high, prev = i, min(first, i), max(high, i - prev), i
         return max(high // 2, i - last, first)
