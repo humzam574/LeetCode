6c41039562634e0492db0430bfb421e113fd5b3e
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 22:31:09 -0800

    Time: 31 ms (98.17%) | Memory: 21.7 MB (50.00%) - LeetSync

--- 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.py
+++ 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        curr = 0
+        curr, targ = 0, 1
         for num in nums:
-            if (num + curr) % 2 == 0: curr += 1
+            if num != targ: curr, targ = curr + 1, 1 - targ
         return curr
