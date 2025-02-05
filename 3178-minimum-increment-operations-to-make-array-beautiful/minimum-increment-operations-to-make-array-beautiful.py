30c0edb203600ddf85325ed23e144dc85ae643c0
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 22:19:11 -0800

    Time: 150 ms (71.43%) | Memory: 33.7 MB (56.04%) - LeetSync

---
+++ 3178-minimum-increment-operations-to-make-array-beautiful/minimum-increment-operations-to-make-array-beautiful.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minIncrementOperations(self, nums: List[int], k: int) -> int:
+        dp = [0] * len(nums)
+        dp[0] = max(0, k - nums[0])
+        dp[1] = max(0, k - nums[1])
+        dp[2] = max(0, k - nums[2])
+        for i in range(3, len(dp)):
+            temp = k - nums[i]
+            if temp >= 0:
+                dp[i] = temp + min(dp[i - 3], dp[i - 2], dp[i - 1])
+            else:
+                dp[i] = min(dp[i - 3], dp[i - 2], dp[i - 1])
+        print(dp)
+        return min(dp[-2], dp[-1], dp[-3])
