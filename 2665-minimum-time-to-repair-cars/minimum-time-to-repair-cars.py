34613c24e5808119f96917b46232564190bf100a
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 22:02:52 -0800

    Time: 964 ms (26.00%) | Memory: 21.9 MB (62.67%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -3,6 +3,6 @@ class Solution:
         l, r = 0, min(ranks) * cars * cars
         while l < r:
             m = (l + r) // 2
-            if sum(int(math.sqrt(m/v)) for v in ranks) < cars: l = m + 1
+            if sum(math.floor((m/v)**(0.5)) for v in ranks) < cars: l = m + 1
             else: r = m
         return l
