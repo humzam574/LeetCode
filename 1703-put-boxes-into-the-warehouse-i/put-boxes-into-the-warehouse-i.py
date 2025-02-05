2fb2b9dc5f240c808013c4276f14189d2dc982ee
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-05 14:48:11 -0800

    Time: 45 ms (88.24%) | Memory: 37.6 MB (27.45%) - LeetSync

--- 1703-put-boxes-into-the-warehouse-i/put-boxes-into-the-warehouse-i.py
+++ 1703-put-boxes-into-the-warehouse-i/put-boxes-into-the-warehouse-i.py
@@ -1,19 +1,6 @@
 class Solution:
     def maxBoxesInWarehouse(self, boxes: List[int], warehouse: List[int]) -> int:
-        boxes.sort()
-        b, w = 0, 0
-        n = len(warehouse)
-        ans = 0
-        mins = [0] * n
-        mins[0] = warehouse[0]
-        
-        for i in range(1, n):
-            mins[i] = min(warehouse[i], mins[i - 1])
-        mins = mins[::-1]
-        #print(mins)
-        while b < len(boxes) and w < len(mins):
-            if boxes[b] <= mins[w]:
-                ans += 1
-                b += 1
-            w += 1
-        return ans
+        k = 0
+        for box in sorted(boxes, reverse=True): 
+            if k < len(warehouse) and box <= warehouse[k]: k += 1
+        return k
