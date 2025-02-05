6134b775284add1cb4ca8885e0bf5e58578ebca9
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-05 14:25:04 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (14.71%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,26 +1,13 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
-        a1 = sentence1.split()
-        a2 = sentence2.split()
-        #two pointers
-        #keep going on each side until you dont match anymore
-        #check to see if l >= r
-        if len(a1) > len(a2):
-            a1, a2 = a2, a1
-        l, r = 0, -1
-        p1, p2 = 0, len(a2) - 1
-        # print(a1)
-        # print(a2)
+        a1, a2, l, r = sentence1.split(), sentence2.split(), 0, -1
+        if len(a1) > len(a2): a1, a2 = a2, a1
         while l <= (len(a1) + r):
-            #print(str(l) + " " + str(r))
             cont = False
             if a1[l] == a2[l]:
-                cont = True
-                l += 1
+                cont, l = True, l + 1
             if a1[r] == a2[r]:
-                cont = True
-                r -= 1
-            if not cont:
-                return False
-        return l >= r
+                cont, r = True, r - 1
+            if not cont: return False
+        return True
             
