9edcbcc0f9bf7a4a5835eaa113df5a9232a8d27d
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-04 21:57:43 -0800

    Time: 1569 ms (5.12%) | Memory: 22 MB (40.22%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -0,0 +1,41 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        #pick a minutes m
+        #floor 
+        #take the square root
+        #temp = cars
+        #temp -= root(m / r)
+
+        l = 0
+        r = 18446744073709551616
+        #r = 1024
+        potential = [inf]
+        while l < r:
+            m = (l + r) // 2
+            
+            temp = cars
+            for v in ranks:
+                #r * n^2 = m
+                temp = temp - int(math.sqrt(m/v))
+            #print(str(l) + " " + str(r) + " " + str(m) + " " + str(temp))
+            if temp <= 0:
+                potential.append(m)
+            if temp > 0:
+                l = m + 1
+                #r = m - 1
+            else:
+                r = m - 1
+                #l = m + 1
+        # print(potential)
+        # print(l)
+        # print(r)
+        t1 = cars
+        t2 = cars
+        for v in ranks:
+            t1 -= int(math.sqrt(l/v))
+            t2 -= int(math.sqrt(r/v))
+        if t1 <= 0:
+            potential.append(l)
+        if t2 <= 0:
+            potential.append(r)
+        return min(potential)
