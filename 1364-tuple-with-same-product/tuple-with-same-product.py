fedc21f7903401412596ed1f9b0aaa0c53d0dd4f
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-05 20:00:34 -0800

    Time: 326 ms (81.99%) | Memory: 46.3 MB (78.20%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.py
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -2,13 +2,14 @@ class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
         #create a set with all the combos
         #or a dictionary with key product value pair
-        dict = defaultdict(list)
+        dict = defaultdict(int)
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
-                dict[nums[i]*nums[j]].append({nums[i], nums[j]})
+                dict[nums[i]*nums[j]]+=1
         #print(dict) #n * (n - 1) * 4
         ans = 0
-        for arr in dict.values():
-            ans += (len(arr) * (len(arr) - 1)) * 4
+        for count in dict.values():
+            if count > 1:
+                ans += (count * (count - 1)) * 4
         return ans
