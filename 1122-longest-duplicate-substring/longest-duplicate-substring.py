64dbeb54e16e90e68f15290d3fabdc25303678d7
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-05 20:45:35 -0800

    Time: 796 ms (93.14%) | Memory: 22.1 MB (73.20%) - LeetSync

--- 1122-longest-duplicate-substring/longest-duplicate-substring.py
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.py
@@ -1,38 +1,14 @@
 class Solution:
-    def longestDupSubstring(self, s: str) -> str:
-        def search(v):
-            drop = ord('a')
-            val = 0
-            high = int(26 ** (v-1))
-            for i in range(v):
-                val = val*26 + (ord(s[i]) - drop) 
-            seen = {val}
-            for i in range(v, len(s)):
-                # Remove leftmost character
-                val = val - (ord(s[i-v]) - drop) * high
-                # Shift left and add new character
-                val = val * 26 + (ord(s[i]) - drop)
-                
-                # Check if we've seen this hash before
-                if val in seen:
-                    return s[i-v+1:i+1]
-                seen.add(val)
-            return ""
-        l, r = 0, len(s) - 1
-        st = set()
-        if len(set(s)) == 1:
-            return s[:-1]
-        curr = ""
-        high = search(1)
-        if high:
-            l+=1
-        while l < r:
-            m = (l + r) // 2
-            curr = search(m)
-            if curr:
-                high = curr
-                l = m + 1
-            else:
-                r = m
-            #print(str(l) + " " + str(r) + " " + str(m))
-        return high
+    def longestDupSubstring(self, S: str) -> str:
+        A, mod, res, lo, hi = [ord(c) - ord('a') for c in S], 2**63 - 1, 0, 0, len(S)
+        def test(L):
+            p, cur = pow(26, L, mod), reduce(lambda x, y: (x * 26 + y) % mod, A[:L], 0); seen = {cur}
+            for i in range(L, len(S)):
+                cur = (cur * 26 + A[i] - A[i - L] * p) % mod
+                if cur in seen: return i - L + 1
+                seen.add(cur)
+        while lo < hi:
+            mi = (lo + hi + 1) // 2; pos = test(mi)
+            if pos: lo, res = mi, pos
+            else: hi = mi - 1
+        return S[res:res + lo]
