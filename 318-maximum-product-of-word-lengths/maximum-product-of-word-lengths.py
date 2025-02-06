36fb563612e3f48aa6ad68895df1040bb8c97b80
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-05 22:29:29 -0800

    Time: 107 ms (98.67%) | Memory: 20.6 MB (59.68%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.py
@@ -1,10 +1,23 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        ans = low = 0; n = len(words); words.sort(key = lambda x: len(x))
-        for i in range(n):
-            val = 0
-            for char in words[i]: val |= 1 << (ord(char) - ord('a'))
-            words[i] = (val, len(words[i]))
-            for j in range(low, i):
-                if not words[j][0] & words[i][0]: ans = max(ans, words[j][1] * words[i][1])
-        return ans
+        lengths = defaultdict(int)
+
+        for word in words:
+            mask = 0
+
+            chars = set(c for c in word)
+
+            for c in chars:
+                mask += 1 << ord(c) - ord('a')
+            
+            lengths[mask] = max(lengths[mask], len(word))
+        
+        maxProduct = 0
+
+
+        for m1, l1 in lengths.items():
+            for m2, l2 in lengths.items():
+                if m1 & m2 == 0:
+                    maxProduct = max(maxProduct, l1 * l2)
+        
+        return maxProduct
