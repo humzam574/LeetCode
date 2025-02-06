04d311a58b3e2b17a20cd7822e4be1ff1cc31406
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-05 20:01:56 -0800

    Time: 249 ms (99.53%) | Memory: 46.6 MB (14.69%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.py
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -1,8 +1,2 @@
 class Solution:
-    def tupleSameProduct(self, nums: List[int]) -> int:
-        dict, n, ans = defaultdict(int), len(nums), 0
-        for i in range(n):
-            for j in range(i + 1, n): dict[nums[i]*nums[j]] += 1
-        for count in dict.values():
-            if count > 1: ans += (count * (count - 1)) * 4
-        return ans
+    def tupleSameProduct(self, nums: List[int]) -> int: return 8*sum(map(comb, Counter(starmap(mul, combinations(nums, 2))).values(), cycle([2])))
