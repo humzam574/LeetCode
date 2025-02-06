334a6bbb872fe79098978de3170b2137707bb721
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-06 00:24:49 -0800

    Time: 35 ms (83.33%) | Memory: 34.4 MB (30.56%) - LeetSync

--- 2674-make-the-prefix-sum-non-negative/make-the-prefix-sum-non-negative.py
+++ 2674-make-the-prefix-sum-non-negative/make-the-prefix-sum-non-negative.py
@@ -1,14 +1,8 @@
 class Solution:
     def makePrefSumNonNegative(self, nums: List[int]) -> int:
-        curr = 0
-        ans = 0
-        heap = []
-        heapify(heap)
+        curr, ans, heap = 0, 0, []; heapify(heap)
         for n in nums:
             curr += n
-            if n < 0:
-                heappush(heap, n)
-            if curr < 0:
-                curr -= heappop(heap)
-                ans += 1
+            if n < 0: heappush(heap, n)
+            if curr < 0: curr, ans = curr - heappop(heap), ans + 1
         return ans
