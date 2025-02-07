5d2b117a74728e02ab0d0ffa591d62fc67a9f8c7
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-06 18:26:17 -0800

    Time: 59 ms (96.25%) | Memory: 63.4 MB (31.85%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -1,15 +1,17 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        ans = [0] * len(queries)
-        curr = 0
-        seen = {}
-        counter = defaultdict(int)
-        for i, (x, y) in enumerate(queries):
-            if x in seen:
-                counter[seen[x]] -= 1
-                if counter[seen[x]] == 0:
-                    del counter[seen[x]]
-            seen[x] = y
-            counter[y] += 1
-            ans[i] = len(counter.values())
-        return ans
+        balls = {}
+        colours = {}
+        res = []
+
+        for b, c in queries:
+            if b in balls:
+                if colours[balls[b]] == 1:
+                    del colours[balls[b]]
+                else:
+                    colours[balls[b]] -= 1
+            colours[c] = colours.get(c, 0) + 1
+            balls[b] = c
+            res.append(len(colours))
+        
+        return res
