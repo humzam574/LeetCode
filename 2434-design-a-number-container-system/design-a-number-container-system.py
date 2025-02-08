aba7cff2bdd17e770435c7dbc5d403c87e76ab13
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-07 23:11:34 -0800

    Time: 671 ms (17.16%) | Memory: 151.7 MB (18.14%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -0,0 +1,31 @@
+
+class NumberContainers:
+    
+    def __init__(self):
+        #insert and replace a number
+        #find the smallest index for any given number
+        #dict of heaps
+        #index = number
+        self.nums = defaultdict(SortedSet)
+        self.idx = {}
+        
+    def change(self, index: int, number: int) -> None:
+        if index in self.idx:
+            prev = self.idx[index]
+            self.nums[prev].remove(index)
+            if not self.nums[prev]:
+                del self.nums[prev]
+        self.idx[index] = number
+        self.nums[number].add(index)
+        
+    def find(self, number: int) -> int:
+        if number in self.nums and self.nums[number]:
+            return self.nums[number][0]
+        return -1
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
