448c7f01e133dd40412d4a4d3a4e57b05806716f
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-08 02:18:52 -0800

    Time: 45 ms (100.00%) | Memory: 18 MB (81.42%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
@@ -2,13 +2,17 @@ class Solution:
     def minimumDeletions(self, word: str, k: int) -> int:
         arr = Counter(word).values()
         ans = inf
+        temp = 0
         for x in arr:
             curr = 0
             for a in arr:
                 if a < x:
                     curr += a
                 else:
-                    curr += max(0, a - x - k)
+                    temp = a - x - k
+                    if temp > 0:
+                        curr += temp
+                    #curr += max(0, a - x - k)
             if curr < ans:
                 ans = curr
         return ans
