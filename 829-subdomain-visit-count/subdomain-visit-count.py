b1e7630805ac27c0f77271e54b5240293b7550a5
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-08 01:39:21 -0800

    Time: 6 ms (54.77%) | Memory: 17.7 MB (73.96%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.py
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -1,46 +1,11 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        dict = defaultdict(int)
-        for d in cpdomains:
-            if d.count('.') == 2:
-                one = two = three = ""
-                l = 0
-                m = 0
-                r = 0
-                for i, c in enumerate(d):
-                    if c == " ":
-                        l = i
-                    elif c == ".":
-                        if m == 0:
-                            m = i
-                        else:
-                            r = i
-                            break
-                one = d[r + 1:]
-                two = d[m + 1:]
-                three = d[l + 1:]
-                count = int(d[:l])
-                dict[one] += count
-                dict[two] += count
-                dict[three] += count
-            else:
-                one = two = ""
-                l = 0
-                r = 0
-                for i, c in enumerate(d):
-                    if c == " ":
-                        l = i
-                    elif c == ".":
-                        r = i
-                        break
-                count = int(d[:l])
-                one = d[l+1:]
-                two = d[r+1:]
-                dict[two]+=count
-                dict[one]+=count
-        
-        ans = []
-        for k, v in dict.items():
-            temp = str(v) + " " + k
-            ans.append(temp)
-        return ans
+        domain_counts = defaultdict(int)
+        for cpdomain in cpdomains:
+            times, domains = cpdomain.split()
+            times = int(times)
+            domain_counts[domains] += times
+            while '.' in domains:
+                domains = domains[domains.index('.') + 1:]
+                domain_counts[domains] += times
+        return [str(v) + ' ' + d for d, v in domain_counts.items()]
