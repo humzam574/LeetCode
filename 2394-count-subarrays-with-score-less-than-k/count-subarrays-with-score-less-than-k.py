f4597600bce65f30603d9afabfeec3840fe7ac18
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-08 21:20:43 -0800

    Time: 85 ms (100.00%) | Memory: 30.7 MB (27.72%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
@@ -1,10 +1,24 @@
+from typing import List
+
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        sum, res, j = 0, 0, 0
-        for i, n in enumerate(nums):
-            sum += n
-            while sum * (i - j + 1) >= k:
-                sum -= nums[j]
-                j += 1
-            res += i - j + 1
-        return res
+        left = 0
+        sum_lr = 0
+        len_lr = 0
+        total = 0
+        N = len(nums)
+        
+        for right in range(N):
+            sum_lr += nums[right]
+            len_lr += 1
+            
+            # Adjust the window from the left if the score condition is violated
+            while sum_lr * len_lr >= k:
+                sum_lr -= nums[left]
+                left += 1
+                len_lr -= 1
+            
+            # Add the number of valid subarrays ending at 'right'
+            total += len_lr
+        
+        return total
