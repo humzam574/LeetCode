2d5f31bf89ed0bbaac1811d9b807616bb07d1c40
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-08 21:58:56 -0800

    Time: 120 ms (100.00%) | Memory: 82.8 MB (94.54%) - LeetSync

--- 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -1,29 +1,18 @@
 class Solution:
     def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
-        self.graph = [[] for i in range(n)]
-        self.ans = [0] * n
-        for a, b in edges:
-            self.graph[a].append(b)
-            self.graph[b].append(a)
-        #print(self.graph)
-        def dfs(curr, prev):
-            if not len(self.graph[curr]):
-                arr = [0] * 26
-                idx = ord(labels[curr]) - 97
-                arr[idx] += 1
-                self.ans[curr] += arr[idx]
-            else:
-                arr = [0] * 26
-                for x in self.graph[curr]:
-                    if x == prev:
-                        continue
-                    arr = [a + b for a, b in zip(arr, dfs(x, curr))]
-                idx = ord(labels[curr]) - 97
-                arr[idx] += 1
-                self.ans[curr] += arr[idx]
-            # print(curr)
-            # print(arr)
-            # print()
-            return arr
-        dfs(0, None)
-        return self.ans
+        g = [[] for _ in range(n)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        count = [0] * 26
+        ret = [0] * n
+        def dfs(node, parent):
+            index = ord(labels[node]) - 97
+            prev = count[index]
+            count[index] += 1
+            for child in g[node]:
+                if child != parent:
+                    dfs(child, node)
+            ret[node] = count[index] - prev
+        dfs(0, -1)
+        return ret
