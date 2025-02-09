2d14160e47ab11e677719518600855389f6dd846
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-08 20:58:43 -0800

    Time: 99 ms (90.48%) | Memory: 21.6 MB (69.84%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -7,14 +7,11 @@ class Solution:
         mod = int(1e9 + 7)
         for y in range(1, steps):
             for x in range(m):
-                curr = 0
+                curr = dp[x][y - 1]
                 if x > 0:
                     curr += dp[x - 1][y - 1]
-                curr += dp[x][y - 1]
                 if x < m - 1:
                     curr += dp[x + 1][y - 1]
                 dp[x][y] = curr % mod
-        # for row in dp:
-        #     print(row)
         return dp[0][-1] % mod
         
