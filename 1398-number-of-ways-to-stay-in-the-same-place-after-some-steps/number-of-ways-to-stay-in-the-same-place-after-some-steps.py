4f46d909c43713ec1a94b0444bdb0239333c9145
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-08 20:58:53 -0800

    Time: 27 ms (100.00%) | Memory: 18 MB (82.54%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,17 +1,26 @@
 class Solution:
     def numWays(self, steps: int, arrLen: int) -> int:
-        steps += 1
-        m = min(arrLen, 1 + steps // 2)
-        dp = [[0] * steps for i in range(m)]
-        dp[0][0] = 1
-        mod = int(1e9 + 7)
-        for y in range(1, steps):
-            for x in range(m):
-                curr = dp[x][y - 1]
-                if x > 0:
-                    curr += dp[x - 1][y - 1]
-                if x < m - 1:
-                    curr += dp[x + 1][y - 1]
-                dp[x][y] = curr % mod
-        return dp[0][-1] % mod
-        
+        # Define a modulo constant to prevent integer overflow
+        modulo = 1000000007
+
+        # Calculate the maximum possible position on the staircase that can be reached
+        max_len = min(arrLen, 1 + steps // 2)
+
+        # Initialize an array to store the number of ways to reach each position on the staircase
+        ways = [0] * (max_len + 1)
+
+        # Set the base case: There is one way to stay at the initial position
+        ways[0] = 1
+
+        # Iterate through the number of steps taken
+        for i in range(steps):
+            left = 0
+
+            # Iterate through possible positions on the staircase, considering bounds
+            for j in range(min(max_len, i + 2, steps - i + 3)):
+                # Calculate the number of ways to reach the current position `j`
+                # using the dynamic programming recurrence relation and considering the `left` value
+                left, ways[j] = ways[j], (ways[j] + left + ways[j + 1]) % modulo
+
+        # The `ways[0]` value represents the number of ways to reach the top of the staircase
+        return ways[0]
