9203885e416b882cae9c9041877eac369f372dc8
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-09 18:20:40 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (27.31%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -2,8 +2,6 @@ class Solution:
     def clearDigits(self, s: str) -> str:
         stack = ""
         for i in s:
-            if not i.isdigit():
-                stack = stack + i
-            else:
-                stack = stack[:len(stack)-1]
+            if not i.isdigit(): stack = stack + i
+            else: stack = stack[:len(stack)-1]
         return stack
