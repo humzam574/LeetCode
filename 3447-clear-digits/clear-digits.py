77e8783567f4e6f433047805d205a2a5df4012ba
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-09 16:12:32 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (43.55%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -2,10 +2,6 @@ class Solution:
     def clearDigits(self, s: str) -> str:
         st = []
         for ch in s:
-            if '0' <= ch <= '9' and st:
-                st.pop()
-            else:
-                st.append(ch)
-        
-        s = "".join(st)
-        return s
+            if ch.isdigit() and st: st.pop()
+            else: st.append(ch)
+        return "".join(st)
