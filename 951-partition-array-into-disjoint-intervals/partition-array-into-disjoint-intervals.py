bd116889c3131bc01b34996ff1062d4322f5882a
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-09 18:51:40 -0800

    Time: 16 ms (100.00%) | Memory: 29 MB (64.55%) - LeetSync

--- 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.py
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.py
@@ -1,20 +1,13 @@
 class Solution:
     def partitionDisjoint(self, nums: List[int]) -> int:
-        #a prefix and a suffix
-        #prefix tracks the highest value at or before i
-        #suffix tracks the lowest number after i
-        #1000001
-        prefix = [-1] * len(nums)
-        prefix[0] = nums[0]
-        suffix = [1000001] * len(nums)
-        suffix[-1] = nums[-1]
-        for i in range(1, len(nums)):
-            prefix[i] = max(prefix[i - 1], nums[i])
-        for i in range(len(nums) - 2, -1, -1):
-            suffix[i] = min(suffix[i + 1], nums[i + 1])
-        #print(prefix)
-        #print(suffix)
-        for i in range(len(nums)):
-            if prefix[i] <= suffix[i]:
-                return min(len(nums) - 1, i + 1)
-        return -1
+        a = nums[0]
+        b = nums[0]
+        c = 0
+        for i in range(1,len(nums)):
+            if nums[i] >= a:
+                if nums[i] > b:
+                    b = nums[i]
+            else:
+                c = i
+                a = b
+        return c + 1 
