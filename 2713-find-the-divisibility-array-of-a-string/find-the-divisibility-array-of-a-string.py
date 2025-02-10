ac6f45846d5c94809ff99187e277e52a0573aa77
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-09 19:09:04 -0800

    Time: 68 ms (98.52%) | Memory: 20.9 MB (24.63%) - LeetSync

--- 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.py
+++ 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.py
@@ -1,10 +1,10 @@
 class Solution:
     def divisibilityArray(self, word: str, m: int) -> List[int]:
-        ans = [0] * len(word)
-        curr = 0
-        for i, c in enumerate(word):
-            curr = curr*10 + int(c)
-            if curr % m == 0:
-                ans[i] = 1
-            curr = curr % m
+        ans = []
+        num = 0
+        for d in word:
+            num *= 10
+            num += ord(d) - ord('0')
+            num %= m
+            ans.append(1 if num == 0 else 0)
         return ans
