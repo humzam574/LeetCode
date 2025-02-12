58f58c81ee695e4a11c50e22b724acc7ac531703
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-12 14:21:58 -0800

    Time: 8 ms (49.78%) | Memory: 17.8 MB (83.84%) - LeetSync

--- 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.py
+++ 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.py
@@ -7,26 +7,17 @@ class Solution:
         arr = [i for i in range(high)]
         ans = 10
         for num in range(high):
-            temp = []
-            for row in mat:
-                temp.append(list(row))
-            bits = bin(num)[2:][::-1] + ("0" * (m*n))
-            #print(bits)
+            temp = [row[:] for row in mat]
             inc = 0
             for x in range(m):
                 for y in range(n):
-                    if bits[inc] == "1":
-                        print(str(x) + ", " + str(y))
+                    if (num >> inc) & 1:
                         temp[x][y] = 1 - temp[x][y]
                         for dx, dy in ((1,0), (0,1), (-1,0), (0, -1)):
                             nx, ny = x+dx, y+dy
                             if 0 <= nx < m and 0 <= ny < n:
                                 temp[nx][ny] = 1 - temp[nx][ny]
                     inc += 1
-            # for row in temp:
-            #     print(row)
-            # print()
-            # print()
             if sum(sum(row) for row in temp) == 0:
-                ans = min(ans, bits.count("1"))
+                ans = min(ans, bin(num).count('1'))
         return -1 if ans == 10 else ans
