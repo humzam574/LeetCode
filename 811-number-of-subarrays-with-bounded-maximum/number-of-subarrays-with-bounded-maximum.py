055c98840a688d4ae10a88ae944399e0e3c44a8a
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-12 15:19:10 -0800

    Time: 37 ms (32.02%) | Memory: 23.9 MB (17.22%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
@@ -1,14 +1,5 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        #no subarrays where a big number exists
-        #no subarrays where no in range number exists
-        #so find all the subarrays with big numbers
-        #find all subarrays with only small numbers
-        #and subtract by the total
-
-        #use two pointer to find subarrays with small numbers
-        #for big numbers, ...
-
         def search(high):
             ans = 0
             l = 0
@@ -16,19 +7,10 @@ class Solution:
                 if n > high:
                     delta = r - l
                     if delta > 0:
-                        #print(str(l) + " " + str(r))
-                        ans += (delta * (delta + 1)//2)# - 1
+                        ans += (delta * (delta + 1)//2)
                     l = r + 1
-            r += 1
-            delta = r - l
+            delta = r - l + 1
             if delta > 0:
-                #print(str(l) + " " + str(r))
-                ans += (delta * (delta + 1)//2)# - 1
+                ans += (delta * (delta + 1)//2)
             return ans
-        ri = search(right)
-        #print()
-        #print()
-        li = search(left - 1)
-        #print(ri)
-        #print(li)
-        return ri - li#search(right) - search(left - 1)
+        return search(right) - search(left - 1)
