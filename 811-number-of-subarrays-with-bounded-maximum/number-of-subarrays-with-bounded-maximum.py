2659718641e69ab2687e5a763bbe5bdd85031373
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-12 15:20:23 -0800

    Time: 38 ms (29.91%) | Memory: 24 MB (17.22%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
@@ -1,16 +1,25 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        def search(high):
-            ans = 0
-            l = 0
-            for r, n in enumerate(nums):
-                if n > high:
-                    delta = r - l
-                    if delta > 0:
-                        ans += (delta * (delta + 1)//2)
-                    l = r + 1
-            delta = r - l + 1
-            if delta > 0:
-                ans += (delta * (delta + 1)//2)
-            return ans
-        return search(right) - search(left - 1)
+        ans = 0
+        l = 0
+        for r, n in enumerate(nums):
+            if n > right:
+                delta = r - l
+                if delta > 0:
+                    ans += (delta * (delta + 1)//2)
+                l = r + 1
+        delta = r - l + 1
+        if delta > 0:
+            ans += (delta * (delta + 1)//2)
+        right = left - 1
+        l = 0
+        for r, n in enumerate(nums):
+            if n > right:
+                delta = r - l
+                if delta > 0:
+                    ans -= (delta * (delta + 1)//2)
+                l = r + 1
+        delta = r - l + 1
+        if delta > 0:
+            ans -= (delta * (delta + 1)//2)
+        return ans
