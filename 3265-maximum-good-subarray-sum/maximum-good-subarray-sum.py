476737e8cf56b67e3a39a11c9541acf966563c3b
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-12 14:47:01 -0800

    Time: 300 ms (37.54%) | Memory: 32.9 MB (32.08%) - LeetSync

--- 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.py
+++ 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.py
@@ -4,17 +4,13 @@ class Solution:
         #keep the lowest prefix sums for any number n
         prefix = {}
         start = [0] * (len(nums) + 1)
+        ans = -inf
         for i in range(1, len(nums) + 1):
             start[i] = start[i - 1] + nums[i - 1]
-        #print(start)
-        #sum of all numbers before
-        #prefix sum = sum of all numbers before j+1 - sum of all numbers before i
-        #find the lowest prefix sums for all of i
-        ans = -inf
-        for i, n in enumerate(nums):
+            n = nums[i - 1]
             if n - k in prefix:
-                ans = max(ans, start[i + 1] - prefix[n - k])
+                ans = max(ans, start[i] - prefix[n - k])
             if n + k in prefix:
-                ans = max(ans, start[i + 1] - prefix[n + k])
-            prefix[n] = min(start[i], prefix.get(n, inf))
+                ans = max(ans, start[i] - prefix[n + k])
+            prefix[n] = min(start[i - 1], prefix.get(n, inf))
         return ans if ans != -inf else 0
