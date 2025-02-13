513a4d67202dba8b6e93c0a126b8bd6ae0206960
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-12 21:51:52 -0800

    Time: 63 ms (95.86%) | Memory: 29.2 MB (61.37%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -1,36 +1,20 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        n = len(nums)
-        prefix = [0] * n
-        prefix[0] = nums[0]
-        for i in range(1, n):
-            prefix[i] = prefix[i - 1] + nums[i]
-        suffix = [0] * (n)
-        #suffix[-1] = nums[-1]
-        suffix[0] = nums[-1]
-        ptr = 1
-        for i in range(n - 2, -1, -1):
-            suffix[ptr] = suffix[ptr - 1] + nums[i]
-            ptr += 1
-        #suffix = suffix[::-1]
-        temp = bisect_left(suffix, x)
-        # print(prefix)
-        # print(suffix)
-        # print(temp)
-        if 0 <= temp < n and suffix[temp] == x:
-            ans = temp + 1
-        else:
-            ans = 100001
-        for i in range(n):
-            curr = i + 1
-            left = prefix[i]
-            if left == x:
-                ans = min(ans, curr)
-                continue
-            if left > x:
-                break
-            target = x - left
-            temp = bisect_left(suffix, target)
-            if 0 <= temp < n and suffix[temp] == target:
-                ans = min(ans, curr + temp + 1)
-        return -1 if ans > n else ans
+        target = sum(nums) - x
+        rst = -inf
+
+        if target == 0:
+            return len(nums)
+        elif target < 0:
+            return -1
+
+        left = 0
+        acc = 0
+        for i in range(len(nums)):
+            acc += nums[i]
+            while acc > target:
+                acc -= nums[left]
+                left += 1
+            if acc == target:
+                rst = max(rst, i - left + 1)
+        return -1 if rst == -inf else len(nums) - rst
