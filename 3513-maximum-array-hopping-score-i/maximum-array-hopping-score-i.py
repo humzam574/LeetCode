2b541756ab04bfb50266d969b6399aaca702c57a
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-13 21:00:22 -0800

    Time: 2 ms (99.04%) | Memory: 17.7 MB (87.50%) - LeetSync

--- 3513-maximum-array-hopping-score-i/maximum-array-hopping-score-i.py
+++ 3513-maximum-array-hopping-score-i/maximum-array-hopping-score-i.py
@@ -1,8 +1,12 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [0] * n
-        for j in range(1, n):
-            for i in range(j):
-                dp[j] = max(dp[j], dp[i] + (j - i)*nums[j])
-        return dp[-1]
+        maxv=nums[-1]
+        ret = 0
+        i = len(nums)-1
+        while i!=0:
+            if nums[i]>maxv:
+                maxv=nums[i]
+            ret+=maxv
+            i-=1
+
+        return ret
