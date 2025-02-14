0e052621d5cc8dfdffb73f35f786c2ce84294bc4
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-13 19:41:03 -0800

    Time: 63 ms (99.93%) | Memory: 49.2 MB (41.51%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.py
+++ 2165-plates-between-candles/plates-between-candles.py
@@ -1,48 +1,39 @@
-from typing import List
-
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         n = len(s)
-        prefix = [0] * n
-        candles = []
-        
-        # Compute prefix sum of plates and store candle positions
-        for i in range(n):
-            if s[i] == '|':
-                candles.append(i)
-            prefix[i] = prefix[i - 1] + (s[i] == "*") if i > 0 else (s[i] == "*")
-        
-        ans = []
-        for left, right in queries:
-            # Find the leftmost candle >= left
-            l, r = 0, len(candles) - 1
-            while l <= r:
-                m = (l + r) // 2
-                if candles[m] < left:
-                    l = m + 1
-                else:
-                    r = m - 1
+        right = [0]*n
+        left = [0]*n
+        count = 0
+        start = False
+        last = 0
+        res = []
 
-            if l >= len(candles) or candles[l] > right:  # No valid candle found
-                ans.append(0)
-                continue
+        # right
+        for i in range(n-1, -1, -1):
+            c = s[i]
             
-            left_candle = candles[l]
+            if not start and c == '*':
+                right[i] = 0
+                continue
+            start = True
 
-            # Find the rightmost candle <= right
-            l, r = 0, len(candles) - 1
-            while l <= r:
-                m = (l + r) // 2
-                if candles[m] > right:
-                    r = m - 1
-                else:
-                    l = m + 1
+            if c == '|':
+                last = count
+            else: # c == '*'
+                count += 1
+            right[i] = last
 
-            right_candle = candles[r]
+        # left
+        last = right[0]
+        for i in range(n):
+            c = s[i]
+            if c == '|':
+                last = right[i]
+            left[i] = last
 
-            if left_candle < right_candle:  # Ensure at least one valid segment
-                ans.append(prefix[right_candle] - prefix[left_candle])
-            else:
-                ans.append(0)
-
-        return ans
+        for query in queries:
+            s,e = query[0], query[1]
+            val = right[s] - left[e]
+            res.append(val if val >= 0 else 0)
+        
+        return res
