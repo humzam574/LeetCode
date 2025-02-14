b8f21095d048b07e088ddc89618826eb960fb6cd
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-13 21:23:07 -0800

    Time: 130 ms (99.43%) | Memory: 30.3 MB (34.86%) - LeetSync

--- 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.py
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.py
@@ -1,17 +1,11 @@
 class Solution:
     def minimumRemoval(self, beans: List[int]) -> int:
         beans.sort()
-        if len(beans) == 1:
-            return 0
-        tot = sum(beans)
-        #print(beans)
-        curr = 0
-        i = 0
-        n = len(beans)
-        ans = tot - n*beans[0]
-        while curr < ans and i < n - 1:
-            curr += beans[i]
-            tot -= beans[i]
-            i += 1
-            ans = min(ans, curr + tot - (n - i)*beans[i])
+        ans = float('inf')
+        total = sum(beans)
+        length = len(beans)
+        for bean in beans:
+            curr = total - length * bean
+            length -= 1
+            if curr < ans: ans = curr
         return ans
