a52d9cc14b14c8ac7c4ca711e6315c6d32e2ec18
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-16 19:57:38 -0800

    Time: 1 ms (98.78%) | Memory: 17.9 MB (67.21%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,22 +1,8 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        #do a dfs with a length goal, curr, memo
-        self.memo = set()
-        def dfs(length, curr, rem):
-            # if curr in self.memo:
-            #     return
-            if len(curr) == length:
-                self.memo.add(curr)
-                return
-            for item in rem.keys():
-                if rem[item] == 0:
-                    continue
-                curr = curr + item
-                rem[item] -= 1
-                dfs(length, curr, rem.copy())
-                rem[item] += 1
-                curr = curr[:len(curr) - 1]
-        for i in range(1, len(tiles) + 1):
-            dfs(i, "", Counter(tiles))
-            #print(self.memo)
-        return len(self.memo)
+        L, t, f, tiles = len(tiles), 0, math.factorial, sorted(list(tiles))
+        for n in range(1,L+1):
+            q = ('',)
+            for p in itertools.combinations(tiles,n):
+                if p > q: t, q = t + f(n)//prod(list(map(f,collections.Counter(p).values()))), p
+        return t
