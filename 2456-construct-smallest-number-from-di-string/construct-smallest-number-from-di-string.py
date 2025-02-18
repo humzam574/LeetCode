341333a096124c161b191829862bd2299e3b2d15
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-17 18:47:42 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.89%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,14 +1,10 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        n=len(pattern)
-        res=[i for i in range(1,n+2)]
-        i=0
-        while i<n:
-            t=i
-            while t< n and pattern[t]=="D":
-                t+=1
-            res[i:t+1] =reversed(res[i:t+1])
-            if t!=i:
-                i=t-1
-            i+=1
-        return "".join(map(str,res))
+        n, ans, i = len(pattern), [i for i in range(1, len(pattern) + 2)], 0
+        while i < n:
+            t = i
+            while t < n and pattern[t] == 'D': t += 1
+            ans[i:t+1] = ans[i:t+1][::-1]
+            if t != i: i = t - 1
+            i += 1
+        return ''.join(str(i) for i in ans)
