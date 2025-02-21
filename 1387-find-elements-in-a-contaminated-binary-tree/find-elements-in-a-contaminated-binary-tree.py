c37e1db2ba2636b0c3dea8d92eb1ab259fce6957
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-20 16:05:25 -0800

    Time: 19 ms (19.25%) | Memory: 22.3 MB (10.70%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -6,17 +6,15 @@
 #         self.right = right
 class FindElements:
     def __init__(self, root: Optional[TreeNode]):
-        self.inside = {0}
-        def dfs(root, val):
-            if not root:
+        def dfs(node, val):
+            if not node:
                 return
-            if root.right:
-                #val = 2 * val + 
-                self.inside.add(2*val + 2)
-                dfs(root.right, 2*val + 2)
-            if root.left:
-                self.inside.add(2*val + 1)
-                dfs(root.left, 2*val + 1)
+
+            self.inside.add(val)
+            dfs(node.left, 2 * val + 1)
+            dfs(node.right, 2 * val + 2)
+            
+        self.inside = set()
         dfs(root, 0)
 
     def find(self, target: int) -> bool:
