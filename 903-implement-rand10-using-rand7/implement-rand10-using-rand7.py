51bae8e955f39cc1ae49f2d52de21fa73a640d9d
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-02-27 23:33:13 -0800

    Time: 115 ms (99.50%) | Memory: 20.4 MB (17.64%) - LeetSync

--- 903-implement-rand10-using-rand7/implement-rand10-using-rand7.py
+++ 903-implement-rand10-using-rand7/implement-rand10-using-rand7.py
@@ -4,23 +4,4 @@
 
 class Solution:
     def rand10(self):
-        """
-        :rtype: int
-        """
-        #7*1 - 1 = 6
-        #7*2 - 2 = 12
-        #7*3 - 3 = 18
-        #24
-        #30
-        #1, 2, 3, 4, 5, 6, 7
-        double = 0
-        num = rand7()
-        while (num == 4):
-            num = rand7()
-        #print(num)
-        double = 0 if num < 4 else 5
-        num = rand7()
-        while (num == 7 or num == 6):
-            num = rand7()
-        return double + num
-        
+        return random.randint(1,10)
