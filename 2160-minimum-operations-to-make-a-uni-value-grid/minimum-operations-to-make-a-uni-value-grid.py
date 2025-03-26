c7bb687b1445e935783c08ed58e3668a72b6195c
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-03-26 09:41:43 -0700

    Time: 100 ms (97.37%) | Memory: 39.1 MB (50.44%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,28 +1,28 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        #0, 1, 2, 3, 4, 5
-        #prefix sum
-        #1 2 3 5
-        arr = []
-        for row in grid:
-            arr = arr + row
-        arr.sort()
-        post = sum(arr)# - arr[0]
-        pre = 0
-        n = len(arr)
-        curr = 0
-        ans = inf
-        delta = arr[0] % x
-        #print(arr)
-        for i in range(n):
-            if (arr[i] - delta) % x:
+        M, N = len(grid), len(grid[0])
+        d = defaultdict(int)
+        
+        medianIdx = (M*N)//2
+
+        for i in range(M):
+            for j in range(N):
+                d[grid[i][j]] += 1
+                
+        keys = sorted(d)
+        curr = median = 0
+        
+        for k in keys:
+            curr += d[k]
+            if curr > medianIdx:
+                median = k
+                break
+        ans = 0  
+        for k in d:
+            cnt = abs(k-median)/x
+            if cnt%1 != 0:
                 return -1
-            post -= arr[i]
-            curr = (post - (n - i - 1) * arr[i]) // x + max(0, (arr[i]*i-pre)//x)
-            #print(str(i) + " " + str(curr) + " " + str(pre) + " " + str(post))
-            
-            pre += arr[i]
-            
-            ans = min(ans, curr)
-        return ans
+            ans += cnt*d[k]
+
+        return int(ans)
         
