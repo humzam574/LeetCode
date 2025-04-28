c82d71eca1dc200a06193936b45f49e97df72a53
Humza Mahmood <65996060+humzam574@users.noreply.github.com>
2025-04-28 15:44:49 -0700

    Time: 27 ms (99.62%) | Memory: 18.4 MB (98.78%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.py
+++ 971-shortest-bridge/shortest-bridge.py
@@ -1,47 +1,50 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        #find the first one and mark it as 2
-        #put all the two coords in a list
-        #put all the 1 coords in a list
-        #find the shortest manhattan distance and return n-1
-        m = len(grid)
-        n = len(grid[0])
-        twos = []
-        ones = []
-        cont = True
-        for x in range(m):
-            if not cont:
-                x = m + 1
-                break
-            for y in range(n):
-                if not cont:
-                    y = n + 1
+
+        m, n = len(grid), len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
+
+                if grid[i][j]:
+                    queue = [(i, j)]
                     break
-                if grid[x][y] == 1:
-                    cont = False
-                    dq = deque()
-                    grid[x][y] = 2
-                    dq.append((x, y))
-                    twos.append((x, y))
-                    #print("x and y " + str(x) + " " + str(y))
-                    while dq:
-                        nx, ny = dq.popleft()
-                        for dx, dy in ((1,0), (0, 1), (-1, 0), (0, -1)):
-                            #print(str(nx+dx) + " " + str(ny+dy))
-                            if 0 <= (nx+dx) < m and 0 <= (ny + dy) < n and grid[nx+dx][ny+dy] == 1:
-                                dq.append((nx + dx, ny + dy))
-                                grid[nx+dx][ny+dy] = 2
-                                twos.append((nx+dx, ny+dy))
-                    
-                    break
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y] == 1:
-                    ones.append((x, y))
-        # print(ones)
-        # print(twos)
-        ans = inf
-        for a, b in twos:
-            for c, d in ones:
-                ans = min(ans, abs(d-b) + abs(c - a) - 1)
-        return ans
+
+        next_queue = []
+
+        while queue:
+
+            x, y = queue.pop()
+            grid[x][y] = -1
+
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+
+                _x = x + dx
+                _y = y + dy
+
+                if _x >= 0 and _y >= 0 and _x < m and _y < n:
+
+                    if grid[_x][_y] == 0:
+                        next_queue.append((_x, _y, 1))
+                        grid[_x][_y] = -1
+                    elif grid[_x][_y] == 1:
+                        queue.append((_x, _y))
+                        grid[_x][_y] = -1
+
+        # print(grid)
+
+        while next_queue:
+
+            x, y, depth = next_queue.pop(0)
+
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+
+                _x = x + dx
+                _y = y + dy
+                if _x >= 0 and _y >= 0 and _x < m and _y < n:
+                    if grid[_x][_y] == 0:
+                        next_queue.append((_x, _y, depth + 1))
+                        grid[_x][_y] = -1
+
+                    if grid[_x][_y] == 1:
+                        return depth 
