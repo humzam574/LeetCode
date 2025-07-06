d2261224117a3d00f759d0885a81c9f910e0b1e4
Humza Mahmood <humzam574@gmail.com>
2025-07-06 14:10:27 -0700

    Time: 0 ms (100.00%) | Memory: 20 MB (5.00%) - LeetSync

--- 333-largest-bst-subtree/largest-bst-subtree.py
+++ 333-largest-bst-subtree/largest-bst-subtree.py
@@ -1,45 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def is_valid_bst(self, root: Optional[TreeNode]) -> bool:
-        """Check if given tree is a valid BST using in-order traversal."""
-        # An empty tree is a valid Binary Search Tree.
-        if not root:
-            return True
-        
-        # If left subtree is not a valid BST return false.
-        if not self.is_valid_bst(root.left):
-            return False
-
-        # If current node's value is not greater than the previous 
-        # node's value in the in-order traversal return false.
-        if self.previous and self.previous.val >= root.val:
-            return False
-
-        # Update previous node to current node.
-        self.previous = root
-
-        # If right subtree is not a valid BST return false.
-        return self.is_valid_bst(root.right)
-
-    # Count nodes in current tree.
-    def count_nodes(self, root: Optional[TreeNode]) -> int:
-        if not root: 
-            return 0
-
-        # Add nodes in left and right subtree.
-        # Add 1 and return total size.
-        return 1 + self.count_nodes(root.left) + self.count_nodes(root.right)
-        
     def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
-        # Previous node is initially null.
-        self.previous = None
+        self.kids = {}
+        self.ans = 1
+        def dfs(curr):
+            if not curr:
+                return 0
+            if not curr.left and not curr.right:
+                self.kids[curr] = 1
+                return 1
+            val = 1 + dfs(curr.left) + dfs(curr.right)
+            self.kids[curr] = val
+            return val
+        dfs(root)
+        # for k, v in self.kids.items():
+        #     print(v, end = " ")
+        #     print(k)
+        #     print()
+        def bst(node, leftLimit, rightLimit):
+            if not node:
+                return True
+            
+            if node.val <= leftLimit or node.val >= rightLimit:
+                return False
 
-        # If current subtree is a validBST, its children will have smaller size BST.
-        if self.is_valid_bst(root):
-            return self.count_nodes(root)
-        
-        # Find BST in left and right subtrees of current nodes.
-        return max(self.largestBSTSubtree(root.left), self.largestBSTSubtree(root.right))
-        
+            return bst(node.left, leftLimit, node.val) and bst(node.right, node.val, rightLimit)
+        def recurse(curr):
+            if bst(curr, -inf, inf):
+                self.ans = max(self.ans, self.kids[curr])
+            else:
+                if curr.left:
+                    recurse(curr.left)
+                if curr.right and self.ans < self.kids[curr.right]:
+                    recurse(curr.right)
+        recurse(root)
+        return self.ans
