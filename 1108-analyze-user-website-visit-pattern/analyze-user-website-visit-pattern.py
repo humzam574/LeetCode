9342a4f9145b23888fd11c9379a22fc8fb52a2b9
Humza Mahmood <humzam574@gmail.com>
2025-07-20 14:04:41 -0700

    Time: 7 ms (81.36%) | Memory: 18.3 MB (67.80%) - LeetSync

--- 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -1,70 +1,28 @@
-class Solution(object):
-    def mostVisitedPattern(self, username, timestamp, website):
-        """
-        :type username: List[str]
-        :type timestamp: List[int]
-        :type website: List[str]
-        :rtype: List[str]
-        """
-        #for each user create an array of their website visits
-        #make a dict key username value arr
-        #each value in arr is [website, timestamp]
-        #then sort by timestamp and chop it off
-        #then make a freq dict
-        visits = defaultdict(list)
-        n = len(username)
-        for i in range(n):
-            ign = username[i]
-            ts = timestamp[i]
-            wb = website[i]
-            visits[ign].append([wb, ts])
-        # for k, v in visits.items():
-        #     print("k: ", end = "")
-        #     print(k)
-        #     print("v: ", end = "")
-        #     print(v)
-        #     print()
-        # print("ON TO FREQ")
-        # print()
-        # print()
-        freq = defaultdict(int)
-        for k, v in visits.items():
-            if len(v) < 3:
-                continue
-            v.sort(key = lambda x : x[1])
-            v = [a[0] for a in v]
-            # for i in range(len(v) - 2):
-            #     freq[tuple(v[i:i+3])]+=1
-            st = set()
-            ln = len(v)
-            for i in range(ln):
-                for j in range(i + 1, ln):
-                    for k in range(j + 1, ln):
-                        # freq[(v[i], v[j], v[k])]+=1
-                        st.add((v[i], v[j], v[k]))
-            for val in st:
-                freq[val]+=1
-        # for k, v in freq.items():
-        #     print("k: ", end = "")
-        #     print(k)
-        #     print("v: ", end = "")
-        #     print(v)
-        #     print()
-        val = 0
-        ans = ""
-        for k, v in freq.items():
-            if v > val:
-                val = v
-                ans = k
-            elif v == val:
-                # if ''.join(ans) > ''.join(k):
-                #     ans = k
-                if ans[0] > k[0]:
-                    ans = k
-                elif ans[0] == k[0]:
-                    if ans[1] > k[1]:
-                        ans = k
-                    elif ans[1] == k[1]:
-                        if ans[2] > k[2]:
-                            ans = k
-        return ans
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        #adding tuple of each to sort on time
+        comb = []
+        for name,time,web in zip(username,timestamp,website):
+            comb.append((name,time,web))
+        comb.sort(key = lambda x : x[1])
+        # amking a hashmap for each user
+        table = defaultdict(list)
+        for var in comb:
+            table[var[0]].append(var[2])
+        #identifying patterns
+        patterns = defaultdict(int)
+        for val in table.values():
+            if len(val) >= 3:
+                n = len(val)
+                triplets = [(val[i],val[j],val[k]) for i in range(n) for j in range(i+1,n) for k in range(j+1,n)]
+                for pat in set(triplets):
+                    patterns[pat]+=1
+        #getting the max len patterns
+        maxval, res = 0,[]
+        for key,val in patterns.items():
+            if val > maxval or (val == maxval and list(key) < res):
+                res = list(key)
+                maxval = val
+        # sorting to get the least lexographically val
+        # res.sort()
+        return res
