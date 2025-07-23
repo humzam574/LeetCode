22950021dbf87cf8f6e0f557710de041b47f3310
Humza Mahmood <humzam574@gmail.com>
2025-07-22 17:19:18 -0700

    Time: 134 ms (98.71%) | Memory: 18 MB (96.13%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -1,34 +1,23 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        ans = 0
         if x > y:
-            stack = []
-            for c in s:
-                stack.append(c)
-                if len(stack) >= 2 and stack[-1] == 'b' and stack[-2] == 'a':
-                    stack.pop()
-                    stack.pop()
-                    ans+=x
-            stack2 = []
-            for c in stack:
-                stack2.append(c)
-                if len(stack2) >= 2 and stack2[-1] == 'a' and stack2[-2] == 'b':
-                    stack2.pop()
-                    stack2.pop()
-                    ans+=y
+            return self.getMax(s, 'a', 'b', x, y)
         else:
-            stack = []
-            for c in s:
-                stack.append(c)
-                if len(stack) >= 2 and stack[-1] == 'a' and stack[-2] == 'b':
-                    stack.pop()
-                    stack.pop()
-                    ans+=y
-            stack2 = []
-            for c in stack:
-                stack2.append(c)
-                if len(stack2) >= 2 and stack2[-1] == 'b' and stack2[-2] == 'a':
-                    stack2.pop()
-                    stack2.pop()
-                    ans+=x
+            return self.getMax(s, 'b', 'a', y, x)
+        
+    def getMax(self, s: str, a:str, b:str, x: int, y: int):
+        c1 = c2 = ans = 0
+        s += 'c'
+        for c in s:
+            if c == a:
+                c1 += 1
+            elif c == b:
+                if c1 == 0:
+                    c2 += 1
+                else:
+                    ans += x
+                    c1 -= 1
+            else:
+                ans += y * min(c1, c2)
+                c1 = c2 = 0
         return ans
