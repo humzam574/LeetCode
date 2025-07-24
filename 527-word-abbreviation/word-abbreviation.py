9c8208935ee1d37a4c9efab621dc2685a7c40a5d
Humza Mahmood <humzam574@gmail.com>
2025-07-23 22:14:39 -0700

    Time: 6 ms (98.08%) | Memory: 18.6 MB (80.08%) - LeetSync

--- 527-word-abbreviation/word-abbreviation.py
+++ 527-word-abbreviation/word-abbreviation.py
@@ -1,40 +1,31 @@
-class Solution:
-    def wordsAbbreviation(self, words: List[str]) -> List[str]:
-        freq = defaultdict(list)
-        for i, w in enumerate(words):
-            if len(w) > 3:
-                freq[(w[0], w[-1], len(w))].append(i)
-        # print(freq)
-        for k, v in freq.items():
-            if len(v) == 1:
-                idx = v.pop()
-                temp = words[idx]
-                words[idx] = temp[0] + str(len(temp) - 2) + temp[-1]
-            else:
-                #construct a trie and see where each one branches off to be unique
-                #once a path is unique, then construct
-                trie = {}
-                for idx in v:
-                    curr = trie
-                    anchor = curr
-                    # print(words[idx][1:-1])
-                    for c in words[idx][1:-1]:
-                        if c not in curr:
-                            curr[c] = {}
-                        curr[0] = curr.get(0, 0) + 1
-                        curr = curr[c]
-                        
-                    trie = anchor
-                # print(trie)
-                for idx in v:
-                    curr = trie
-                    dep = 1
-                    w = words[idx]
-                    while 0 in curr and curr[0] != 1:
-                        curr = curr[w[dep]]
-                        dep += 1
-                    if dep < len(w) - 2:
-                        words[idx] = w[:dep] + str(len(w) - dep - 1) + w[-1]
-                    
-        # print(words)
-        return words
+class Solution(object):
+    def wordsAbbreviation(self, words):
+        def longest_common_prefix(a, b):
+            i = 0
+            while i < len(a) and i < len(b) and a[i] == b[i]:
+                i += 1
+            return i
+
+        ans = [None for _ in words]
+
+        groups = collections.defaultdict(list)
+        for index, word in enumerate(words):
+            groups[len(word), word[0], word[-1]].append((word, index))
+
+        for (size, first, last), enum_words in groups.items():
+            enum_words.sort()
+            lcp = [0] * len(enum_words)
+            for i, (word, _) in enumerate(enum_words):
+                if i:
+                    word2 = enum_words[i-1][0]
+                    lcp[i] = longest_common_prefix(word, word2)
+                    lcp[i-1] = max(lcp[i-1], lcp[i])
+
+            for (word, index), p in zip(enum_words, lcp):
+                delta = size - 2 - p
+                if delta <= 1:
+                    ans[index] = word
+                else:
+                    ans[index] = word[:p+1] + str(delta) + last
+
+        return ans
