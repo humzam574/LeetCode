6c4fda73b5575aa5ed0920827b112ae468f58377
Humza Mahmood <humzam574@gmail.com>
2025-07-24 10:57:02 -0700

    Time: 193 ms (5.71%) | Memory: 19.3 MB (31.43%) - LeetSync

---
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
@@ -0,0 +1,51 @@
+class Solution:
+    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+        #2d dp for the first path to get the max score
+        #another dp to find the number of ways... ?
+        #brute force is 200!/(100!*100!) -> not possible
+        n = len(board)
+
+        dp = [[(0, 0)] * n for i in range(n)]
+        dp[0][0] = (0, 1)
+        # dp = [[0] * n for i in range(n)]
+        board[0] = '0' + board[0][1:]
+        for i in range(n):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                if board[i][j] == 'X':
+                    dp[i][j] = -999999
+                    continue
+                # pos = False
+                a, b = 0, defaultdict(int)
+                if i != 0 and '0' <= board[i-1][j] <= '9':
+                    # pos = True
+                    # dp[i][j] = max(dp[i][j], dp[i - 1][j] + int(board[i-1][j]))
+                    temp = dp[i-1][j][0] + int(board[i-1][j])
+                    b[temp]+=dp[i-1][j][1]
+                    a = max(a, temp)
+                if j != 0 and '0' <= board[i][j-1] <= '9':
+                    # pos = True
+                    # dp[i][j] = max(dp[i][j], dp[i][j - 1] + int(board[i][j-1]))
+                    temp = dp[i][j-1][0] + int(board[i][j-1])
+                    b[temp]+=dp[i][j-1][1]
+                    a = max(a, temp)
+                if i != 0 and j != 0 and '0' <= board[i-1][j-1] <= '9':
+                    # pos = True
+                    # dp[i][j] = max(dp[i][j], dp[i-1][j-1] + int(board[i-1][j-1]))
+                    temp = dp[i-1][j-1][0] + int(board[i-1][j-1])
+                    b[temp]+=dp[i-1][j-1][1]
+                    a = max(a, temp)
+                # if not pos:
+                #     # print(i)
+                #     # print(j)
+                #     #return [0, 0]
+                #     dp[i][j] = (a, 0)
+                dp[i][j] = (a, b[a] % 1000000007)
+        # print(dp)
+        if dp[n-1][n-1][1] == 0:
+            return [0, 0]
+        return [dp[n-1][n-1][0], dp[n-1][n-1][1] % 1000000007]
+        # return [dp[n-1][n-1]]
+        #the number of ways to get to the corner in max points is the sum of the number of ways to get to the 3 adjacents in points (max - board[adj])
+        
