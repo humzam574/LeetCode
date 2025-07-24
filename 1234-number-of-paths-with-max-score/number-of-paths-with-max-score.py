5e5e2bf365a92cd4872a2e01cb2faa5df1458c04
Humza Mahmood <humzam574@gmail.com>
2025-07-24 11:13:20 -0700

    Time: 106 ms (78.10%) | Memory: 18.9 MB (84.76%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
@@ -11,55 +11,32 @@ class Solution:
         board[0] = '0' + board[0][1:]
         for i in range(n):
             for j in range(n):
-                if i == 0 and j == 0:
+                if i == 0 and j == 0 or board[i][j] == 'X':
                     continue
-                if board[i][j] == 'X':
-                    dp[i][j] = -999999
-                    continue
-                # pos = False
-                #a, b = 0, defaultdict(int)
                 a = 0
                 b = 0
-                if i != 0 and '0' <= board[i-1][j] <= '9':
-                    # pos = True
-                    # dp[i][j] = max(dp[i][j], dp[i - 1][j] + int(board[i-1][j]))
+                if i != 0 and board[i-1][j] != 'X':# and '0' <= board[i-1][j] <= '9':
                     temp = dp[i-1][j][0] + ord(board[i-1][j]) - 48
-                    # b[temp]+=dp[i-1][j][1]
-                    # a = max(a, temp)
                     b = dp[i-1][j][1]
                     a = temp
-                if j != 0 and '0' <= board[i][j-1] <= '9':
-                    # pos = True
-                    # dp[i][j] = max(dp[i][j], dp[i][j - 1] + int(board[i][j-1]))
+                if j != 0 and board[i][j-1] != 'X':# '0' <= board[i][j-1] <= '9':
                     temp = dp[i][j-1][0] + ord(board[i][j-1]) - 48
-                    # b[temp]+=dp[i][j-1][1]
-                    # a = max(a, temp)
                     if temp > a:
                         a = temp
                         b = dp[i][j-1][1]
                     elif temp == a:
                         b+=dp[i][j-1][1]
-                if i != 0 and j != 0 and '0' <= board[i-1][j-1] <= '9':
-                    # pos = True
-                    # dp[i][j] = max(dp[i][j], dp[i-1][j-1] + int(board[i-1][j-1]))
+                if i != 0 and j != 0 and board[i-1][j-1] != 'X':# and '0' <= board[i-1][j-1] <= '9':
                     temp = dp[i-1][j-1][0] + ord(board[i-1][j-1]) - 48
-                    # b[temp]+=dp[i-1][j-1][1]
-                    # a = max(a, temp)
                     if temp > a:
                         a = temp
                         b = dp[i-1][j-1][1]
                     elif temp == a:
                         b+=dp[i-1][j-1][1]
-                # if not pos:
-                #     # print(i)
-                #     # print(j)
-                #     #return [0, 0]
-                #     dp[i][j] = (a, 0)
                 dp[i][j] = (a, b % 1000000007)
-        # print(dp)
         if dp[n-1][n-1][1] == 0:
             return [0, 0]
-        return [dp[n-1][n-1][0], dp[n-1][n-1][1] % 1000000007]
+        return [0,0] if not dp[-1][-1][1] else [dp[n-1][n-1][0], dp[n-1][n-1][1] % 1000000007]
         # return [dp[n-1][n-1]]
         #the number of ways to get to the corner in max points is the sum of the number of ways to get to the 3 adjacents in points (max - board[adj])
         
