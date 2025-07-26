2d6aba688aa4867712be95a91d77cdceeb355aed
Humza Mahmood <humzam574@gmail.com>
2025-07-26 14:30:37 -0700

    Time: 98 ms (88.37%) | Memory: 44.5 MB (66.67%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
@@ -1,34 +1,52 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        #group the pairs together
-        #for each group, get the group of chars
-        #sort
-        #make an answer array and insert
         n = len(s)
-        ans = ['' for i in range(n)]
-        self.groups = []
-        self.adj = [[] for i in range(n)]
-        for a, b in pairs:
-            self.adj[a].append(b)
-            self.adj[b].append(a)
-        self.visited = [False] * n
-        def dfs(node, prev):
-            self.visited[node] = True
-            self.groups[-1].add(node)
-            for val in self.adj[node]:
-                if not self.visited[val]:
-                    dfs(val, node)
+        self.root = [i for i in range(n)]
+        self.rank = [1] * n
+        
+        component_map = {}
+        
+        res = list(s)
+        
+        for pair in pairs:
+            self.union(pair[0], pair[1])
+
+        
         for i in range(n):
-            if not self.visited[i]:
-                self.groups.append(set())
-                dfs(i, None)
-        # print(self.adj)
-        # print(self.groups)
-        for group in self.groups:
-            arr = sorted([s[i] for i in group])
-            # print(arr)
-            idx = 0
-            for i in sorted(list(group)):
-                ans[i] = arr[idx]
-                idx+=1
-        return ''.join(ans)
+            root = self.find(i)
+            if root not in component_map:
+                component_map[root] = []
+            component_map[root].append(i)
+        
+        
+        for indices in component_map.values():
+            chars = [s[i] for i in indices]
+            indices.sort()
+            chars.sort()
+            for i, ch in zip(indices, chars):
+                res[i] = ch
+                
+        return ''.join(res)
+        
+        
+        
+        
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1    
+        
