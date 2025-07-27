3c163e677f39217b7eae708a9524833f9a8ca698
Humza Mahmood <humzam574@gmail.com>
2025-07-26 17:49:54 -0700

    Time: 111 ms (89.53%) | Memory: 17.7 MB (86.05%) - LeetSync

--- 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.py
+++ 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.py
@@ -1,38 +1,22 @@
+def merge(a,b):
+    if b in a:
+        return a
+    for i in range(min(len(a),len(b)), 0 ,-1):
+         if a[-i:] == b[:i]:
+            return a+b[i:]
+    return a+b
 class Solution:
-    def minimumString(self, d: str, e: str, f: str) -> str:
-        #there are 3 strings, abc
-        #abc, acb, bac, bca, cab, cba
-        #six permutations
-        #calculate for each perm and maintain an ans
-        #use a two ptr to find overlap
-        
-        def comp(a, b):
-            if a in b:
-                return b
-            if b in a:
-                return a
-            # if len(b) > len(a):
-            #     a, b = b, a
-            i = max(len(a) - len(b), 0)
-            while i < len(a):
-                if b.startswith(a[i:]):
-                    break
-                i+=1
-            return a[:i] + b
-            #b.startswith(a[i:])
-            #so i < len(a)
-        # print(comp("bbb", "bbcb"))
-        arr = [d, e, f]
-        #ans = ''.join(arr)
-        ans = "z" * 300
-        for x,y,z in ((0, 1, 2), (0, 2, 1), (1, 0, 2), (1, 2, 0), (2, 0, 1), (2, 1, 0)):
-            #a, b, c = arr[x], arr[y], arr[z]
-            curr = comp(comp(arr[x], arr[y]), arr[z])
-            # print(curr)
-
-            if len(curr) < len(ans):
-                ans = curr
-            elif len(curr) == len(ans):
-                if curr < ans:
-                    ans = curr
+    def minimumString(self, a: str, b: str, c: str) -> str:
+        candidates = []
+        candidates.append(merge(merge(a,b),c))    
+        candidates.append(merge(merge(b,a),c))    
+        candidates.append(merge(merge(a,c),b))    
+        candidates.append(merge(merge(c,a),b))    
+        candidates.append(merge(merge(b,c),a))    
+        candidates.append(merge(merge(c,b),a))
+        ans = candidates[0]
+        #return candidates
+        for s in candidates:
+            if len(s)<len(ans) or (len(s) == len(ans) and s<ans):
+                ans = s
         return ans
