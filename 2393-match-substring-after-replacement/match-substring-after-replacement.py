2f3251093c01cf53feaeb6c4712e29f0fd75e3d5
Humza Mahmood <humzam574@gmail.com>
2025-07-28 15:03:45 -0700

    Time: 15 ms (96.04%) | Memory: 18.3 MB (87.13%) - LeetSync

--- 2393-match-substring-after-replacement/match-substring-after-replacement.py
+++ 2393-match-substring-after-replacement/match-substring-after-replacement.py
@@ -1,27 +1,34 @@
 class Solution:
-    def matchReplacement(self, s: str, sub: str, mappings: List[List[str]]) -> bool:
-        maps = defaultdict(set)
-        sb = len(sub)
-        for a, b in mappings:
-            #maps[a].add(b)
-            maps[b].add(a)
-        # l = 0
-        # for r in range(len(sub), len(s)+1):
-        #     #code code code
-        #     pos = True
-        #     for i in range(l, r):
-        #         if not (s[i] == sub[i - l] or sub[i - l] in maps[s[i]]):
-        #             pos = False
-        #             break
-        #     if pos:
-        #         return True
-        #     l+=1
-        for l in range(len(s) - sb, -1, -1):
-            pos = True
-            for i in range(l, l + sb):
-                if not (s[i] == sub[i - l] or sub[i - l] in maps[s[i]]):
-                    pos = False
+    def matchReplacement(
+        self, 
+        s: str, 
+        sub: str, 
+        mappings: List[List[str]]
+    ) -> bool:
+        empty, m, ss = [], {}, set(s)
+
+        if len(sub) > len(s):
+            return False
+        elif sub in s:
+            return True
+
+        for mapping in mappings:
+            if mapping[1] not in ss:
+                continue
+
+            if mapping[0] not in m:
+                m[mapping[0]] = []
+
+            m[mapping[0]].append(mapping[1])
+
+        if not m:
+            return False
+        
+        for i in range(len(s) - len(sub), -1, -1):
+            for j, c in enumerate(s[i : i + len(sub)]):
+                if c != sub[j] and c not in m.get(sub[j], empty):
                     break
-            if pos:
+            else:
                 return True
+            
         return False
