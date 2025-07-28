dbf344158f04e858c26202dd3a8829a709563e02
Humza Mahmood <humzam574@gmail.com>
2025-07-27 18:38:22 -0700

    Time: 1356 ms (10.59%) | Memory: 17.8 MB (55.19%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -9,12 +9,13 @@ class Solution:
 
         for mask in range(1 << n):  # 2^n subsets
             subseq = []
+            curr = 0
             for i in range(n):
                 if mask & (1 << i):
-                    subseq.append(nums[i])
-            curr = 0
-            for val in subseq:
-                curr |= val
+                    curr |= nums[i]
+            # curr = 0
+            # for val in subseq:
+            #     curr |= val
             if curr == target:
                 ans+=1
         return ans
