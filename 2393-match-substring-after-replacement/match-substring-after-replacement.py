9fd4befb6e009acb2b96c588760045f013b9d0c0
Humza Mahmood <humzam574@gmail.com>
2025-07-28 15:03:34 -0700

    Time: 263 ms (78.22%) | Memory: 18.5 MB (51.49%) - LeetSync

--- 2393-match-substring-after-replacement/match-substring-after-replacement.py
+++ 2393-match-substring-after-replacement/match-substring-after-replacement.py
@@ -1,18 +1,27 @@
 class Solution:
     def matchReplacement(self, s: str, sub: str, mappings: List[List[str]]) -> bool:
         maps = defaultdict(set)
+        sb = len(sub)
         for a, b in mappings:
             #maps[a].add(b)
             maps[b].add(a)
-        l = 0
-        for r in range(len(sub), len(s)+1):
-            #code code code
+        # l = 0
+        # for r in range(len(sub), len(s)+1):
+        #     #code code code
+        #     pos = True
+        #     for i in range(l, r):
+        #         if not (s[i] == sub[i - l] or sub[i - l] in maps[s[i]]):
+        #             pos = False
+        #             break
+        #     if pos:
+        #         return True
+        #     l+=1
+        for l in range(len(s) - sb, -1, -1):
             pos = True
-            for i in range(l, r):
+            for i in range(l, l + sb):
                 if not (s[i] == sub[i - l] or sub[i - l] in maps[s[i]]):
                     pos = False
                     break
             if pos:
                 return True
-            l+=1
         return False
