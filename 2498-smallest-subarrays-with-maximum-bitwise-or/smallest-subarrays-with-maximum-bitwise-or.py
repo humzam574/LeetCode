909897faa11789a18b33b09146356679f100bf83
Humza Mahmood <humzam574@gmail.com>
2025-07-30 11:01:48 -0700

    Time: 1152 ms (24.62%) | Memory: 59.2 MB (5.97%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
@@ -1,10 +1,5 @@
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
-        #create a list that is a counter of all the bits
-        #then run a b-search
-
-        #print(bin(22))
-        #22 = 0b10110
         n = len(nums)
         maxor = 0
         high = 0
@@ -14,20 +9,13 @@ class Solution:
             maxor |= num
         arridxlen = len(bin(maxor)) - 2
         arr = [[0] * arridxlen for i in range(n+1)]
-        # print(arr)
         curr = [0] * arridxlen
         for i in range(n):
             curr = arr[i-1].copy()
-            # print(bin(nums[i])[2:][::-1])
             for j, char in enumerate(bin(nums[i])[2:][::-1]):
                 if char == "1":
                     curr[j]+=1
             arr[i] = curr.copy()
-        # print(arr)
-        # curr = [0] * arridxlen
-        # for i, c in enumerate(bin(maxor)[2:][::-1]):
-        #     if c == "1":
-        #         curr[i] += 1
         curr = arr[-2].copy()
         ans = [0] * n
         ptr = 0
@@ -42,13 +30,8 @@ class Solution:
                 if c == "1":
                     curr[j] -= 1
             target = [b + 1 if a else 0 for a, b in zip(curr, arr[i-1])]
-            # print()
-            # print(i)
-            # print(target)
             while ptr < n and not comp(target, arr[ptr]):
                 ptr+=1
-            # if sum(target) == 0:
-            #     ans[i] = 0
             else:
                 ans[i] = max(ptr - i + 1, 1)
         return ans
