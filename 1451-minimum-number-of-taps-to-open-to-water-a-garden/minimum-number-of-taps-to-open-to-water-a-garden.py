125185810e0b7db21261a0f58802ef8e66175c79
Humza Mahmood <humzam574@gmail.com>
2025-08-02 12:33:32 -0700

    Time: 276 ms (18.52%) | Memory: 18 MB (96.84%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,12 +1,12 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
         #DEE PEE
-        dp = [inf] * (n + 100)
+        dp = [inf] * (n+1)
         for i,r in enumerate(ranges):
             if r == 0:
                 continue
             low = max(0, i - r)
-            high = i + r
+            high = min(i + r, n)
             if low != 0:
                 val = min(dp[low:i]) + 1
             else:
@@ -15,6 +15,6 @@ class Solution:
                 dp[x] = min(dp[x], val)
             # print(dp[:n+1])
         
-        if max(dp[:n+1]) == inf:
+        if max(dp) == inf:
             return -1
-        return dp[n]
+        return dp[-1]
