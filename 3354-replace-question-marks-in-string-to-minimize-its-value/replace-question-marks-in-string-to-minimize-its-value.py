d068b452650c9b2604cd4dd3e59e5cd49a5a45ae
Humza Mahmood <humzam574@gmail.com>
2025-08-02 10:33:43 -0700

    Time: 213 ms (100.00%) | Memory: 18.7 MB (98.70%) - LeetSync

--- 3354-replace-question-marks-in-string-to-minimize-its-value/replace-question-marks-in-string-to-minimize-its-value.py
+++ 3354-replace-question-marks-in-string-to-minimize-its-value/replace-question-marks-in-string-to-minimize-its-value.py
@@ -1,40 +1,19 @@
+alphabet = ascii_lowercase
+
 class Solution:
-    def minimizeStringValue(self, s: str) -> str:
-        heap = [[0, i] for i in range(26)]
-        #arr = [[0, i] for i in range(26)]
-        q = 0
-        for c in s:
-            if c == "?":
-                q+=1
-            else:
-                idx = ord(c) - 97
-                heap[idx][0] += 1
+    def minimizeStringValue(self, s: str)  -> str:
+
+        ctr, dic = Counter(s), defaultdict(int)                 # <-- 1.
+        qmarks = ctr['?']
+        rep = lambda x, y: x.replace('?', y, dic[y] - ctr[y])
+
+        heap = list(map(lambda x: (ctr[x],x), alphabet))        # <-- 2.
         heapify(heap)
-        arr = [0 for i in range(26)]
-        for i in range(q):
-            val, idx = heappop(heap)
-            heappush(heap, [val+1, idx])
-            arr[idx]+=1
-        # print(arr)
-        # heapify(arr)
-        #ans = 0
-        ans = []
-        ptr = 0
-        #heap = list(heap)
-        #heap.sort(key = lambda x : x[1])
-        for char in s:
-            # c = chr(idx + 97)
-            # for x in range(a):
-            #     ans.append(c)
-            if char != "?":
-                ans.append(char)
-            else:
-                # c,i = heappop(arr)
-                # ans.append(i)
-                # heappush(arr, [c+1, i])
-                while ptr < 26 and arr[ptr] == 0:
-                    ptr+=1
-                ans.append(chr(ptr + 97))
-                arr[ptr]-=1
         
-        return ''.join(ans)
+        for _ in range(qmarks):                                 # <-- 3.
+            cost, ch = heappop(heap)
+            heappush(heap, (cost + 1, ch))
+
+        for cost, ch in heap: dic[ch] = cost                    # <-- 4.
+
+        return reduce(rep, alphabet, s)                         # <-- 5.
