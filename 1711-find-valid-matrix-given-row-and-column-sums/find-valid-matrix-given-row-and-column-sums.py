422c763169f233f61a42d243770e1f713468b805
Humza Mahmood <humzam574@gmail.com>
2025-08-01 17:43:14 -0700

    Time: 4 ms (95.11%) | Memory: 22.1 MB (58.41%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -1,32 +1,15 @@
 class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
-        #[5,7,10]
-        #[8,6,8]
-
-        #[5, 0, 0]
-        #[3, 4, 0]
-        #[0, 2, 8]
-
-        #[3, 8]
-        #[4, 7]
-        
-        #[3, 0]
-        #[1, 7]
-
-        #cant i just do a greedy approach
-
-        ans = [[0] * len(colSum) for _ in range(len(rowSum))]
-        m = len(colSum)
-        n = len(rowSum)
-        for i in range(n):
-            curr = ans[i]
-            rem = rowSum[i]
-            rowSum[i] = 0
-            for j in range(m):
-                curr[j] += min(rem, colSum[j])
-                rem -= curr[j]
-                colSum[j] -= curr[j]
-            ans[i] = curr
-        # for row in ans:
-        #     print(row)
-        return ans
+        m = len(rowSum)
+        n = len(colSum)
+        matrix = [[0] * n for _ in range(m)]
+        i, j = 0, 0
+        while(i < m and j < n):
+            matrix[i][j] = min(rowSum[i], colSum[j])
+            rowSum[i] -= matrix[i][j]
+            colSum[j] -= matrix[i][j]
+            if(rowSum[i] == 0):
+                i += 1
+            else:
+                j += 1
+        return matrix
