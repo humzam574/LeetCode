badbede9fd425392e44e1366410b357c12dc33cb
Humza Mahmood <humzam574@gmail.com>
2025-08-02 12:34:29 -0700

    Time: 7 ms (99.86%) | Memory: 18.3 MB (57.06%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,20 +1,16 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        #DEE PEE
-        dp = [inf] * (n+1)
-        for i,r in enumerate(ranges):
-            if r == 0:
-                continue
-            low = max(0, i - r)
-            high = min(i + r, n)
-            if low != 0:
-                val = min(dp[low:i]) + 1
-            else:
-                val = 1
-            for x in range(low, high+1):
-                dp[x] = min(dp[x], val)
-            # print(dp[:n+1])
-        
-        if max(dp) == inf:
-            return -1
-        return dp[-1]
+        maxReach = [0] * (n + 1)
+        for i, r in enumerate(ranges):
+            left = 0 if i - r < 0 else i - r
+            right = n if i + r > n else i + r
+            maxReach[left] = right if right > maxReach[left] else maxReach[left]
+        taps = curEnd = farthest = 0
+        for i in range(n):
+            farthest = maxReach[i] if maxReach[i] > farthest else farthest
+            if i == curEnd:
+                if farthest == curEnd:
+                    return -1
+                taps += 1
+                curEnd = farthest
+        return taps
