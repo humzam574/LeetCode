f7b9b83d340f458573c5075dc6a63f1cb5740a25
Humza Mahmood <humzam574@gmail.com>
2025-08-02 10:59:58 -0700

    Time: 22 ms (59.21%) | Memory: 18.4 MB (37.10%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.py
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.py
@@ -1,25 +1,17 @@
 class Solution:
     def longestWPI(self, hours: List[int]) -> int:
-        n = len(hours)
-        prefix = [0] * n
-        for i in range(n):
-            hours[i] = 1 if hours[i] > 8 else -1
-            prefix[i] = prefix[i-1] + hours[i]
-        # print(hours)
-        # print(prefix)
-        #keep track first point that has a given value
-        points = {0: -1}
-        for i, v in enumerate(prefix):
-            if v not in points and v < 0:
-                # if v-1 in points:
-                #     points[v] = min(i, points[v-1])
-                # else:
-                points[v] = i
-        ans = 0
-        for i, v in enumerate(prefix):
-            if v > 0:
-                ans = max(ans, i+1)
-            elif v - 1 in points:
-                # print(str(i) + ", " + str(i - points[v-1] + 1))
-                ans = max(ans, i - points[v-1])
-        return ans
+        score = 0
+        res = 0
+        first_occurrence = {}
+
+        for i, h in enumerate(hours):
+            score += (1 if h > 8 else -1)
+            if score > 0:
+                res = i + 1
+            else:
+                if score - 1 in first_occurrence:
+                    res = max(res, i - first_occurrence[score-1])
+            if score not in first_occurrence:
+                first_occurrence[score] = i
+        
+        return res
