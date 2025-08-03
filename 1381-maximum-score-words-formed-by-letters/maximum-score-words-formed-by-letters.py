a91c81545a8ac51b02e821a0f8779ae081e224c8
Humza Mahmood <humzam574@gmail.com>
2025-08-03 13:56:26 -0700

    Time: 11 ms (70.38%) | Memory: 17.9 MB (53.99%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
@@ -1,37 +1,46 @@
 class Solution:
-    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
-        n = len(words)
-        freq = defaultdict(int)
-        for l in letters:
-            freq[l]+=1
-        ans = 0
-        # print(freq)
-        for i in range(1, (2**n)+1):
-            # print(bin(i)[2:])
-            b = bin(i)[2:]
-            b = ("0" * (n - len(b))) + b
-            # print(b)
-            curr = defaultdict(int)
-            use = []
-            for j,c in enumerate(b):
-                if c == "1":
-                    use.append(words[j])
-            cont = True
-            for word in use:
-                for w in word:
-                    curr[w]+=1
-                    if curr[w] > freq[w]:
-                        cont = False
-                        break
-            # print(curr)
-            if not cont:
-                continue
-            # print(curr)
-            
-            val = 0
-            for k, v in curr.items():
-                val+=v*score[ord(k) - 97]
-            # print(val)
-            # print()
-            ans = max(ans, val)
-        return ans
+    def maxScoreWords(
+        self, words: List[str], letters: List[str], score: List[int]
+    ) -> int:
+        W = len(words)
+        # Count how many times each letter occurs
+        self.max_score = 0
+        freq = [0 for i in range(26)]
+        subset_letters = [0 for i in range(26)]
+        for c in letters:
+            freq[ord(c) - 97] += 1
+
+        # Check if adding this word exceeds the frequency of any letter
+        def is_valid_word(subset_letters):
+            for c in range(26):
+                if freq[c] < subset_letters[c]:
+                    return False
+            else:
+                return True
+
+        def check(w, words, score, subset_letters, total_score):
+            if w == -1:
+                # If all words have been iterated,
+                # check the score of this subset
+                self.max_score = max(self.max_score, total_score)
+                return
+            # Not adding words[w] to the current subset
+            check(w - 1, words, score, subset_letters, total_score)
+            # Adding words[w] to the current subset
+            L = len(words[w])
+            for i in range(L):
+                c = ord(words[w][i]) - 97
+                subset_letters[c] += 1
+                total_score += score[c]
+            if is_valid_word(subset_letters):
+                # Consider the next word if this subset is still valid
+                check(w - 1, words, score, subset_letters, total_score)
+            # Rollback effects of this word
+            for i in range(L):
+                c = ord(words[w][i]) - 97
+                subset_letters[c] -= 1
+                total_score -= score[c]
+
+        check(W - 1, words, score, subset_letters, 0)
+        # Return max_score as the result
+        return self.max_score
