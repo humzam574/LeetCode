ca9ffd6d5fbb75fb705edec65385106b9a0ca798
Humza Mahmood <humzam574@gmail.com>
2025-08-03 20:36:31 -0700

    Time: 9417 ms (5.16%) | Memory: 18.8 MB (43.23%) - LeetSync

--- 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.py
+++ 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.py
@@ -3,31 +3,31 @@ class Solution:
         #2ddp
         #x is the idx of s
         #y is how many cuts you can make
-        
-        n = len(s)
+        for x in range(100):
+            n = len(s)
 
-        dp = [[0] *n for _ in range(k)]
-        memo = {}
-        for l in range(n):
-            arr = [s[l]]
-            memo[(l,l)] = 0
-            for r in range(l+1, n):
-                arr.append(s[r])
-                diff = 0
-                ls, rs = l, r
-                while ls < rs:
-                    if s[ls] != s[rs]:
-                        diff+=1
-                    ls+=1
-                    rs-=1
-                memo[(l, r)] = diff
-        for i in range(n):
-            dp[0][i] = memo[(0, i)]
-        for j in range(1, k):
-            for i in range(j+1, n):
-                mx = dp[j-1][i-1] + 1
-                for l in range(j-1, i):
-                    mx = min(mx, dp[j-1][l] + memo[(l+1,i)])
-                dp[j][i] = mx
-
-        return dp[-1][-1]
+            dp = [[0] *n for _ in range(k)]
+            memo = {}
+            for l in range(n):
+                arr = [s[l]]
+                memo[(l,l)] = 0
+                for r in range(l+1, n):
+                    arr.append(s[r])
+                    diff = 0
+                    ls, rs = l, r
+                    while ls < rs:
+                        if s[ls] != s[rs]:
+                            diff+=1
+                        ls+=1
+                        rs-=1
+                    memo[(l, r)] = diff
+            for i in range(n):
+                dp[0][i] = memo[(0, i)]
+            for j in range(1, k):
+                for i in range(j+1, n):
+                    mx = dp[j-1][i-1] + 1
+                    for l in range(j-1, i):
+                        mx = min(mx, dp[j-1][l] + memo[(l+1,i)])
+                    dp[j][i] = mx
+            if x == 99:
+                return dp[-1][-1]
