d10cced2fc31b12779545ad48f81dbf0424254ac
Humza Mahmood <humzam574@gmail.com>
2025-08-10 16:46:45 -0700

    Time: 0 ms (100.00%) | Memory: 39.8 MB (21.34%) - LeetSync

--- 1143-find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.py
+++ 1143-find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.py
@@ -1,6 +1,39 @@
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
-        st = set(mat[0])
-        for row in mat:
-            st = st & set(row)
-        return -1 if len(st) == 0 else min(st)
+        '''
+        1. Find numbers that are common to all rows.
+        2. Find numbers that are common to all cols.
+        3. Find numbers that are common to 1 and 2 and get the minimum.
+        '''
+        ROWS = len(mat)
+        COLS = len(mat[0])
+        unique = set()
+        
+        
+        def search(target: int, nums: List[int]) -> bool:
+            left, right = 0, len(nums)-1
+            while left <= right:
+                mid = (left + right) //2
+                if nums[mid] == target:
+                    return True
+                if nums[mid] < target:
+                    left = mid+1
+                else:
+                    right = mid-1
+            return False
+
+        mini = mat[0][0]
+        maxi = mat[0][COLS-1]
+        for r in range(ROWS):
+            mini = max(mini, mat[r][0])
+            maxi = min(maxi, mat[r][COLS-1])
+        for num in mat[0]:
+            if mini <= num <= maxi:
+                is_present = True
+                for row in range(1, ROWS):
+                    if not search(num, mat[row]):
+                        is_present = False
+                        break
+                if is_present:
+                    return num
+        return -1
