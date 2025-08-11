4cedc24db933fe37e2fbab6f1087ab8eaa3d65c5
Humza Mahmood <humzam574@gmail.com>
2025-08-10 18:19:13 -0700

    Time: 995 ms (98.77%) | Memory: 18.1 MB (93.85%) - LeetSync

--- 1808-stone-game-vii/stone-game-vii.py
+++ 1808-stone-game-vii/stone-game-vii.py
@@ -1,13 +1,16 @@
+
 class Solution:
     def stoneGameVII(self, stones: List[int]) -> int:
-        prefix = list(accumulate(stones, initial = 0))
+        n = len(stones)
+        flag = 1
+        lis = list(stones)
+        for i in range(1,n):
+            for j in range(n - i):
+                if flag:
+                    lis[j] = max(lis[j],lis[j+1])
+                else:
+                    lis[j] = min(lis[j] + stones[j + i],lis[j+1] + stones[j])
+            flag ^= 1
+            #print(lis)
+        return lis[0] if not flag else sum(stones) - lis[0]
         
-        @lru_cache(65536)
-        # @cache
-        def dp(i, j):
-            if i == j: 
-                return 0
-            return max(prefix[j + 1] - prefix[i + 1] - dp(i + 1, j), \
-                       prefix[j] - prefix[i] - dp(i, j - 1))
-        
-        return dp(0, len(stones) - 1)
