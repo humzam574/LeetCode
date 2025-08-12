2e9b878aa5a776171ae7403528d6571a0edc84ab
Humza Mahmood <humzam574@gmail.com>
2025-08-12 14:49:20 -0700

    Time: 106 ms (20.50%) | Memory: 23 MB (10.82%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,26 +1,57 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        num = 0
-        res = 0
-        pre_op = '+'
-        s+='+'
         stack = []
+        minus = 1
         for c in s:
-            if c.isdigit():
-                num = num*10 + int(c)
-            elif c == ' ':
-                    pass
+            if c == ' ':
+                continue
+            elif ord('0') <= ord(c) <= ord('9'):
+                if stack and type(stack[-1]) is int:
+                    val = stack.pop()
+                    stack.append(val*10 + int(c))
+                    
+                else:
+                    stack.append(int(c))
+                # stack[-1]*=minus
+                # minus = 1
+            # elif 
             else:
-                if pre_op == '+':
-                    stack.append(num)
-                elif pre_op == '-':
-                    stack.append(-num)
-                elif pre_op == '*':
-                    operant = stack.pop()
-                    stack.append((operant*num))
-                elif pre_op == '/':
-                    operant = stack.pop()
-                    stack.append(math.trunc(operant/num))
-                num = 0
-                pre_op = c
-        return sum(stack)
+                
+            
+                while len(stack) >= 3 and (type(stack[-1]) is int and type(stack[-3]) is int and (stack[-2] == "/" or stack[-2] == "*")):
+                    # print(stack)
+                    num1 = stack.pop()
+                    expr = stack.pop()
+                    num2 = stack.pop()
+                    if expr == "*":
+                        stack.append(num1*num2)
+                    else:
+                        stack.append(num2//num1)
+                stack.append(c)
+        while len(stack) >= 3 and (type(stack[-1]) is int and type(stack[-3]) is int and (stack[-2] == "/" or stack[-2] == "*")):
+            num1 = stack.pop()
+            expr = stack.pop()
+            num2 = stack.pop()
+            if expr == "*":
+                stack.append(num1*num2)
+            else:
+                stack.append(num2//num1)
+        if not stack:
+            return 0
+        
+        ans = 0#stack[0]
+        # print()
+        # print(stack)
+        minus = 1
+        for c in stack:
+            if type(c) is int:
+                ans+=(minus*c)
+                minus = 1
+                
+            elif c == "+":
+                minus = 1
+            else:
+                minus = -1
+        return ans
+            
+            
