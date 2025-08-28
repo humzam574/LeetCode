291cdb4df3448cdf284e552134da74e6720b2fea
Humza Mahmood <humzam574@gmail.com>
2025-08-27 17:00:51 -0700

    Time: 12 ms (54.58%) | Memory: 17.7 MB (97.50%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
@@ -1,60 +1,39 @@
 class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
-        arr = [0] * n
-        #non reverse for the first n
-        #(0, 2)
-        #(0, 1), (1, 2)
-        #(0, 0), (1, 1), (2, 2)
-        #(1, 0), (2, 1)
-        #(2, 0)
 
-        sx, sy = 0, n - 1
-        for sy in range(n - 1, -1, -1):
-            x = 0
-            y = sy
+        for k in range(n):
             arr = []
-            # print(x)
-            # print(y)
-            for y in range(sy, n):
-                #print(str(x) + " " + str(y))
-                arr.append(grid[x][y])
-                x = x + 1
-                
-            x = 0
-            y = sy
-            if sy == 0:
-                arr.sort(reverse = True)
-            else:
-                arr.sort()
-            #print(arr)
-            #print(grid)
-            for y in range(sy, n):
-                grid[x][y] = arr[y - sy]
-                x += 1
-            
-        sx, sy = n - 1, 0
+            i, j = k, 0
 
-        for sx in range(n - 1, -1, -1):
-            y = 0
-            x = sx
-            arr = []
-            for x in range(sx, n):
-                arr.append(grid[x][y])
-                y += 1
-            x = sx
-            y = 0
-            arr.sort(reverse = True)
-            #print(arr)
-            for x in range(sx, n):
-                grid[x][y] = arr[x - sx]
-                y += 1
+            while i < n:
+                arr.append(grid[i][j])
+                i += 1
+                j += 1
             
+            arr.sort(reverse=True)
+            i, j = k, 0
+
+            while i < n:
+                grid[i][j] = arr[j]
+                i += 1
+                j += 1
         
-        #for sx in range(n - 1, )
-        # for i in range(n):
-        #     arr[i] = grid[i][i]
-        # arr.sort(reverse = True)
-        # for i in range(n):
-        #     grid[i][i] = arr[i]
+        for k in range(1, n):
+            arr = []
+            i, j = 0, k
+
+            while j < n:
+                arr.append(grid[i][j])
+                i += 1
+                j += 1
+            
+            arr.sort()
+            i, j = 0, k
+
+            while j < n:
+                grid[i][j] = arr[i]
+                i += 1
+                j += 1
+        
         return grid
