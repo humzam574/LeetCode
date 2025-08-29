b9a23c3947b1af8f66ecbbee6fa0011e711c36e2
Humza Mahmood <humzam574@gmail.com>
2025-08-28 17:25:42 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.61%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -1,17 +1,2 @@
 class Solution:
-    def flowerGame(self, n: int, m: int) -> int:
-        #n is odd m is even
-        #n is even m is odd
-        
-        nodd = (n+1) // 2
-        
-        neven = (n)//2
-        modd = (m+1) // 2
-        meven = (m)//2
-
-        # print(nodd)
-        # print(neven)
-        # print(modd)
-        # print(meven)
-
-        return nodd*meven + neven*modd
+    def flowerGame(self, n: int, m: int): return (m*n) // 2
