5e5e5c4656736205476e49920552a0648bb1bc0c
Humza Mahmood <humzam574@gmail.com>
2025-08-28 21:17:33 -0700

    Time: 1 ms (47.58%) | Memory: 17.8 MB (95.40%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,11 +1,3 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        self.ans = []
-        def bt(curr, lt):
-            if not curr: self.ans.append(lt)
-            for item in curr.copy():
-                curr.remove(item)
-                bt(curr, lt + [item])
-                curr.add(item)
-        bt(set(nums), [])
-        return self.ans
+        return list(itertools.permutations(nums))
