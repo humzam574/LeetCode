944183e603562f7ed999484eddbfa60664d7ebe1
Humza Mahmood <humzam574@gmail.com>
2025-08-28 23:11:53 -0700

    Time: 107 ms (98.21%) | Memory: 58.9 MB (80.87%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.py
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.py
@@ -1,17 +1,32 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
-        self.dict = defaultdict(list)
-        for a,b in adjacentPairs:
-            self.dict[a].append(b)
-            self.dict[b].append(a)
-        self.ans = []
-        def dfs(prev, curr):
-            self.ans.append(curr)
-            for val in self.dict[curr]:
-                if val == prev:
-                    continue
-                dfs(curr, val)
-        for k,v in self.dict.items():
-            if len(v) == 1:
-                dfs(None, k)
-                return self.ans
+        # make adjacency matrix
+        matrix = defaultdict(list)
+        for u, v in adjacentPairs:
+            matrix[u].append(v)
+            matrix[v].append(u)
+
+        root = None
+
+        for key, value in matrix.items():
+            if len(value) == 1:
+                root = key
+                break
+        
+        prev = None
+        cur = root
+
+        res = [root]
+
+        while len(res) < len(matrix):
+            for val in matrix[cur]:
+                if val != prev:
+                    res.append(val)
+                    prev = cur
+                    cur = val
+                    break
+
+        return res
+        # find root
+
+        # set prev = None, cur = root
