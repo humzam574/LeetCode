1c0a486ffe62663084b0454cf867e8ed011fe77c
Humza Mahmood <humzam574@gmail.com>
2025-09-01 17:43:54 -0700

    Time: 303 ms (10.26%) | Memory: 17.8 MB (53.85%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
@@ -0,0 +1,44 @@
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        #generate n^2 number of pairs
+        #go thru every single value to find a match (n)
+        #total is O(N^3)
+        def pt(x,y):
+            print(str(x) + ", " + str(y))
+        ans = 0
+        n = len(points)
+        for i in range(len(points)):
+            a,b = points[i]
+            for j in range(i+1, len(points)):
+                c,d = points[j]
+                if d >= b and c <= a:
+                    print("searching")
+                    cont = True
+                    for e,f in points:
+                        if (e == c and f == d) or (e == a and f == b):
+                            continue
+                        if b <= f <= d and c <= e <= a:
+                            pt(a,b)
+                            pt(c,d)
+                            pt(e,f)
+                            print()
+                            cont = False
+                            break
+                    if cont:
+                        ans+=1
+                elif b >= d and c >= a:
+                    print("searching")
+                    cont = True
+                    for e,f in points:
+                        if (e == c and f == d) or (e == a and f == b):
+                            continue
+                        if b >= f >= d and c >= e >= a:
+                            pt(a,b)
+                            pt(c,d)
+                            pt(e,f)
+                            print()
+                            cont = False
+                            break
+                    if cont:
+                        ans+=1
+        return ans
