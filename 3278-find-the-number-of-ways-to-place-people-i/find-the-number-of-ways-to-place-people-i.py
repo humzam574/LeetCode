23000001257be4408d5ede5aa92da8b6925b3c45
Humza Mahmood <humzam574@gmail.com>
2025-09-01 17:44:09 -0700

    Time: 115 ms (56.41%) | Memory: 17.7 MB (53.85%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
@@ -12,31 +12,31 @@ class Solution:
             for j in range(i+1, len(points)):
                 c,d = points[j]
                 if d >= b and c <= a:
-                    print("searching")
+                    # print("searching")
                     cont = True
                     for e,f in points:
                         if (e == c and f == d) or (e == a and f == b):
                             continue
                         if b <= f <= d and c <= e <= a:
-                            pt(a,b)
-                            pt(c,d)
-                            pt(e,f)
-                            print()
+                            # pt(a,b)
+                            # pt(c,d)
+                            # pt(e,f)
+                            # print()
                             cont = False
                             break
                     if cont:
                         ans+=1
                 elif b >= d and c >= a:
-                    print("searching")
+                    # print("searching")
                     cont = True
                     for e,f in points:
                         if (e == c and f == d) or (e == a and f == b):
                             continue
                         if b >= f >= d and c >= e >= a:
-                            pt(a,b)
-                            pt(c,d)
-                            pt(e,f)
-                            print()
+                            # pt(a,b)
+                            # pt(c,d)
+                            # pt(e,f)
+                            # print()
                             cont = False
                             break
                     if cont:
