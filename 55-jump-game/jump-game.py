0699ffab91130a2591719282675393954abf50cc
Humza Mahmood <humzam574@gmail.com>
2025-09-09 17:09:18 -0700

    Time: 25 ms (60.04%) | Memory: 18.6 MB (68.15%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,16 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        # if n == 1:
-        #     return True
         far = nums[0]
         prev = 0
         while far != prev and far < n-1:
             temp = prev
             prev = far
-            # print("looping")
             for i in range(temp+1, far+1):
-                
                 if i + nums[i] > far:
                     far = nums[i] + i
         return far >= n-1
