793a1321addf74b521443a65bcd61d636ccc7cbe
Humza Mahmood <humzam574@gmail.com>
2025-09-12 14:50:04 -0700

    Time: 4 ms (85.80%) | Memory: 18 MB (79.76%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -1,14 +1,6 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        #if theres an even amount, alice will pick an odd chunk and bob loses
-
-        #if there is a odd amount, alice picks an odd amount and the rest is bob which is even
-        #now there is an even amount and bob takes it all
-
-        count = 0
-        for c in s:
-            if c in {'a', 'e', 'i', 'o', 'u'}:
-                return True
-        # if not count:
-        #     return False
-        return False
+        count = s.count('a') + s.count('e') + s.count('i') + s.count('o') + s.count('u')
+        if count == 0: return False
+        else: return True
+        
