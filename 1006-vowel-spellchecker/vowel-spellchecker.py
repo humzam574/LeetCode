b46a7b95646438ee59cb5a0ec09b98bd3f9cde92
Humza Mahmood <humzam574@gmail.com>
2025-09-14 18:01:51 -0700

    Time: 15 ms (99.43%) | Memory: 20.2 MB (78.16%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.py
+++ 1006-vowel-spellchecker/vowel-spellchecker.py
@@ -1,33 +1,30 @@
+from collections import defaultdict
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
-        broad = defaultdict(list)
-        narrow = defaultdict(set)
-        narrowlist = defaultdict(list)
-        for word in wordlist:
-            arr = ''.join([(c.lower() if c not in {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'} else "*") for c in word])
-            broad[arr].append(word)
-            narrow[word.lower()].add(word)
-            narrowlist[word.lower()].append(word)
         
-        ans = []
-        # print(broad)
-        # print(narrow)
-        for q in queries:
-            temp = ''.join([(c.lower() if c not in {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'} else "*") for c in q])
-            # print(temp)
-            # if temp not in dict:
-            #     ans.append("")
-            # elif q in dict[temp]:
-            #     ans.append(q)
-            # else:
-            #     ans.append(next(iter(dict[temp])))
-            if q.lower() in narrow and q in narrow[q.lower()]:
-                ans.append(q)
-            elif q.lower() in narrow:
-                ans.append(narrowlist[q.lower()][0])
-            elif temp in broad:
-                ans.append(broad[temp][0])
-            else:
-                ans.append("")
+        def case_hash(s):
+            return s.lower()
         
-        return ans
+        def vowel_hash(s):
+            return s.lower().replace('e', 'a').replace('i', 'a').replace('o', 'a').replace('u', 'a')
+        
+        
+        exact = set(wordlist)
+        case = defaultdict()
+        vowl = defaultdict()
+        for w in wordlist:
+            c = case_hash(w)
+            if c not in case: case[c] = w
+            v = vowel_hash(w)
+            if v not in vowl: vowl[v] = w
+        
+        def correct(w):
+            if w in exact: return w
+            c = case_hash(w)
+            if c in case: return case[c]
+            v = vowel_hash(w)
+            if v in vowl: return vowl[v]
+            return ''
+                    
+        return [correct(q) for q in queries]
+            
