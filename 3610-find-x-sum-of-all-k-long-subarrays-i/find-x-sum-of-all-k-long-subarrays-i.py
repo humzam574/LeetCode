5464c5178d161cc31997d30ba295dff3e3b5364e
Humza Mahmood <humzam574@gmail.com>
2025-11-03 19:07:21 -0800

    Time: 20 ms (83.38%) | Memory: 17.6 MB (96.88%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,21 +1,26 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        ctr = defaultdict(int)
+        k_ele_maps = {}
+        answer = [0] * (len(nums) - k + 1)
+
         for i in range(k):
-            ctr[nums[i]]+=1
-        
-        l = 0
-        ans = []
-        arr = sorted(list(ctr.keys()), key = lambda x : (ctr[x], x), reverse=True)
-        print(arr)
-        ans.append(sum([arr[i]*ctr[arr[i]] for i in range(min(x,len(arr)))]))
-        for r in range(k, len(nums)):
-            ctr[nums[l]]-=1
-            ctr[nums[r]]+=1
-            arr = sorted(list(ctr.keys()), key = lambda x : (ctr[x], x), reverse=True)
-            print(arr)
-            print(ctr)
-            print()
-            ans.append(sum([arr[i]*ctr[arr[i]] for i in range(min(x,len(arr)))]))
-            l+=1
-        return ans
+            if nums[i] not in k_ele_maps:
+                k_ele_maps[nums[i]] = 1
+            else:
+                k_ele_maps[nums[i]] += 1
+
+        for i in range(len(nums) - k + 1):
+            top = sorted(k_ele_maps.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)[:x]
+            answer[i] = sum(v * f for v, f in top)
+            out_num = nums[i]
+            k_ele_maps[out_num] -= 1
+            if k_ele_maps[out_num] == 0:
+                del k_ele_maps[out_num]
+            if i + k < len(nums):
+                in_num = nums[i + k]
+                if in_num not in k_ele_maps:
+                    k_ele_maps[in_num] = 1
+                else:
+                    k_ele_maps[in_num] += 1
+
+        return(answer)  
