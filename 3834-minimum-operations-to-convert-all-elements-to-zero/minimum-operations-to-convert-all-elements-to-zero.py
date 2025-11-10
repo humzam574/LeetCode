013fcd1d043ab751232be4ba1863d1b9cd3b5b7a
Humza Mahmood <humzam574@gmail.com>
2025-11-10 15:04:41 -0800

    Time: 150 ms (98.76%) | Memory: 29.5 MB (96.27%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.py
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.py
@@ -1,28 +1,15 @@
 class Solution:
-    def minOperations(self, nums: List[int]) -> int:
-        #get all the unique numbers, make a dict which will count when you need to make a new subset
-        #something monotonic stack
-        uniques = set()
-        low = inf
-        for num in nums:
-            low = min(low, num)
-            uniques.add(num)
-        uniques.remove(low)
-        ans = int(bool(low)) #if the lowest number is 0, start with 0, else 1
-
-        mono = []
-        #ans = 0
+    def minOperations(self, nums):
+        stack = [0] * (len(nums) + 1)
+        top = 0
+        ans = 0
 
         for num in nums:
-            while mono and mono[-1] > num:
-                mono.pop()
-                ans+=1
-            if mono and mono[-1] == num:
-                continue
-            mono.append(num)
-            # print(mono)
-        while len(mono) >= 2 and mono[-1] > mono[-2]:
-            mono.pop()
-            ans+=1
-        # ans+=int(bool(mono))
-        return ans
+            while stack[top] > num:
+                top -= 1
+                ans += 1
+            if stack[top] != num:
+                top += 1
+                stack[top] = num
+
+        return ans + top
