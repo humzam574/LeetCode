d811d81c642b6b4831c831e8310d1ad964f62834
Humza Mahmood <humzam574@gmail.com>
2025-11-15 16:33:21 -0800

    Time: 7 ms (94.38%) | Memory: 18.8 MB (5.24%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -1,13 +1,8 @@
 class Solution:
     def numSub(self, s: str) -> int:
-        curr = 0
-        ans = 0
-        s+="0"
-        for c in s:
-            if c == "0":
-                ans+=((curr)*(curr+1))/2
-                ans = int(ans % 1000000007)
-                curr = 0
-            else:
-                curr+=1
-        return ans % 1000000007
+        cnt = 0
+        for part in s.split('0'):
+            n = len(part)
+            cnt += n*(n+1)
+
+        return (cnt // 2) % (10**9 + 7)
