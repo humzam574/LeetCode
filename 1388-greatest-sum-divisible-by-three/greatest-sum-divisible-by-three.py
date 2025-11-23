d985a93924f090e2643bf30fb632672fb0d5d032
Humza Mahmood <humzam574@gmail.com>
2025-11-22 20:43:05 -0800

    Time: 156 ms (18.93%) | Memory: 28.3 MB (13.02%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
@@ -4,7 +4,6 @@ class Solution:
         # dp[0][nums[0]%3]=1
         for i in range(len(nums)):
             delta = nums[i] % 3
-            #if True:#dp[i-1][0] or i==0:
             dp[i][delta] = dp[i-1][0] + nums[i]
             if dp[i-1][1]:
                 dp[i][(delta+1)%3] = dp[i-1][1] + nums[i]
@@ -13,7 +12,5 @@ class Solution:
             for j in range(3):
                 dp[i][j] = max(dp[i][j],dp[i-1][j])
             
-        for row in dp:
-            print(row)
         return dp[-2][0]
             
